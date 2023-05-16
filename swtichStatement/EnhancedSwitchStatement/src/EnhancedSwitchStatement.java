c281d1864a582159ab81dce8ba64aa6f07727635
Umair Ali <umairmamoor@gmail.com>
2023-05-16 15:25:32 -0400

    EnhancedSwitchStatment

---
+++ swtichStatement/EnhancedSwitchStatement/src/EnhancedSwitchStatement.java
@@ -0,0 +1,31 @@
+public class EnhancedSwitchStatement {
+
+    public static void printDayOfWeek(int day){
+
+        //using switch statement as an expression
+        String dayOfWeek = switch (day) {
+            case 0 -> "Sunday"; //not using system.out.println because the expression is string
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> "Friday";
+            case 6 -> "Saturday";
+            default -> "Invalid Day";
+        };
+        System.out.println(day + " stands for " + dayOfWeek);
+        //because the method is void so we cant pass the value of the String.
+    }
+
+    public static void main(String[] args) {
+
+      printDayOfWeek(0);
+      printDayOfWeek(1);
+      printDayOfWeek(2);
+      printDayOfWeek(3);
+      printDayOfWeek(4);
+      printDayOfWeek(5);
+      printDayOfWeek(6);
+      printDayOfWeek(7);
+    }
+}
