e0b0bc624f8a6b7fcef57ca23a62136846ab7d81
Umair Ali <umairmamoor@gmail.com>
2023-05-24 14:24:40 -0400

    EvenDigitSum

---
+++ Introduction to Java/TheClassObjectStaticAndinstanceFeildsAndMethods/EvenDigitSum/src/EvenDigitSum.java
@@ -0,0 +1,58 @@
+public class EvenDigitSum {
+
+    public static void main(String[] args) {
+        System.out.println(getEvenDigitSum(123456789));
+        System.out.println(getEvenDigitSum(252));
+        System.out.println(getEvenDigitSum(-22));
+
+    }
+
+    public static int getEvenDigitSum(int number){
+        int sum = 0;
+
+        if (number < 0){
+            return -1;
+        }
+
+        while(number > 0) {
+            if(number % 2 == 0) {
+                int lastDigit = (number % 10);
+                sum += lastDigit;
+            }
+            number /= 10;  // divide and assignment operator
+
+        }
+
+
+
+
+
+        return sum;
+    }
+
+}
+
+
+
+
+
+/*
+* Even Digit Sum
+Write a method named getEvenDigitSum with one parameter of type int called number.
+
+The method should return the sum of the even digits within the number.
+
+If the number is negative, the method should return -1 to indicate an invalid value.
+
+
+
+EXAMPLE INPUT/OUTPUT:
+
+getEvenDigitSum(123456789); → should return 20 since 2 + 4 + 6 + 8 = 20
+
+getEvenDigitSum(252); → should return 4 since 2 + 2 = 4
+
+getEvenDigitSum(-22); → should return -1 since the number is negative
+
+
+* */
