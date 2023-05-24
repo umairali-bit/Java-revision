ca3ffe2ffe4f5ec4cb5ce723d9984dd7b3f456be
Umair Ali <umairmamoor@gmail.com>
2023-05-24 12:06:23 -0400

    First and last digit sum

--- TheClassObjectStaticAndinstanceFeildsAndMethods/NumberPalindrome/src/numberPalindrome.java
+++ TheClassObjectStaticAndinstanceFeildsAndMethods/NumberPalindrome/src/numberPalindrome.java
@@ -1,6 +1,6 @@
 public class numberPalindrome {
 
-    public static boolean isPlaindrome(int number) {
+    public static boolean isPalindrome(int number) {
 
 
         int reverse = 0;
@@ -24,8 +24,8 @@ public class numberPalindrome {
     }
 
     public static void main(String[] args) {
-        System.out.println(isPlaindrome(1221));
-        System.out.println(isPlaindrome(11));
+        System.out.println(isPalindrome(1221));
+        System.out.println(isPalindrome(11));
     }
 }
 
