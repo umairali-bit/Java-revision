9e877d132ff4451500dc5be9f48eb3f4677cb220
Umair Ali <umairmamoor@gmail.com>
2023-05-30 15:16:53 -0400

    ReadingUserInput

--- Introduction to Java/TheClassObjectStaticAndinstanceFeildsAndMethods/NumberToWords/src/NumberToWords.java
+++ Introduction to Java/TheClassObjectStaticAndinstanceFeildsAndMethods/NumberToWords/src/NumberToWords.java
@@ -70,7 +70,7 @@ public class NumberToWords {
         int reverse = 0;
 
 
-        //if number is negative how to reverse that
+        //if a number is negative how to reverse that
         if(number < 0){
 
             number = -1 * number;
@@ -87,7 +87,7 @@ public class NumberToWords {
 
         }
 
-        //if number is +ve how to reverse that
+        //if a number is +ve how to reverse that
         while (number > 0) {
             int lastDigit = number % 10;
             reverse = lastDigit + reverse * 10;
