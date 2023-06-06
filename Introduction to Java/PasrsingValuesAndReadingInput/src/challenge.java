ff9f125cd666072959db1cb19b4ab17b9584dc60
Umair Ali <umairmamoor@gmail.com>
2023-06-06 10:40:17 -0400

    flour pack problem

--- Introduction to Java/PasrsingValuesAndReadingInput/src/challenge.java
+++ Introduction to Java/PasrsingValuesAndReadingInput/src/challenge.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
-            canPack();
+            //canPack();
+            System.out.println(canPack(-3,2,12));
         }
 
         public static void canPack() {
@@ -50,4 +51,22 @@ import java.util.Scanner;
 
 
         }
-}
+
+
+            public static boolean canPack(int bigCount, int smallCount, int goal) {
+                if ((bigCount < 0) || (smallCount < 0) || (goal < 0)) {
+                    return false;
+                }
+
+                for (int i = 0; i <= bigCount; i++){
+                    for (int j = 0; j <= smallCount; j++) {
+                        if (i*5 + j == goal){
+                            return true;
+                        }
+                    }
+                }
+                return false;
+
+            }
+        }
+
