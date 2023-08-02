32daf3927d8dcbd3757368cc6dc0613f404535ba
Umair Ali <umairmamoor@gmail.com>
2023-08-02 14:51:57 -0400

    getters method

--- OOP-Part1-Inheritance/IntoductionToClassesAndObjects/src/Car.java
+++ OOP-Part1-Inheritance/IntoductionToClassesAndObjects/src/Car.java
@@ -2,17 +2,40 @@ public class Car {
 
     // creating fields
 
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
-    private boolean convertible;
+    private String make = "G-Wagon";
+    private String model = "G63";
+    private String color = "Black";
+    private int doors = 5;
+    private boolean convertible = false;
 
+    //get make
+    public String getMake() {
+        return make;
+    }
 
+    // get color
+    public String getColor() {
+        return color;
+    }
+
+    //get doors
+    public int getDoors() {
+        return doors;
+    }
+
+    //get model
+    public int getModel() {
+        return doors;
+    }
+
+    //isConvertibel?
+    public boolean isConvertible() {
+        return convertible;
+    }
     //non-static method cuz we are accessing the instances in the class
 
     public void describeCar(){
-        System.out.println(doors + "Door" +
+        System.out.println(doors + "-Door " +
                 color + "  " +
                 make + "  " +
                 model + "  " +
--- OOP-Part1-Inheritance/IntoductionToClassesAndObjects/src/Main.java
+++ OOP-Part1-Inheritance/IntoductionToClassesAndObjects/src/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Car car = new Car();
+//        car.make = "Porche";
+//        car.model = "Carrera";
+//        car.color = "Red";
+        System.out.println("make = " + car.getMake());
+        System.out.println("model = " + car.getModel());
+        System.out.println("Color = " + car.getColor());
+        car.describeCar();
+
     }
 }
