a0c0bd22ab05d268a5667cb3a1428839dd4dc139
Umair Ali <umairmamoor@gmail.com>
2023-08-18 19:03:30 -0400

    inheritance

--- OOP-Part1-Inheritance/Inheritance/src/Main.java
+++ OOP-Part1-Inheritance/Inheritance/src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal animal = new Animal("Generic Animal ", " huge ", 400);
+        doAnimalStuff(animal, " slow ");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog, " fast ");
+
+        Dog yorkie = new Dog("yorkie ", 15);
+        doAnimalStuff(yorkie, "fast");
+        Dog retriever = new Dog("Lab", 65, "Floppy", "Swimmer");
+        doAnimalStuff(retriever , " Slow " );
+    }
+
+    // static method because we want to call it from the main method
+    public static void doAnimalStuff(Animal animal, String speed){
+        animal.makeNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_____");
+
+    }
 }
