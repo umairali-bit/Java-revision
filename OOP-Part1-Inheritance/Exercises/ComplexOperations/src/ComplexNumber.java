e92f2bced22cd0e9fcd7418663ac64736df5a729
Umair Ali <umairmamoor@gmail.com>
2023-08-18 18:10:46 -0400

    Inheritance

--- OOP-Part1-Inheritance/Exercises/ComplexOperations/src/ComplexNumber.java
+++ OOP-Part1-Inheritance/Exercises/ComplexOperations/src/ComplexNumber.java
@@ -17,23 +17,33 @@ public class ComplexNumber {
     }
 
     public void add (double real, double imaginary){
+        //adding parameters to its fields
         this.real += real;
         this.imaginary += imaginary;
     }
 
     public void add (ComplexNumber c){
-        this.real = c.real;
-        this.imaginary = c.imaginary;
+        //adding complex number parameter to its corresponding fields
+        this.real += c.real;
+        this.imaginary += c.imaginary;
 
     }
 
+
+
+
     public void subtract (double real, double imaginary){
+        //subtracting parameters from its fields
         this.real-= real;
         this.imaginary-= imaginary;
     }
 
+
     public void subtract (ComplexNumber c){
+        //subtracting complex number parameter from its corresponding fields
         this.real -= c.real;
         this.imaginary -= c.imaginary;
     }
+
+
 }
--- OOP-Part1-Inheritance/Exercises/ComplexOperations/src/Main.java
+++ OOP-Part1-Inheritance/Exercises/ComplexOperations/src/Main.java
@@ -21,6 +21,7 @@ public class Main {
 }
 
 /*
+
 Complex Operations
 A complex number is a number that can be expressed in the form a + bi, where a and b are real numbers, and i is a solution of the equation x2 = −1. Because no real number satisfies this equation, i is called an imaginary number. For the complex number a + bi, a is called the real part, and b is called the imaginary part. To add or subtract two complex numbers, just add or subtract the corresponding real and imaginary parts. For instance, the sum of 5 + 3i and 4 + 2i is 9 + 5i. For another, the sum of 3 + i and –1 + 2i is 2 + 3i.
 
@@ -78,4 +79,5 @@ NOTE: All ​methods should be defined as public NOT public static.
 NOTE: In total, you have to write 6 methods.
 
 NOTE: Do not add a main method to the solution code.
+
  */
