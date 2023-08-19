fdbf12dbd59a5bd3ed2390b349ad98f1787181a3
Umair Ali <umairmamoor@gmail.com>
2023-08-19 19:40:31 -0400

    JavaLangObject

--- OOP-Part1-Inheritance/JavaLangObject/src/Main.java
+++ OOP-Part1-Inheritance/JavaLangObject/src/Main.java
@@ -2,10 +2,13 @@ public class Main extends Object{
 
     public static void main(String[] args) {
         Student max = new Student("MAX", 25);
-        System.out.println(max.toString());
+        //System.out.println(max.toString());
+        System.out.println(max);
         //Student@7a81197d. prints name of the class "Student" and hashcode of the object "7a81197d"
         // hashcode is an integer which is unique to an instance.Its a mechanisim for instance
 
+        primarySchoolStudent jimmy = new primarySchoolStudent("Jimmy", 8, "Carol");
+        System.out.println(jimmy);
     }
 }
 
@@ -17,4 +20,37 @@ class Student{
         this.name = name;
         this.age = age;
     }
+
+    /*
+
+    @Override
+    public String toString(){
+        return super.toString(); //its reduntant code because hashcode is coming from the Object class
+    }
+
+     */
+
+    @Override
+    public String toString() {
+        return name + " is " + age;
+//        return "Student{" +
+//                "name='" + name + '\'' +
+//                ", age=" + age +
+//                '}';
+    }
+}
+
+class primarySchoolStudent extends Student{
+    private String parentName;
+
+    primarySchoolStudent(String name, int age, String parentName) {
+        super(name, age);
+        this.parentName = parentName;
+    }
+
+    @Override
+    public String toString() {
+        return parentName + "'s kid, " + super.toString();
+    }
+
 }
