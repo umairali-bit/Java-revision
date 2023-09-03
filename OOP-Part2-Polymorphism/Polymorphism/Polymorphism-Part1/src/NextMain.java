e04d52c4689201e95f18d27bb29a11842db4ffd4
Umair Ali <umairmamoor@gmail.com>
2023-09-03 13:44:23 -0400

    using instance0f

--- OOP-Part2-Polymorphism/Polymorphism/Polymorphism-Part1/src/NextMain.java
+++ OOP-Part2-Polymorphism/Polymorphism/Polymorphism-Part1/src/NextMain.java
@@ -28,6 +28,38 @@ public class NextMain {
         var plane = new Comedy("Airplane");
         plane.watchComedy();
 
+        //testing the runtime and three ways to do it
+        //if an unknown(object) movie is truly an Adventure(object)
+        Object unknownObject = Movie.getMovie("C", "Airplane");
+        if(unknownObject.getClass().getSimpleName() == "Comedy"){//method1
+            Comedy c = (Comedy) unknownObject;
+            c.watchComedy();
+        } else if(unknownObject instanceof Adventure){//method2
+            ((Adventure) unknownObject).watchAdventure();//chaining a method directly if unknownObject is an instance of Adventure
+        } else if(unknownObject instanceof ScienceFiction syfy){//method3
+            syfy.watchSciFi();//another method to do this
+        }
+
+        //testing the runtime using method 2
+        Object unknownObject1 = Movie.getMovie("A", "Jaws");
+        if(unknownObject1 instanceof Comedy){
+            ((Comedy)unknownObject1).watchComedy();
+        } else if(unknownObject1 instanceof Adventure){
+            ((Adventure) unknownObject1).watchAdventure();//chaining a method directly if unknownObject is an instance of Adventure
+        } else if(unknownObject1 instanceof ScienceFiction syfy1){
+            syfy1.watchSciFi();//another method to do this
+        }
+
+        //testing the runtime using method 3
+        Object unknownObject2 = Movie.getMovie("S", "Star Wars");
+        if(unknownObject2 instanceof Comedy com){
+            com.watchComedy();
+        } else if(unknownObject2 instanceof Adventure adv){
+            adv.watchAdventure();//chaining a method directly if unknownObject is an instance of Adventure
+        } else if(unknownObject2 instanceof ScienceFiction syfy2){
+            syfy2.watchSciFi();//another method to do this
+        }
+
 
     }
 }
