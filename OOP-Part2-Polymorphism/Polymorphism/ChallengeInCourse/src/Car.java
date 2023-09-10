b5e0e8d5da8bdb56d93a8440c3337d4069f6e8ad
Umair Ali <umairmamoor@gmail.com>
2023-09-10 10:24:02 -0400

    Polymorphism challenge in course

--- OOP-Part2-Polymorphism/Polymorphism/ChallengeInCourse/src/Car.java
+++ OOP-Part2-Polymorphism/Polymorphism/ChallengeInCourse/src/Car.java
@@ -59,3 +59,66 @@ class GasPoweredCar extends Car{
         System.out.printf("Gas -> usage exceeds the average: %.2f %n", avgKmPerLiter);
     }
 }
+class ElectricCar extends Car {
+
+    private double avgKmPerCharge;
+    private int batterySize = 6;
+
+    //constructor without any arguments
+    public ElectricCar(String description) {
+        super(description);
+    }
+
+    //constructor with arguments to pass the number of cylinders and avgKmPerLiter for GasPoweredClass
+    // now we can create an instance of this kind of car in the main method and execute the runrace() on it
+    public ElectricCar(String description, double avgKmPerCharge, int batterySize) {
+        super(description);
+        this.avgKmPerCharge = avgKmPerCharge;
+        this.batterySize = batterySize;
+    }
+
+    //overriding methods
+
+    @Override
+    public void startEngine() {
+        System.out.printf("BEV -> switch %d kWh battery on, Ready! %n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("BEV -> usage on the average: %.2f %n", avgKmPerCharge);
+    }
+}
+class HybridCar extends Car{
+
+    private double avgKmPerLiter;
+    private int cylinders = 6;
+    private int batterySize;
+
+    //constructor without any arguments
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    //constructor with arguments to pass the number of cylinders and avgKmPerLiter for GasPoweredClass
+    // now we can create an instance of this kind of car in the main method and execute the runrace() on it
+    public HybridCar(String description, double avgKmPerLiter, int cylinders, int batterySize) {
+        super(description);
+        this.avgKmPerLiter = avgKmPerLiter;
+        this.cylinders = cylinders;
+        this.batterySize = batterySize;
+    }
+
+    //overriding methods
+
+    @Override
+    public void startEngine() {
+        System.out.printf("Hybrid -> %d cylinders are fired up. %n", cylinders);
+        System.out.printf("Hybrid -> switch %d kWh battery on, Ready!%n", batterySize);
+    }
+
+    @Override
+    protected void runEngine() {
+        System.out.printf("Hybrid -> usage below the average: %.2f %n", avgKmPerLiter);
+    }
+}
--- OOP-Part2-Polymorphism/Polymorphism/ChallengeInCourse/src/Main.java
+++ OOP-Part2-Polymorphism/Polymorphism/ChallengeInCourse/src/Main.java
@@ -2,14 +2,20 @@ public class Main {
 
     public static void main(String[] args) {
 
+        /* using the constructor in the GasPoweredCar class
+           the output tells us that car's method were called but actual runtime type is gaspoweredcar cuz we
+           used .getClass().getSimpleName()
+         */
+
         Car car = new Car("2022 Blue Ferrari 296 GTS");
         runRace(car);
 
-
-        // using the constructor in the GasPoweredCar class
-        //the output tells us that car's method were called but actual runtime type is gaspoweredcar cuz we used .getClass().getSimpleName()
         Car ferrari = new GasPoweredCar("2022 Blue Ferrari 296 GTS", 15.4,6);
         runRace(ferrari);
+        Car tesla = new ElectricCar("2022 Red Tesla model 3",568, 75 );
+        runRace(tesla);
+        Car ferrariHybrid = new HybridCar("2022 Black SF90 Ferrari 296", 16,8,8);
+        runRace(ferrariHybrid);
 
     }
 
