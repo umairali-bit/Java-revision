2a83bdc0d19b1defc8008081859f373a6dbe611f
Umair Ali <umairmamoor@gmail.com>
2023-10-13 09:44:23 -0400

    use of java.uti.Random class. getRandomArray returning 10 random elements in the array.

--- Arrays/java.util.Arrays(sort,fill,copyOf)/src/de/lpa/Main.java
+++ Arrays/java.util.Arrays(sort,fill,copyOf)/src/de/lpa/Main.java
@@ -1,7 +1,31 @@
 package de.lpa;
+import java.util.Arrays;
+import java.util.Random; //to return random numbers
 
 public class Main {
+
+    //method that will return an array of random integers
+    private static int[] getRandomArray(int len){
+        //to return random numbers
+        Random random = new Random();
+        int[] newInt = new int[len];
+
+        //assign a random number to each element in the array under 100
+        for(int i = 0; i <len; i++){
+            newInt[i] = random.nextInt(100);
+        }
+
+        return newInt;
+        }
+
+
+
     public static void main(String[] args) {
+
+        int[] firstArray = getRandomArray(10);
+        //to strong method because we are using static method
+        System.out.println(Arrays.toString(firstArray));
+
 
     }
 }
