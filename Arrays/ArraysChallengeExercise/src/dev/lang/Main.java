388ec373a8e030147eb4137393ea527f4e1a0874
Umair Ali <umairmamoor@gmail.com>
2023-10-14 15:26:17 -0400

    swapping of elements to get the descending sorted array

---
+++ Arrays/ArraysChallengeExercise/src/dev/lang/Main.java
@@ -0,0 +1,63 @@
+package dev.lang;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+
+public class Main {
+
+
+    public static int[] listOfIntegers(int len){
+
+        Random random = new Random();
+        int[] arrayOfInt = new int[len];
+
+        for(int i = 0; i < len; i++){
+            arrayOfInt[i] = random.nextInt(1000);
+        }
+        return arrayOfInt;
+
+    }
+
+    private static int[] sortIntegers(int [] array){
+        System.out.println(Arrays.toString(array));
+        int[] sortedArray = Arrays.copyOf(array, array.length);
+
+        //sorting in descending order
+        boolean flag = true;
+        int temp;
+
+        while(flag){
+            flag = false;
+
+            for(int i = 0; i < sortedArray.length-1; i++){
+                if (sortedArray[i] < sortedArray[i+1]){
+                    temp = sortedArray[i];
+                    sortedArray[i] = sortedArray[i+1];
+                    sortedArray[i + 1] = temp;
+                    flag = true;
+                    System.out.println("-------->" + Arrays.toString(sortedArray));
+                }
+            }
+            System.out.println("--->" + Arrays.toString(sortedArray));
+
+        }
+        return sortedArray;
+
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(Arrays.toString(listOfIntegers(10)));
+        int sortedArray[] = sortIntegers(new int[]{5,6,7});
+        System.out.println(Arrays.toString(sortedArray));
+
+
+
+    }
+
+
+}
+
+
