099cede62c98d52751c4059842a27f95671d27a9
Umair Ali <umairmamoor@gmail.com>
2023-10-16 09:33:25 -0400

    array recap

--- Arrays/Challenge/src/dev/lpa/PrintArray.java
+++ Arrays/Challenge/src/dev/lpa/PrintArray.java
@@ -3,7 +3,7 @@ package dev.lpa;
 
 public class PrintArray {
 
-    public static void printArray(int[] array){ //only static would not be allowed here cuz we cant retrun an array
+    public static void printArray(int[] array){ //only static would not be allowed here cuz we can't return an array
         for(int i = 0; i < array.length; i++) {
             System.out.println("Element " + i + " contents " + array[i]);
         }
---
+++ Arrays/MoreArrays/src/dev/lpa/Main.java
@@ -0,0 +1,11 @@
+package dev.lpa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //creating an array
+        int [] array = new int[5]; //5 is the length of the array
+
+    }
+}
