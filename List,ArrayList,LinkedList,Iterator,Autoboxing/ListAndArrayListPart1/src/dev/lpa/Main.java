7b3b4248bb181df954cfaccbfe8438ef45944f7e
Umair Ali <umairmamoor@gmail.com>
2023-11-08 10:52:38 -0500

    adding elements to the arrayList, indexing, toString method for a cleaner output

--- List,ArrayList,LinkedList,Iterator,Autoboxing/ListAndArrayListPart1/src/dev/lpa/Main.java
+++ List,ArrayList,LinkedList,Iterator,Autoboxing/ListAndArrayListPart1/src/dev/lpa/Main.java
@@ -13,6 +13,12 @@ record GroceryItem(String name, String type, int count) {
     public GroceryItem(String name){
         this(name, "DAIRY", 1);
     }
+
+    //to have cleaner output
+    @Override
+    public String toString() {
+        return String.format("%d %s in %s", count ,name.toUpperCase(),type);
+    }
 }
 public class Main {
 
@@ -25,7 +31,7 @@ public class Main {
         groceryArray[2] = "5 Oranges";
         System.out.println(Arrays.deepToString(groceryArray));
 
-
+        // notice the difference
         GroceryItem[] groceryArray2 = new GroceryItem[3];
         groceryArray2[0] = new GroceryItem("Milk");
         groceryArray2[1] = new GroceryItem("Apples", "PRODUCE", 6);
@@ -38,9 +44,23 @@ public class Main {
         objectList.add(new GroceryItem("Butter"));
         objectList.add("Yogurt");
 
-
+        //notice the difference
         ArrayList<GroceryItem> groceryList = new ArrayList<>();
         groceryList.add(new GroceryItem("Butter"));
+        System.out.println(groceryList);//it works without calling Arrays.toString or deep method.
+
+
+        System.out.println();
+        System.out.println("-".repeat(15)+"ArrayList Part 2"+"-".repeat(15));
+
+        groceryList.add(new GroceryItem("Milk"));
+        groceryList.add(new GroceryItem("Oranges","PRODUCE",6));
+        //if you want a certain item at a certain index
+        groceryList.add(0,
+                new GroceryItem("Apples","PRODUCE", 4));
         System.out.println(groceryList);
+
+
+
     }
 }
