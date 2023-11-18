2816c5c595f04e25428bc0fac2e5432d291c1d5a
Umair Ali <umairmamoor@gmail.com>
2023-11-18 06:19:38 -0500

    Queue(s), Stack()s

--- List,ArrayList,LinkedList,Iterator,Autoboxing/LinkedListPart1/src/dev/lpa/Main.java
+++ List,ArrayList,LinkedList,Iterator,Autoboxing/LinkedListPart1/src/dev/lpa/Main.java
@@ -17,7 +17,10 @@ public class Main {
         System.out.println(placesToVisit);//[Canberra, Sydney]
 
         addMoreElements(placesToVisit); //passing placesToVisit list into addMoreElements();
-        System.out.println(placesToVisit); //[Darwin, Canberra, Sydney, Hobart]
+        System.out.println(placesToVisit); //[Brisbane, Darwin, Canberra, Sydney, Hobart, Melbourne, Toowoomba]
+
+        removeElements(placesToVisit);
+        System.out.println(placesToVisit);//[Alice Springs, Darwin, Canberra, Hobart, Melbourne, Toowoomba]
 
     }
 
@@ -25,6 +28,40 @@ public class Main {
 
         list.addFirst("Darwin");
         list.addLast("Hobart");
+
+        //Queue methods
+        list.offer("Melbourne");
+        /*
+        double ended queue
+         */
+        list.offerFirst("Brisbane");
+        list.offerLast("Toowoomba");
+
+        //stack methods
+        list.push("Alice Springs");//[Alice Springs, Brisbane, Darwin, Canberra, Sydney, Hobart, Melbourne, Toowoomba]
+
+    }
+
+    private static void removeElements(LinkedList<String> list){
+
+        list.remove(4);
+        list.remove("Brisbane");
+
+
+        //additional methods that are used to remove elements in LinkedList
+        //no argument remove method
+        System.out.println(list);
+        String s1 = list.remove(); //removes first element
+        System.out.println(s1 + " was removed"); //Alice Springs was removed
+
+        String s2 = list.removeFirst(); // also removes first element
+        System.out.println(s2 + " was removed"); //Darwin was removed
+
+        String s3 = list.removeLast(); // removes last element
+        System.out.println(s3 + " was removed");//Toowoomba was removed
+
+
+
     }
 
 
