96cc002b1296d4124c4704065bdc4a4358625fbf
Umair Ali <umairmamoor@gmail.com>
2023-12-03 11:53:19 -0500

    Interactive part of the program, scanner class, iteration and setting the cursor

---
+++ List,ArrayList,LinkedList,Iterator,Autoboxing/LinkedListChallenge/src/dev/lpa/InteractivePart.java
@@ -0,0 +1,29 @@
+/*
+Create an interactive part of your program, with the items shown below
+Available actions (select word or letter)
+(F)orward
+(B)ackward
+(L)ist Places
+(M)enu
+(Q)uit
+
+Use scanner class to get the input from the user. Use ListIterator to move back and forth in the List, through the itinerary
+ */
+
+package dev.lpa;
+
+public class InteractivePart {
+
+    public static void printMenu(){
+        System.out.println("""
+                Available actions (select word or letter):
+                (F)orward
+                (B)ackwards
+                (L)ist Places
+                (M)enu
+                (Q)uit""");
+    }
+
+
+
+}
--- List,ArrayList,LinkedList,Iterator,Autoboxing/LinkedListChallenge/src/dev/lpa/Main.java
+++ List,ArrayList,LinkedList,Iterator,Autoboxing/LinkedListChallenge/src/dev/lpa/Main.java
@@ -5,11 +5,13 @@ Create  type that has a town or place name, and a field or storing distance om t
 Next create  itinerary of places or town to visit
 
 
+
  */
 
 package dev.lpa;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 record Place (String name, int distance){
     @Override
@@ -30,6 +32,7 @@ public class Main {
 
         //checking if duplicates work
         addPlace(placesToVisit, new Place("new York", 406));
+        addPlace(placesToVisit, new Place("Virginia", 0));
         addPlace(placesToVisit, new Place("Maryland", 210));
         addPlace(placesToVisit, new Place("Delaware", 272));
         addPlace(placesToVisit, new Place("New Jersey", 374));
@@ -38,6 +41,73 @@ public class Main {
 
 
         System.out.println(placesToVisit); //[New York (406)]
+
+
+        var iterator = placesToVisit.listIterator();
+        Scanner scanner = new Scanner(System.in);
+        boolean quitLoop = false;
+        boolean forward = true;
+
+        InteractivePart.printMenu();
+
+        while(!quitLoop){
+
+            if(!iterator.hasPrevious()){ //printing the start of the list
+                System.out.println("Originating : " + iterator.next());
+                forward = true;
+            }
+
+            if(!iterator.hasNext()){ //printing the end of the list
+                System.out.println("Final :" + iterator.previous());
+                forward = false;
+            }
+            System.out.println("Enter Value: ");
+            String menuItem = scanner.nextLine().toUpperCase().substring(0,1); //(select word or letter):
+
+            switch(menuItem){
+
+                case "F": System.out.println("User wants to go forward");
+
+                            if(!forward){                //reversing direction
+                                forward = true;
+                                if(iterator.hasNext()){
+                                    iterator.next();     // Adjusting position forward
+                                }
+                            }
+                            if(iterator.hasNext()) {
+                                System.out.println(iterator.next());
+                            }
+                            break;
+
+                case "B":  System.out.println("User wants to go backwards");
+
+                            if(forward){                //reversing direction
+                               forward = false;
+                               if(iterator.hasPrevious()){
+                                    iterator.previous();     // Adjusting position backward
+                            }
+                        }
+
+                            if(iterator.hasPrevious()){
+                                System.out.println(iterator.previous());
+                            }
+
+                            break;
+
+                case "L": System.out.println(placesToVisit);
+
+                            break;
+
+                case "M": InteractivePart.printMenu();
+
+                            break;
+
+                default: quitLoop = true;
+
+                            break;
+            }
+        }
+
 
 
 
