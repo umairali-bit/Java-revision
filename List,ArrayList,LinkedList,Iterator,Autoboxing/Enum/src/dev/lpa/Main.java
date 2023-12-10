0e7e3b196229946d7d44deb1e65b57b001af0d21
Umair Ali <umairmamoor@gmail.com>
2023-12-10 07:53:22 -0500

    getRandomDay() of type DayOfTheWeek that returns random days and their order, also prints out a message if the day is Friday

--- List,ArrayList,LinkedList,Iterator,Autoboxing/Enum/src/dev/lpa/Main.java
+++ List,ArrayList,LinkedList,Iterator,Autoboxing/Enum/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -11,11 +13,30 @@ public class Main {
         System.out.printf("Name is %s, Ordinal Value = %d%n",
                 weekDay.name(),weekDay.ordinal());    // Name is TUES, Ordinal Value = 2
 
+        for(int i = 0; i<10; i++) {
+            weekDay = getRandomDay();
 
+            System.out.printf("Name is %s, Ordinal Value = %d%n",
+                    weekDay.name(),weekDay.ordinal());
+
+            if(weekDay == DayOfTheWeek.FRI) {
+                System.out.println("Found a Friday!!!");
+            }
+
+
+        }
 
     }
 
-    
+    // creating a method that returns a random day of the week
+
+    public static DayOfTheWeek getRandomDay() {
+
+        int randomInteger = new Random().nextInt(7);
+        var allDays = DayOfTheWeek.values();
+
+        return allDays[randomInteger];
+    }
 
 
 
