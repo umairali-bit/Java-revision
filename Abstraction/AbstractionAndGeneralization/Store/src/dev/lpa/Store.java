fb2950324079a3beff51783c3a5d5ce616286333
Umair Ali <umairmamoor@gmail.com>
2024-03-06 16:46:28 -0500

    Application 'Store', Store class as the main class, abstract ProductOfSale.java with two concrete methods and one abstract method

---
+++ Abstraction/AbstractionAndGeneralization/Store/src/dev/lpa/Store.java
@@ -0,0 +1,16 @@
+package dev.lpa;
+
+import java.util.ArrayList;
+
+public class Store {
+
+    public static void main(String[] args) {
+
+        ArrayList<ProductForSale> productsForSale = new ArrayList<ProductForSale>();
+
+    }
+
+
+
+
+}
