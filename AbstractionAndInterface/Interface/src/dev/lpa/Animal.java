7a2386ff6eb0151d28d580adb9fa6dbe5a840ba4
Umair Ali <umairmamoor@gmail.com>
2024-03-12 03:06:06 -0400

    Interface introduction, declaration, notes, a project with Main, Animal and Bird class. Bird class extends Animal and implements FlightLanded and Trackable interface. FlightLanded and Trackable interfaces have abstract methods

---
+++ AbstractionAndInterface/Interface/src/dev/lpa/Animal.java
@@ -0,0 +1,18 @@
+package dev.lpa;
+
+
+interface FlightEnabled{
+
+    void takeoff();
+    void land();
+    void fly();
+}
+
+interface Trackable{
+    void track();
+}
+
+public abstract class Animal {
+
+    public abstract void move();
+}
--- AbstractionAndInterface/Interface/src/dev/lpa/Main.java
+++ AbstractionAndInterface/Interface/src/dev/lpa/Main.java
@@ -1,4 +1,31 @@
 package dev.lpa;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //usual way to create an instance of a bird
+        Bird bird = new Bird();
+
+        /*
+        bird is an animal and is FlightEnabled and trackable
+         */
+
+        //assigning bird object to animal
+        Animal animal = bird;
+        FlightEnabled flier = bird;
+        Trackable tracked = bird;
+
+        animal.move();
+//        flier.move();
+//        tracked.move();
+
+        flier.takeoff();
+        flier.fly();
+        flier.land();
+        tracked.track();
+
+
+
+    }
 }
