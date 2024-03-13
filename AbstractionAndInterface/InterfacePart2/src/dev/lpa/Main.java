9073f3abe747baf069c37510adb36ef14c31c7a3
Umair Ali <umairmamoor@gmail.com>
2024-03-13 07:22:03 -0400

    continuing Interface project, added inFlight() with the parameter (Flightenabled flier). Called the method to the main method with passing the flier instance

---
+++ AbstractionAndInterface/InterfacePart2/src/dev/lpa/Main.java
@@ -0,0 +1,45 @@
+package dev.lpa;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //usual way to create an instance of a bird
+        Bird bird = new Bird();
+
+        /*
+        bird is an animal and is FlightEnabled and trackable
+         */
+
+        //assigning bird object to animal
+        Animal animal = bird;
+        FlightEnabled flier = bird;
+        Trackable tracked = bird;
+
+        animal.move();
+//        flier.move();
+//        tracked.move();
+
+//        flier.takeoff();
+//        flier.fly();
+//        flier.land();
+//        tracked.track();
+
+        inFlight(flier);
+    }
+
+    public static void inFlight(FlightEnabled flier){
+
+        flier.takeoff();
+        flier.fly();
+
+        if(flier instanceof Trackable tracked){
+            tracked.track();
+            flier.land();
+        }
+
+    }
+
+
+}
+
