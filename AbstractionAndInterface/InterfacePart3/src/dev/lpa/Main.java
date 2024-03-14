f67595d232a7b4da1b33ca5815cd92796ce641ed
Umair Ali <umairmamoor@gmail.com>
2024-03-14 08:25:49 -0400

    // ArrayList<FlightEnabled> fliers = new ArrayList<>();         // if the param in the method is ArrayList, we cannot pass a List or LinkedList. But we can pass a LinkedList to         // a List

--- AbstractionAndInterface/InterfacePart3/src/dev/lpa/Main.java
+++ AbstractionAndInterface/InterfacePart3/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Main {
@@ -39,38 +40,68 @@ public class Main {
         double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_Miles;
         System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled, milesTraveled);
 
+        System.out.println("-".repeat(30));
 
-        ArrayList<FlightEnabled> fliers = new ArrayList<>();
+
+        // ArrayList<FlightEnabled> fliers = new ArrayList<>();
+        // if the param in the method is ArrayList, we cannot pass a List or LinkedList. But we can pass a LinkedList to
+        // a List
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
         fliers.add(bird);
 
-        List<FlightEnabled> betterFliers = new ArrayList<>();
+        List<FlightEnabled> betterFliers = new LinkedList<>();
         betterFliers.add(bird);
 
+        System.out.println("LinkedList "+"-".repeat(30));
 
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
 
+        System.out.println("LinkedList "+"-".repeat(30));
 
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
 
 
     }
 
-    public static void inFlight(FlightEnabled flier){
+    public static void inFlight(FlightEnabled flier) {
 
         flier.takeoff();
         flier.fly();
 
-        if(flier instanceof Trackable tracked) {
+        if (flier instanceof Trackable tracked) {
             tracked.track();
         }
-            flier.land();
-        }
+        flier.land();
+    }
 
-        private static void triggerFliers(ArrayList<FlightEnabled> fliers){
+
+
+    private static void triggerFliers(List<FlightEnabled> fliers){
 
             for( var flier : fliers){
                 flier.takeoff();
 
         }
 
+    }
+    private static void flyFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.fly();
+
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers) {
+
+        for (var flier : fliers) {
+            flier.land();
+
+        }
     }
 
 
