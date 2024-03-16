e898478d20277d00c6e57d38829b0459dbf252b3
Umair Ali <umairmamoor@gmail.com>
2024-03-16 08:01:10 -0400

    A new method is needed called transition that uses the flight stages enum that we set up. This method is going to be in the FlightEnabled interface in the Animal class. This method is going to take a stage, one of the enum values, and return to another stage because it transitions from one stage to another. We can't use the abstract method since this method can break all the classes implementing that FlightEnabled interface. We are going to use the default method. We can get the run-time instance from this method.

--- AbstractionAndInterface/NewInInterfaces/src/dev/lpa/Animal.java
+++ AbstractionAndInterface/NewInInterfaces/src/dev/lpa/Animal.java
@@ -63,12 +63,22 @@ interface OrbitEarth extends FlightEnabled{
 
 interface FlightEnabled{
 
-    double Miles_To_KM = 1.60934;
+    double MILES_TO_KM = 1.60934;
     double KM_TO_Miles = 0.621371;
 
     void takeoff();
     void land();
     void fly();
+
+    //abstract method used in interface before JDK 8. This method will change every class that implements FlightEnabled Interface
+    // FlightStages transition(FlightStages stage);
+
+    //using default method to solve this problem
+    default FlightStages transition (FlightStages stage) {
+        System.out.println("transition not implemented on " + getClass().getName());
+        return null;
+    }
+
 }
 
 interface Trackable{
