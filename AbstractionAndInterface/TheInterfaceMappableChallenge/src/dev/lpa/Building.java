d85517d62a323db85aaee651bbb8ae1327332618
Umair Ali <umairmamoor@gmail.com>
2024-04-24 06:35:09 -0400

    Building class implements Mappable Interface and override interface abstract methods

--- AbstractionAndInterface/TheInterfaceMappableChallenge/src/dev/lpa/Building.java
+++ AbstractionAndInterface/TheInterfaceMappableChallenge/src/dev/lpa/Building.java
@@ -2,7 +2,7 @@ package dev.lpa;
 
 enum UsageType {ENTERTAINMENT, GOVERNMENT, RESIDENTIAL, SPORTS}
 
-public class Building {
+public class Building implements Mappable{
 
     private String name;
     private UsageType usage;
@@ -11,5 +11,28 @@ public class Building {
     public Building(String name, UsageType usage) {
         this.name = name;
         this.usage = usage;
+    }
+
+    @Override
+    public String getLabel() {
+        return name + "(" + usage + ")";    //it will print the usage of the building, entertainment, sports etc from enum
+    }
+
+    @Override
+    public Geometry getShape() {
+        return Geometry.POINT;      //Just need to return Point from Geometry enum
+    }
+
+    @Override
+    public String getMarker() {
+
+        //setup map icons determined by the building type both for their color and shape using enhanced switch statement
+        return switch (usage) {
+            case ENTERTAINMENT -> Color.GREEN + " " + PointMarker.TRIANGLE;
+            case GOVERNMENT -> Color.RED + " " + PointMarker.STAR;
+            case RESIDENTIAL -> Color.BLUE + " " + PointMarker.SQUARE;
+            case SPORTS -> Color.ORANGE + " " + PointMarker.PUSH_PIN;
+            default -> Color.BLACK + " " + PointMarker.CIRCLE;
+        };
     }
 }
