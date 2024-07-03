ab05d5306ee052635acbb1f1fbbf7bc6ee441d77
Umair Ali <umairmamoor@gmail.com>
2024-07-03 09:03:16 -0400

    Baseball application

--- Generics/GenericsPart1/src/dev/lpa/BaseballTeam.java
+++ Generics/GenericsPart1/src/dev/lpa/BaseballTeam.java
@@ -1,56 +1,60 @@
 package dev.lpa;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballTeam {
 
-    //practicing encapsulation
-
     private String teamName;
 
-    private List <BaseballPlayer> teamMembers= new ArrayList<>();
+    private List<BaseballPlayer> teamMembers = new ArrayList<>();
 
-    int totalWins = 0;
-    int totalLosses = 0;
-    int totalTies = 0;
+    private int totalWins = 0;
+    private int totalLoses = 0;
+    private int totalTies = 0;
+
 
     public BaseballTeam(String teamName) {
         this.teamName = teamName;
     }
 
-    public void addTeamMember (BaseballPlayer player) {
+    //Adding a teamMember
+    public void addTeamMember(BaseballPlayer player) {
 
         if(!teamMembers.contains(player)) {
             teamMembers.add(player);
+
         }
     }
 
-    public void listTeamMembers () {
 
-        System.out.println(teamName + " Roster: ");
+    //Listing team members
+
+    public void ListTeamMembers() {
+        System.out.println(teamName + "Roster");
         System.out.println(teamMembers);
+    }
+
+
+    public int ranking() {
+        return (totalLoses * 2) + totalTies + 1;
 
     }
 
-    //if there are no losses, no ties the team will be ranked 1. The ranked first team cannot be higher than 1
-    public int ranking () {
-        return (totalLosses * 2) + totalTies + 1;
-    }
+    public String setScore(int ourScore, int theirScore) {
 
-    //check the wins
-    public String setScore (int ourScore, int theirScore) {
-         String message = "Lost to:";
-         if(ourScore > theirScore) {
-             totalWins++;
-             message = "beat";
-         } else if (ourScore == theirScore) {
-             totalTies++;
-             message = "tied";
-         } else {
-             totalLosses++;
-         }
-         return message;
+        String message = "lost to";
+        if(ourScore > theirScore) {
+            totalWins++;
+            message = "beat";
+        } else if (ourScore == theirScore) {
+            totalTies++;
+            message = "tied";
+        } else {
+            totalLoses++;
+        }
+        return message;
 
     }
 
@@ -59,3 +63,5 @@ public class BaseballTeam {
         return teamName + " (Ranked " + ranking() + ")";
     }
 }
+
+
--- Generics/GenericsPart1/src/dev/lpa/Main.java
+++ Generics/GenericsPart1/src/dev/lpa/Main.java
@@ -1,34 +1,42 @@
 package dev.lpa;
 
-// creating a record with two fields. Name and postion.
-record BaseballPlayer(String name, String position) {}
+
+//creating a baseball player record with only two fields name and position
+record BaseballPlayer(String name, String position){}
 
 public class Main {
 
     public static void main(String[] args) {
 
-        BaseballTeam Phillies = new BaseballTeam("Phillies");
-        BaseballTeam Houston = new BaseballTeam("Houston");
-        scoreResult(Phillies, 3, Houston, 5);
 
-        //adding some players to the baseball team
-        var Harper = new BaseballPlayer("B Harper", "Right Fielder");
-        var Marsh = new BaseballPlayer("B Marsh", "Right Fielder");
+        //creating two baseball teams
 
-        Phillies.addTeamMember(Harper);
-        Phillies.addTeamMember(Marsh);
-        Phillies.listTeamMembers();
+        BaseballTeam phillies = new BaseballTeam("Philadelphia Phillies");
+        BaseballTeam astros = new BaseballTeam("Houston Astros");
+        scoreResult(phillies, 3, astros, 5 );
+
+        //creating new team members
+        var harper = new BaseballPlayer("B Harper", "Right Fielder");
+        var marsh = new BaseballPlayer("B Marsh", "Right Fielder");
+
+        //adding new members to the team
+        phillies.addTeamMember(harper);
+        phillies.addTeamMember(marsh);
+
+        //listing players in the team
+        phillies.ListTeamMembers();
 
 
 
     }
 
-    public static void scoreResult (BaseballTeam team1, int t1_score,
+    public static void scoreResult (BaseballTeam team1 , int t1_score,
                                     BaseballTeam team2, int t2_score) {
 
         String message = team1.setScore(t1_score, t2_score);
         team2.setScore(t2_score, t1_score);
         System.out.printf("%s %s %s %n", team1, message, team2);
+
     }
 
 }
