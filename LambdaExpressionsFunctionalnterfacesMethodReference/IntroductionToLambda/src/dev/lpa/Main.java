31b998b701a8ca89d08bb9986d373c6b09c3b6ba
Umair Ali <umairmamoor@gmail.com>
2025-01-01 14:15:49 -0500

    Introduction to Functional Interface

--- LambdaExpressionsFunctionalnterfacesMethodReference/IntroductionToLambda/src/dev/lpa/Main.java
+++ LambdaExpressionsFunctionalnterfacesMethodReference/IntroductionToLambda/src/dev/lpa/Main.java
@@ -22,11 +22,13 @@ public class Main {
 
         //creating a list of 'Person' records
         List<Person> people = new ArrayList<>(Arrays.asList(
-                new Main.Person ("Harry", "Potter"), //we can use records as static
-                new Person ("Hermoine" ,"Granger"),
-                new Person ("Darco", "Malfoy"),
-                new Person ("Lord", "Voldermort"),
-                new Person ("Ron", "Weasley")));
+                new Main.Person ("Sally", "Brown"), //we can use records as static
+                new Person ("Peppermint", "Patty"),
+                new Person ("Linus", "Van Pelt"),
+                new Person ("Charlie" ,"Brown"),
+                new Person ("Lucy", "Van Pelt")
+        ));
+
 
         //implementing a comparator method using anonymous class
 
@@ -39,7 +41,34 @@ public class Main {
         };
 
         //executing sorting list
+        //applying Lambda
         people.sort((o1, o2) -> o1.lastName().compareTo(o2.lastName()));
+        System.out.println(people); //same last name is going to be according to insertion order
+
+        //creating an interface that extends comparator
+        interface EnhancedComparator <T> extends Comparator<T> {
+
+            //abstract method
+            int secondLevel (T o1, T o2);
+        }
+
+        //creating an anonymous class
+
+        var comparatorMixed = new EnhancedComparator<Person>() {
+
+            @Override
+            public int compare(Person o1, Person o2) {
+                int result = o1.lastName().compareTo(o2.lastName());
+                return (result == 0 ? secondLevel(o1,o2) : result);
+            }
+
+            @Override
+            public int secondLevel(Person o1, Person o2) {
+                return o1.firstName().compareTo(o2.firstName());
+            }
+        };
+
+        people.sort(comparatorMixed);
         System.out.println(people);
 
 
