f8154a067f2c0d4be0b98f7072c61f36f46d0d37
Umair Ali <umairmamoor@gmail.com>
2025-01-06 21:37:36 -0500

    method reference in constructors

--- LambdaExpressionsFunctionalnterfacesMethodReference/MethodReferenceIntrodunction/src/dev/lpa/Main.java
+++ LambdaExpressionsFunctionalnterfacesMethodReference/MethodReferenceIntrodunction/src/dev/lpa/Main.java
@@ -1,8 +1,24 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
+import java.util.function.Supplier;
+
+
+class PlainOld {
+
+    private static int last_id = 1;
+
+    private int id;
+
+    public PlainOld() {
+
+        id = PlainOld.last_id++;
+        System.out.println("Creating a PlainOld Object: id = " + id );
+    }
+}
 
 public class Main {
 
@@ -18,11 +34,27 @@ public class Main {
 
         calculator(Integer::sum, 10,25 );
         calculator(Double::sum, 10.23, 10.90);
+
+        Supplier<PlainOld> reference1 = PlainOld::new;
+        PlainOld newPojo = reference1.get();
+
+        System.out.println("Getting Array");
+        PlainOld[] pojo2 = seedArray(() -> new PlainOld(), 10);
+        PlainOld[] pojo1 = seedArray(PlainOld::new, 10);
+
     }
 
     private static <T> void calculator(BinaryOperator<T> func, T value1, T value2) {
 
         T result = func.apply(value1, value2);
         System.out.println("Result of operation: " + result);
+    }
+
+    private static PlainOld[] seedArray(Supplier<PlainOld> reference, int count) {
+
+        PlainOld[] array = new PlainOld[count];
+        Arrays.setAll(array, i -> reference.get());
+        return array;
+
     }
 }
