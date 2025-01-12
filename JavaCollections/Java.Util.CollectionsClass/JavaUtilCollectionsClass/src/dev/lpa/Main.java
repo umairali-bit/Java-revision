03413228bc9ae12d8d51e1d5720fabbe7d6270f3
Umair Ali <umairmamoor@gmail.com>
2025-01-11 22:29:16 -0500

    comparing array methods and collections methods, using nCopies() to populate a list in collections

--- JavaCollections/Java.Util.CollectionsClass/JavaUtilCollectionsClass/src/dev/lpa/Main.java
+++ JavaCollections/Java.Util.CollectionsClass/JavaUtilCollectionsClass/src/dev/lpa/Main.java
@@ -32,6 +32,9 @@ ASCII - CLUB = 9827, DIAMOND = 9830, HEART = 9829, SPADE = 9824
 
  */
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -41,6 +44,26 @@ public class Main {
 
         List<Card> deck = Card.getStandardDeck();
         Card.printDeck(deck);
+
+        //using fill method on Array
+        Card[] cardArray = new Card[13];
+        Card aceOfHearts = Card.getFaceCard(Card.Suit.HEART,'A');
+        Arrays.fill(cardArray,aceOfHearts);
+        Card.printDeck(Arrays.asList(cardArray),"Aces of Hearts", 1);
+
+
+        //methods on collection class for populating a list
+
+        List<Card> cards = new ArrayList<>(52);
+        Collections.fill(cards, aceOfHearts);// .fill method on collection class cannot add elements. It would replace the
+        // element with the element passed to the method
+        System.out.println(cards);
+        System.out.println("card.size() = " + cards.size());
+
+        //to populate the list, collection class has nCopies()
+        List<Card> acesOfHearts = Collections.nCopies(13,aceOfHearts);
+        Card.printDeck(acesOfHearts, "Aces of Hearts", 1);
+
 
     }
 }
