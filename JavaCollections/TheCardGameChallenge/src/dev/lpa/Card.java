edaebee1dbba5c3bdb72d65578184aeec798bb0c
Umair Ali <umairmamoor@gmail.com>
2025-01-16 19:58:53 -0500

    getStandardDeck()

--- JavaCollections/TheCardGameChallenge/src/dev/lpa/Card.java
+++ JavaCollections/TheCardGameChallenge/src/dev/lpa/Card.java
@@ -1,5 +1,8 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public record Card(Suit suit, String face, int rank) {
 
     public enum Suit {
@@ -25,6 +28,31 @@ public record Card(Suit suit, String face, int rank) {
         }
         System.out.println("Invalid Numeric Card selected");
         return null;
+    }
+
+    public static Card getFaceCard (Suit suit, char abbrev) {
+
+        int charIndex = "JQKA".indexOf(abbrev);
+        if (charIndex > 1) {
+            return new Card(suit, "" + abbrev, charIndex + 9);
+        }
+        System.out.println("Invalid face card selected");
+        return null;
+
+    }
+
+    public static List<Card> getStandardDec() {
+
+        List<Card> deck = new ArrayList<>(52);
+        for (Suit suit : Suit.values()) {
+            for(int i = 2; i<= 10; i++){
+                deck.add(getNumericCard(suit,i));
+            }
+            for (char c : new char[]{'J','Q', 'K','A'}){
+                deck.add(getFaceCard(suit, c));
+            }
+        }
+        return deck;
     }
 
 
