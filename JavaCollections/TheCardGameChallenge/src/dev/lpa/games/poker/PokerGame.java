5962fcee5edeaf0510244b7cd893576fa82d377b
Umair Ali <umairmamoor@gmail.com>
2025-01-17 10:44:21 -0500

    PokerGame class, with the constructor

---
+++ JavaCollections/TheCardGameChallenge/src/dev/lpa/games/poker/PokerGame.java
@@ -0,0 +1,34 @@
+package dev.lpa.games.poker;
+
+import dev.lpa.Card;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class PokerGame {
+
+    //standard deck
+    private final List<Card> deck = Card.getStandardDeck();
+
+    private int playerCount;
+
+    private int cardsInHand;
+
+    private List<PokerHand> pokerHands;
+
+    private List<Card> remainingCards;
+
+    public PokerGame(int playerCount, int cardsInHand) {
+        this.playerCount = playerCount;
+        this.cardsInHand = cardsInHand;
+        pokerHands = new ArrayList<>(cardsInHand);// setting the capacity to the value contained in cardsInHand
+    }
+
+
+
+
+
+
+
+}
