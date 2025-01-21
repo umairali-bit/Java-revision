d500a8cacbf8b878ab12e3874ebddcc1d552f885
Umair Ali <umairmamoor@gmail.com>
2025-01-21 09:43:44 -0500

    first hasSet of the course

---
+++ JavaCollections/UnderstandingTheImportanceOfTheHashcode/hashing/src/dev/lpa/Main.java
@@ -0,0 +1,43 @@
+package dev.lpa;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        String aText = "Hello";
+        String bText = "Hello";
+        String cText = String.join("l","He","lo");
+        String dText = "He".concat("llo");
+        String eText = "hello";
+
+
+        List<String> hellos = Arrays.asList(
+                aText,bText,cText,dText,eText
+        );
+
+        hellos.forEach(s -> System.out.println(s + ": " + s.hashCode()));
+
+        /*
+        Hello: 69609650
+        Hello: 69609650
+        Hello: 69609650
+        Hello: 69609650
+        hello: 99162322
+         */
+
+
+        //hashSet
+        //hashSet is a class that implements the Set interface and track duplicates by their hascode
+        Set<String> mySet = new HashSet<>(hellos);
+        System.out.println("mySet = " + mySet);
+        System.out.println("# of elements = " + mySet.size());
+
+        /*
+        mySet = [Hello, hello]
+        # of elements = 2
+         */
+    }
+}
