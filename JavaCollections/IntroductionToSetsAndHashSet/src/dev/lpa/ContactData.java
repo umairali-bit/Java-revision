406f45c058d882f0f4efd81e0a99103a84c46325
Umair Ali <umairmamoor@gmail.com>
2025-01-22 16:21:40 -0500

    ContactData class with two final String variables phone Data and emailData

---
+++ JavaCollections/IntroductionToSetsAndHashSet/src/dev/lpa/ContactData.java
@@ -0,0 +1,27 @@
+package dev.lpa;
+
+public class ContactData {
+
+    private static final String phoneData = """
+            Charlie Brown, 3334445555
+            Maid Marion, 1234567890
+            Mickey Mouse, 9998887777
+            Mickey Mouse, 1247489758
+            Minnie Mouse, 4567805666
+            Robin Hood, 5647893000
+            Robin Hood, 7899028222
+            Lucy Van Pelt, 5642086852
+            Mickey Mouse, 9998887777
+           """;
+
+
+
+    private static  final String emailData = """
+            Mickey Mouse, mckmouse@gmail.com
+            Mickey Mouse, micky1@aws.com
+            Minnie Mouse, minnie@verizon.net
+            Robin Hood, rhood@gmail.com
+            Linus Van Pelt, lvpelt2015@gmail.com
+            Daffy Duck, daffy@google.com
+            """;
+}
