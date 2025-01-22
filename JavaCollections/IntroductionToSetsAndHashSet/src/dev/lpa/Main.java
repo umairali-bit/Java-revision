a93b7450f75de56d619c2f09c6f585fab382c4ca
Umair Ali <umairmamoor@gmail.com>
2025-01-21 21:40:31 -0500

    Contact class

---
+++ JavaCollections/IntroductionToSetsAndHashSet/src/dev/lpa/Main.java
@@ -0,0 +1,14 @@
+package dev.lpa;
+
+/*
+Create a Contact Class that has fields, name, a String, emails, a HashSet of String, and phones, another HashSet of String
+4 - constructors. The first just takes a name. The second should have a name, and a single email of type String, The thrid constructor
+should also have two arguments name, and the second parameter being a long, which represents a 10-digit phone number.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
