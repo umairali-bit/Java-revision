016ce7e8f12057193c483351cbdb2c163adbeb7e
Umair Ali <umairmamoor@gmail.com>
2025-01-23 11:47:45 -0500

    TaskClass

---
+++ JavaCollections/SetOperationChallengeTASK/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+/*
+Create a class that represents a Task. It should have an assignee, a project name, a task description, a status, a priority,
+High, Low or Medium. Each of these attributes should be editable.
+
+A task is uniquely identified by its project name and description. The task should implement Comparable, so that tasks
+are sorted  by project name and description.
+
+
+
+ */
+
+    public static void main(String[] args) {
+
+
+    }
+}
