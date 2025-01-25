871954cae82d5dc0090716f15ea7a2d5ac46f794
Umair Ali <umairmamoor@gmail.com>
2025-01-25 11:09:14 -0500

    sort() on Tree

--- JavaCollections/LInkedHashSetAndTreeSet/TreeSet/src/dev/lpa/TreeSetMain.java
+++ JavaCollections/LInkedHashSetAndTreeSet/TreeSet/src/dev/lpa/TreeSetMain.java
@@ -7,7 +7,7 @@ public class TreeSetMain {
     public static void main(String[] args) {
 
         List<Contact> phones = ContactData.getData("phone");
-        List<Contact> emails = ContactData.getData("emails");
+        List<Contact> emails = ContactData.getData("email");
 
 
         Comparator<Contact> mySort = Comparator.comparing(Contact::getName);
@@ -21,10 +21,21 @@ public class TreeSetMain {
         phones.forEach(c -> justNames.add(c.getName()));
         System.out.println(justNames);
 
-        //we can also pass a sorted set in the TreeSet constructor
+
+        //The fullSet set has the sorted mechanism of TreeSet passed to the constructor
         NavigableSet<Contact> fullSet = new TreeSet<>(sorted);
         fullSet.addAll(emails);
         fullSet.forEach(System.out::println);
+
+        //There is a method on the SortedSet Interface that returns a comparator "sorted" used in the set
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+        fullList.sort(sorted.comparator());
+        System.out.println("-".repeat(10));
+//        for (Contact contact : fullList) {
+//            System.out.println(contact);
+//        }
+        fullList.forEach(System.out::println);
 
     }
 }
