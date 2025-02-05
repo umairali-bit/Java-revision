94cd313a5e5ceb8dcbd6e31735bd8fccc03072ac
Umair Ali <umairmamoor@gmail.com>
2025-02-05 12:43:27 -0500

    compute and computeIfAbsent

---
+++ JavaCollections/MapFunctionalityComputeReplaceRemove/src/src/src/dev/lpa/Main.java
@@ -0,0 +1,83 @@
+package src.src.dev.lpa;
+
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Contact> emails = ContactData.getData("email");
+        List<Contact> phones = ContactData.getData("phone");
+        printData("Phone list", phones);
+        printData("Email List", emails);
+
+        //got a list of names with phone numbers with mobile phone contacts and another list with email contacts
+        //combine these contacts, merging any duplicates into a single contact, with multiple emails and phone numbers on a single record
+
+        //creating two hashsets
+        Set<Contact> emailContacts = new HashSet<>(emails);
+        Set<Contact> phoneContacts = new HashSet<>(phones);
+
+        printData("Phone Contacts",phoneContacts);
+        printData("Email Contacts",emailContacts);
+
+        int index = emails.indexOf(new Contact("Robin Hood"));
+        Contact robinHood = emails.get(index);
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.addEmail("Sherwood Forest");
+        robinHood.replaceEmailIfExists("RHood@sherwoodforest.com", "RHood@sherwoodforest.org");
+        System.out.println(robinHood);
+
+        // to perform a union we can use BulkAll()
+        //it will return elements of both of the sets
+        Set<Contact> unionAB = new HashSet<>();
+        unionAB.addAll(emailContacts);
+        unionAB.addAll(phoneContacts);
+        printData("(A \u222A B) Union of emails (A) with phones (B)", unionAB);
+
+        //intersection of two or more sets will return only the elements the sets have common.
+        Set<Contact> intersectAB = new HashSet<>(emailContacts);
+        intersectAB.retainAll(phoneContacts);
+        printData("(A \u2229 B) intersects emails (A) with phones (B)", intersectAB);
+
+        // to change the order of intersection of the sets
+        Set<Contact> intersectBA = new HashSet<>(phoneContacts);
+        intersectBA.retainAll(emailContacts);
+        printData("(B \u2229 A) intersects emails (B) with phones (A)", intersectBA);
+
+
+        //Asymmetric Sets, A - B
+        Set<Contact> AMinusB = new HashSet<>(emailContacts);
+        AMinusB.removeAll(phoneContacts);
+        printData("(A - B) emails (A) - phones (B)", AMinusB);
+
+        //Asymmetric Sets, B - A
+        Set<Contact> BMinusA = new HashSet<>(phoneContacts);
+        BMinusA.removeAll(emailContacts);
+        printData("(B - A) phones (B) - email (A)", BMinusA);
+
+        //symmetric difference in both of the sets
+        Set<Contact> symmetricDiff = new HashSet<>(AMinusB);
+        symmetricDiff.addAll(BMinusA);
+        printData("Symmetric Difference: phones and emails", symmetricDiff);
+
+        //the other way to get symmetric differences
+        Set<Contact> symmetricDiff2 = new HashSet<>(unionAB);
+        symmetricDiff2.removeAll(intersectAB);
+        printData("Symmetric Difference: Phones and emails", symmetricDiff2);
+
+
+
+    }
+
+    public static void printData (String header, Collection<Contact> contacts) {
+        System.out.println("-".repeat(40));
+        System.out.println(header);
+        System.out.println("-".repeat(40));
+        contacts.forEach(System.out::println);
+    }
+}
---
+++ JavaCollections/MapFunctionalityComputeReplaceRemove/src/src/src/dev/lpa/MapMain.java
@@ -0,0 +1,126 @@
+package src.src.dev.lpa;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapMain {
+
+
+    public static void main(String[] args) {
+
+        List<Contact> phones = ContactData.getData("phone");
+        List<Contact> emails = ContactData.getData("email");
+
+
+        List<Contact> fullList = new ArrayList<>(phones);
+        fullList.addAll(emails);
+
+        fullList.forEach(System.out::println);
+        System.out.println("-".repeat(50));
+
+
+        Map<String, Contact> contacts = new HashMap<>();
+
+        for (Contact contact : fullList) {
+            contacts.put(contact.getName(), contact);
+
+        }
+         contacts.forEach((k, v) -> System.out.println("key = " + k + ", value = " + v));
+
+        System.out.println("-".repeat(50));
+
+        System.out.println(contacts.get("Charlie Brown"));
+
+        System.out.println(contacts.get("chuck brown"));
+
+
+        Contact defaultContact = new Contact("Chuck Brown");
+        System.out.println(contacts.getOrDefault("Chuck Brown", defaultContact));
+
+        System.out.println("-".repeat(50));
+        contacts.clear();
+
+        for (Contact contact : fullList) {
+            Contact duplicate = contacts.put(contact.getName(), contact);
+
+            if (duplicate != null) {
+//                System.out.println("duplicate = " + duplicate);
+//                System.out.println("current = " + contact);
+                contacts.put(contact.getName(), contact.mergeContactData(duplicate));
+
+
+
+            }
+
+
+        }
+
+        contacts.forEach((k, v) -> System.out.println("key = " + k + ", value = " + v));
+        System.out.println("-".repeat(50));
+        contacts.clear();
+
+
+
+        for (Contact contact : fullList) {
+            contacts.putIfAbsent(contact.getName(), contact);
+
+        }
+        contacts.forEach((k, v) -> System.out.println("key = " + k + ", value = " + v));
+
+
+        System.out.println("-".repeat(50));
+        contacts.clear();
+
+
+
+        for (Contact contact : fullList) {
+            Contact duplicate = contacts.putIfAbsent(contact.getName(), contact);
+
+            if (duplicate != null) {
+                contacts.put(contact.getName(), contact.mergeContactData(duplicate));
+            }
+
+        }
+        contacts.forEach((k, v) -> System.out.println("key = " + k + ", value = " + v));
+
+        //The merge method, it still takes a key and value but third parameter us a BiFunction interface
+        System.out.println("-".repeat(50));
+        contacts.clear();
+        fullList.forEach(contact -> contacts.merge(contact.getName(), contact,
+                Contact::mergeContactData //lets you merge a bunch of contacts from an ArrayList into a hashmap with a single statement.
+
+                // OR
+
+        //        (previous, current) ->  previous.mergeContactData(current)
+
+                //OR
+        // {
+                    //System.out.println("prev: " + previous + " : current " + current);
+                    //Contact merged = previous.mergeContactData(current);
+                    //System.out.println("merged: " + merged);
+
+                    //return merged;
+    //}
+
+                ));
+        contacts.forEach((k, v) -> System.out.println("key = " + k + ", value = " + v));
+
+        System.out.println("-".repeat(50));
+
+        for (String contactName : new String[] {"Daisy Duck", "Daffy Duck", "Scrooge McDuck"}) {
+            //contacts.compute(contactName, (k,v) -> new Contact(k));
+            contacts.computeIfAbsent(contactName, (k) -> new Contact(k));
+        }
+
+        contacts.forEach((k,v) -> System.out.println("key=" + k + " , value= " + v));
+
+
+
+
+
+
+
+    }
+}
