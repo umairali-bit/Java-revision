ca3403fb0ccb85fbd99b1aca70a85958bc5b082f
Umair Ali <umairmamoor@gmail.com>
2025-02-14 06:13:04 -0500

    data from svc file, under a final constant

---
+++ JavaCollections/AdventureMap/src/GameInsights.java
@@ -0,0 +1,21 @@
+public class GameInsights {
+
+    /*
+    The game starts with the user standing in a road, in the center of the map.
+North would take the user to the Forest and South would take the user to the Valley. East would be the Well House, and West would be the Hill.
+
+You'll want to use two HashMaps in this challenge, one for the board locations, and one for the next places data.
+
+Use a HashMap for the game board locations, keyed on a short descriptor, road or stream, for example, or some other key of your choice.
+Your Location class should have a location description, and could also have a field, a HashMap, to store next places to go from there.
+
+
+Use the console to describe to the player what their current location is, starting at the road, and show the player what options they have to go from here.
+Prompt the player to enter the direction they want to go next.
+Continue to play until the user quits, perhaps using Q for quit.
+
+
+A bonus part of the challenge is to allow customizations to the board locations, as well as the next place directions.
+
+     */
+}
---
+++ JavaCollections/AdventureMap/src/dev/lpa/AdventureGame.java
@@ -0,0 +1,35 @@
+package dev.lpa;
+
+
+
+public class AdventureGame {
+
+
+    public static final String GAME_LOCATIONS = """
+            road	at the end of the road	 W: hill	 E:well house	S:valley	N:forest
+            hill	on top of hill with a view in all directions	N:forest	 E:road		
+            well house	inside a well house for a small spring	W:road	N:lake	S:stream	
+            valley	in a forest valley beside a tumbling stream	N:road	W:hill	E:stream	
+            forest	at the edge of a thick dark forest	S:road	E:lake		
+            lake	by an alpine lake surrounded by wildflowers	W:forest	S:well house		
+            stream	near a stream with a rocky bed	W:valley	 N:well house		
+            """;
+
+    private enum Compass {
+
+        E, N, S, W;
+
+      private static String[] directions = {"East", "North", "South", "West"};
+
+      private String getString() {
+          return directions [this.ordinal()]; //this will take E as East, N as North, S as South and W as West
+
+        }
+      }
+
+      
+
+    }
+
+
+
