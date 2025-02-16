2199f3258019bb09394e0b7ac89f1a4b85d89670
Umair Ali <umairmamoor@gmail.com>
2025-02-16 07:03:09 -0500

    firing up the game

--- JavaCollections/AdventureMap/src/dev/lpa/Main.java
+++ JavaCollections/AdventureMap/src/dev/lpa/Main.java
@@ -1,10 +1,19 @@
 package dev.lpa;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         AdventureGame game = new AdventureGame();
+        game.play("road");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            
+        }
 
     }
 }
