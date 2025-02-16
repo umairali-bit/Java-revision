05312cdcde91bd690e346621ee96bc35ee52397d
Umair Ali <umairmamoor@gmail.com>
2025-02-16 06:33:24 -0500

    code for load the game data

--- JavaCollections/AdventureMap/src/dev/lpa/AdventureGame.java
+++ JavaCollections/AdventureMap/src/dev/lpa/AdventureGame.java
@@ -10,13 +10,13 @@ public class AdventureGame {
 
 
     public static final String GAME_LOCATIONS = """
-            road	at the end of the road	 W: hill	 E:well house	S:valley	N:forest
-            hill	on top of hill with a view in all directions	N:forest	 E:road		
-            well house	inside a well house for a small spring	W:road	N:lake	S:stream	
-            valley	in a forest valley beside a tumbling stream	N:road	W:hill	E:stream	
-            forest	at the edge of a thick dark forest	S:road	E:lake		
-            lake	by an alpine lake surrounded by wildflowers	W:forest	S:well house		
-            stream	near a stream with a rocky bed	W:valley	 N:well house		
+            road,at the end of the road,W: hill,E:well house,S:valley,N:forest
+            hill,on top of hill with a view in all directions,N:forest,E:road		
+            well house,inside a well house for a small spring,W:road,N:lake,S:stream	
+            valley,in a forest valley beside a tumbling stream,N:road,W:hill,E:stream	
+            forest,at the edge of a thick dark forest,S:road,E:lake		
+            lake,by an alpine lake surrounded by wildflowers,W:forest,S:well house		
+            stream,near a stream with a rocky bed,W:valley, N:well house				
             """;
 
     private enum Compass {
@@ -43,10 +43,17 @@ public class AdventureGame {
     private Map<String, Location> adventureMap = new HashMap<>();
 
     public AdventureGame() {
+        this(null);
+
     }
 
     //to have the players the ability to customize the map
     public AdventureGame(String customLocations) {
+        loadLocations(GAME_LOCATIONS);
+
+        if (customLocations != null) {
+            loadLocations(customLocations);
+        }
     }
 
     private void loadLocations(String data) {
