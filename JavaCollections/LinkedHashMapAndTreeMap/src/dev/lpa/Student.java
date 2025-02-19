70859056d5c0e58fbed22423856c26dbc031d773
Umair Ali <umairmamoor@gmail.com>
2025-02-19 06:59:55 -0500

    method to return date

--- JavaCollections/LinkedHashMapAndTreeMap/src/dev/lpa/Student.java
+++ JavaCollections/LinkedHashMapAndTreeMap/src/dev/lpa/Student.java
@@ -1,8 +1,15 @@
 package dev.lpa;
 
+import java.time.LocalDate;
+
 record Course (String courseId, String name, String subject) {}
 
-record purchase (String courseId, int studentId, double price, int yr, int dayOfYear) {}
+record purchase (String courseId, int studentId, double price, int yr, int dayOfYear) {
+
+    public LocalDate purchaseDate() {
+        return LocalDate.ofYearDay(yr, dayOfYear);
+    }
+}
 
 public class Student {
 
