b65ee4d9c3a9f9da47ebf4e298e222d5be543dfc
Umair Ali <umairmamoor@gmail.com>
2025-02-19 07:04:26 -0500

    fields in Student class

--- JavaCollections/LinkedHashMapAndTreeMap/src/dev/lpa/Student.java
+++ JavaCollections/LinkedHashMapAndTreeMap/src/dev/lpa/Student.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.time.LocalDate;
+import java.util.List;
 
 record Course (String courseId, String name, String subject) {}
 
@@ -12,6 +13,15 @@ record purchase (String courseId, int studentId, double price, int yr, int dayOf
 }
 
 public class Student {
+
+    public static int lastId = 1;
+
+    private String name;
+    private int id;
+    private List<Course> courseList;
+    private int serialNo;
+
+    
 
 
 }
