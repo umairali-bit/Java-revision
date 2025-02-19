dcb5ce295d53bb3bcc98cc12e25b79e958686971
Umair Ali <umairmamoor@gmail.com>
2025-02-19 06:58:52 -0500

    student class with two records

--- JavaCollections/LinkedHashMapAndTreeMap/src/dev/lpa/Student.java
+++ JavaCollections/LinkedHashMapAndTreeMap/src/dev/lpa/Student.java
@@ -1,6 +1,10 @@
 package dev.lpa;
 
+record Course (String courseId, String name, String subject) {}
+
+record purchase (String courseId, int studentId, double price, int yr, int dayOfYear) {}
+
 public class Student {
 
-    
+
 }
