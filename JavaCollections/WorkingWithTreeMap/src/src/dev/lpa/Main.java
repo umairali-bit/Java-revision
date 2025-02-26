063ba3d2d70b39f8ea523327118f18f6052716cb
Umair Ali <umairmamoor@gmail.com>
2025-02-26 06:50:24 -0500

    establishing the date

---
+++ JavaCollections/WorkingWithTreeMap/src/src/dev/lpa/Main.java
@@ -0,0 +1,90 @@
+package src.dev.lpa;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
+
+public class Main {
+
+    private static Map<String, Purchase> purchases = new LinkedHashMap<>();
+    private static NavigableMap<String, Student> students = new TreeMap<>();
+    private static final AtomicInteger counter = new AtomicInteger(0);
+    public static Supplier<Integer> generateSerialNumber = () -> counter.incrementAndGet();
+
+
+
+    public static void main(String[] args) {
+
+        Course jmc = new Course("jmc101", "Java Master Class", "Java");
+        Course python = new Course ("pyt101", "Python Master Class", "Python");
+
+
+        addPurchase("Marty Martin", jmc, 129.99);
+        addPurchase("AndyMartin", jmc, 139.99);
+        addPurchase("Marty Martin", python, 149.99);
+        addPurchase("Joe Jones", jmc, 149.99);
+        addPurchase("Bill Brown", python, 119.99);
+
+        addPurchase("Chuck Cheese", python, 119.99);
+        addPurchase("Dave Jones", jmc, 139.99);
+        addPurchase("Eva East", python, 139.99);
+        addPurchase("Fred Forker", jmc, 139.99);
+        addPurchase("Greg Brady", python, 129.99);
+
+        purchases.forEach((key, value) -> System.out.println(key + ": " + value));
+        System.out.println("------------------");
+
+        students.forEach((key, value) -> System.out.println(generateSerialNumber.get() + ". " + key + ": " + value));
+
+
+
+        NavigableMap<LocalDate,List<Purchase>> datedPurchases = new TreeMap<>();
+
+        for (Purchase p : purchases.values()) {
+            datedPurchases.compute(p.purchaseDate(),
+                    (pdate, plist) -> {
+                        List<Purchase> list =
+                                (plist == null) ? new ArrayList<>() : plist;
+                        list.add(p);
+                        return list;
+
+                    });
+
+
+        }
+        datedPurchases.forEach((key, value) -> System.out.println(key + ": " + value));
+
+        int currentYear = LocalDate.now().getYear();
+
+        LocalDate firstDay = LocalDate.ofYearDay(currentYear, 1);
+        LocalDate week1 = firstDay.plusDays(7);
+        
+
+
+
+
+
+
+    }
+
+    private static void addPurchase(String name, Course course, double price) {
+
+        Student existingStudent = students.get(name);
+
+        if (existingStudent == null) {
+            existingStudent = new Student (name, course);
+            students.put(name, existingStudent);
+        } else {
+            existingStudent.addCourse(course);
+        }
+
+        int day = new Random().nextInt(1,15);
+        String key = course.courseId() + "_" + existingStudent.getId();
+        int year = LocalDate.now().getYear();
+        Purchase purchase = new Purchase(course.courseId(), existingStudent.getId(), price, year, day);
+        purchases.put(key, purchase);
+
+    }
+
+}
