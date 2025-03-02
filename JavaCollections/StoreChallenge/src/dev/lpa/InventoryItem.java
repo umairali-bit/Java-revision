051855b4671f47537cc20386f428a3955da35af9
Umair Ali <umairmamoor@gmail.com>
2025-03-02 06:36:12 -0500

    private fields in the InventoryItem class

--- JavaCollections/StoreChallenge/src/dev/lpa/InventoryItem.java
+++ JavaCollections/StoreChallenge/src/dev/lpa/InventoryItem.java
@@ -1,4 +1,18 @@
 package dev.lpa;
 
 public class InventoryItem {
+
+//private fields
+
+    private Product product;
+    private double price;
+    private int qtyTotal;
+    private int qtyReserved;
+    private int qtyReorder; //this is the quantity reordered if a minimum threshold is reached.
+    private int qtyLow; //this has to be reached to order more products.
+
+
+    
+
+
 }
