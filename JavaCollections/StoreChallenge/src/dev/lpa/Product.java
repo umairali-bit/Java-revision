5dce77c5d19aec846dcf0c4060b570561a121af8
Umair Ali <umairmamoor@gmail.com>
2025-03-02 06:31:52 -0500

    InventoryItem class

--- JavaCollections/StoreChallenge/src/dev/lpa/Product.java
+++ JavaCollections/StoreChallenge/src/dev/lpa/Product.java
@@ -1,5 +1,5 @@
 package dev.lpa;
 
 
-//using record because product should be immutable
+//using record because product should be immutableP
 public record Product(String sku, String name, String mfgr, Category category) {}
