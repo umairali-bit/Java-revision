76dadce677de4a5e7bdfc4fc86c886af425b1c30
Umair Ali <umairmamoor@gmail.com>
2025-03-02 06:55:41 -0500

    eservedItem will get called as an item gets added to the shopping cart

--- JavaCollections/StoreChallenge/src/dev/lpa/InventoryItem.java
+++ JavaCollections/StoreChallenge/src/dev/lpa/InventoryItem.java
@@ -6,7 +6,7 @@ public class InventoryItem {
 
     private Product product;
     private double price;
-    private int qtyTotal;
+    private int qtyTotal; //inventory
     private int qtyReserved;
     private int qtyReorder; //this is the quantity reordered if a minimum threshold is reached.
     private int qtyLow; //this has to be reached to order more products.
@@ -35,4 +35,20 @@ public class InventoryItem {
     public double getPrice() {
         return price;
     }
+
+    //reservedItem will get called as an item gets added to the shopping cart
+    public boolean reservedItem (int qty) { //qty is the number of the product the shopper added to the cart
+
+        //to make sure that the qty being added doesnt take the inventory below zero
+        //the qtyTotal, the inventory , minus the qtyReserved, in other people's carts, is still greater than the qty requested
+        if ((qtyTotal - qtyReserved) >= qty) {
+            qtyReserved += qty;// adding the qty to qtyReserved
+            return true;
+        }
+        //if theres not enough inventory for the shopper to buy, return false
+        return false;
+
+
+    }
+
 }
