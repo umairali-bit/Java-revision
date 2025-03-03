957a1cb1a0df14a5311b19bc7cb6183627aab939
Umair Ali <umairmamoor@gmail.com>
2025-03-03 06:26:19 -0500

    private fields

--- JavaCollections/StoreChallenge/src/dev/lpa/Cart.java
+++ JavaCollections/StoreChallenge/src/dev/lpa/Cart.java
@@ -1,6 +1,18 @@
 package dev.lpa;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 public class Cart {
 
     enum CartType {PHYSICAL, VIRTUAL};
+
+    private static int lastId = 1; //cart number
+    private int id;
+    private LocalDate cartDate;
+    private CartType type;
+    private Map<String, Integer> products; //string will be product sku, Integer will be the quantity ordered
+
+    
+
 }
