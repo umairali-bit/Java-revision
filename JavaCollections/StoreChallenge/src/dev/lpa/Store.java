92e96d440b0217730f517fdde9f817b197aa80fd
Umair Ali <umairmamoor@gmail.com>
2025-03-03 16:41:26 -0500

    a few maps for the store class

--- JavaCollections/StoreChallenge/src/dev/lpa/Store.java
+++ JavaCollections/StoreChallenge/src/dev/lpa/Store.java
@@ -1,7 +1,15 @@
 package dev.lpa;
 
+import java.util.*;
+
 public class Store {
 
+
+    private Map<String, InventoryItem> inventory;
+
+    private NavigableSet<Cart> carts = new TreeSet<>(Comparator.comparing(Cart::getId));
+
+    private Map<Category, Map<String, InventoryItem>> aisleInventory;
 
     public static void main(String[] args) {
 
