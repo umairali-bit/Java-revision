2a3b560b67ff77e562b78cac5665220537625cdc
Umair Ali <umairmamoor@gmail.com>
2025-03-04 05:59:44 -0500

    stockStore() with an inventory map

--- JavaCollections/StoreInventoryManagment1/src/src/dev/lpa/Product.java
+++ JavaCollections/StoreInventoryManagment1/src/src/dev/lpa/Product.java
@@ -2,4 +2,5 @@ package src.dev.lpa;
 
 
 //using record because product should be immutable
+//sku is short for stock keep unit, mfgr is manufacturer
 public record Product(String sku, String name, String mfgr, Category category) {}
--- JavaCollections/StoreInventoryManagment1/src/src/dev/lpa/Store.java
+++ JavaCollections/StoreInventoryManagment1/src/src/dev/lpa/Store.java
@@ -1,12 +1,10 @@
 package src.dev.lpa;
 
-import java.util.Comparator;
-import java.util.Map;
-import java.util.NavigableSet;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Store {
 
+    private static Random random = new Random();
 
     private Map<String, InventoryItem> inventory;
 
@@ -14,7 +12,61 @@ public class Store {
 
     private Map<Category, Map<String, InventoryItem>> aisleInventory;
 
+
+
     public static void main(String[] args) {
 
+
+
     }
+
+    //setting up some products, which be stocked in the store ans aisles
+    private void stockStore () {
+
+        //instantiating inventory field to a new HashMap
+        inventory = new HashMap<>();
+
+        //List of products
+        List<Product> products = new ArrayList<>(List.of(
+                new Product("A100","apple","local",Category.PRODUCE),
+                new Product("B100","banana","local",Category.PRODUCE),
+                new Product("P100","pear","local",Category.PRODUCE),
+                new Product("L103","lemon","local",Category.PRODUCE),
+                new Product("M201","milk","farm",Category.DAIRY),
+                new Product("Y001","yogurt","farm",Category.DAIRY),
+                new Product("C333","cheese","farm",Category.DAIRY),
+                new Product("R777","rice chex","Nabisco",Category.CEREAL),
+                new Product("G111","granola","Nat Valley",Category.CEREAL),
+                new Product("BB11","ground beef","butcher",Category.MEAT),
+                new Product("CC11","chicken","butcher",Category.MEAT),
+                new Product("BC11","bacon","butcher",Category.MEAT),
+                new Product("BC77","coke","coca cola",Category.BEVERAGE),
+                new Product("BC88","coffee","value",Category.BEVERAGE),
+                new Product("BC99","tea","herbal",Category.BEVERAGE)
+        ));
+
+        //setting up my inventory map
+
+        products.forEach(p-> inventory.put(p.sku(), new InventoryItem(p,random.nextDouble(0,1.25),
+                1000, 5)));
+        
+
+
+
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
