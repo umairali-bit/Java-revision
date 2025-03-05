73d7a2f6347d27d450612d12c0aa4e737c6258eb
Umair Ali <umairmamoor@gmail.com>
2025-03-05 15:36:55 -0500

    BaseClass with a public recomendedMethod() o control the work flow of the class. This method will invoke OptionalMethod() and mandatoryMethod()

---
+++ MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/dev/lpa/generic/BaseClass.java
@@ -0,0 +1,16 @@
+package dev.lpa.generic;
+
+public class BaseClass {
+
+    /*
+    This class should not be customizable
+    This class should have three methods
+     */
+
+    //to control the work flow on the class we have a public method
+    public void recommendedMethod() {
+        System.out.println("[BaseClass.recommendedMethod]: Best way to do it");
+        optionalMethod();
+        mandatoryMethod();
+    }
+}
