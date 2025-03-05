f852792aa89b7e0ec214702c5f6e0264452fc551
Umair Ali <umairmamoor@gmail.com>
2025-03-05 15:58:17 -0500

    instanced of recommended()

--- MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/cosnumer/specific/ChildClass.java
+++ MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/cosnumer/specific/ChildClass.java
@@ -6,4 +6,3 @@ public class ChildClass extends BaseClass {
 
 }
 
-
--- MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/dev/lpa/Main.java
@@ -1,8 +1,22 @@
 package dev.lpa;
 
+import cosnumer.specific.ChildClass;
+import dev.lpa.generic.BaseClass;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        BaseClass parent = new BaseClass();
+        ChildClass child = new ChildClass();
+
+        BaseClass childReferredToAsBase = new ChildClass();
+
+        parent.recommendedMethod();
+        System.out.println("-".repeat(50));
+        childReferredToAsBase.recommendedMethod();
+        System.out.println("-".repeat(50));
+        child.recommendedMethod();
 
     }
 }
