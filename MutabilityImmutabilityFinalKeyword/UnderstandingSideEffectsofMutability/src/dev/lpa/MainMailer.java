5821f0acbe221f0caf8996b5c7c85fc06a8a6b83
Umair Ali <umairmamoor@gmail.com>
2025-03-06 14:57:31 -0500

    getNames static method that will create a random list of names, a local variable list that will be returned and int index to keep the number of the names in the list

--- MutabilityImmutabilityFinalKeyword/UnderstandingSideEffectsofMutability/src/dev/lpa/MainMailer.java
+++ MutabilityImmutabilityFinalKeyword/UnderstandingSideEffectsofMutability/src/dev/lpa/MainMailer.java
@@ -5,16 +5,28 @@ This class will contain a list containing duplicate names, then use a map to kee
 for each distinct name
  */
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainMailer {
 
     public static void main(String[] args) {
 
         String[] names = {"Ann Jones", "Ann Jones Ph.D.", "Bob Jones M.D.", "Carol Jones", "Ed Green Ph.D.",
                             "Ed Green M.D", "Ed Black"};
+    }
 
-        
+    //a static method that will create a random list of names
+    private static List<StringBuilder> getNames(String[] names) {
+
+        List<StringBuilder> list = new ArrayList<>();
+        int index = 3;
+
+        return list;
+
 
     }
+
 
 
 }
