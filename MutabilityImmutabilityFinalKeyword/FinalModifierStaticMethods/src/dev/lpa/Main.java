4412b352d9697bb72427395ff17c086925dd927c
Umair Ali <umairmamoor@gmail.com>
2025-03-06 07:16:19 -0500

    using mutable object

--- MutabilityImmutabilityFinalKeyword/FinalModifierStaticMethods/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/FinalModifierStaticMethods/src/dev/lpa/Main.java
@@ -31,16 +31,21 @@ public class Main {
         ChildClass.recommendedStatic();
 
         System.out.println("-".repeat(50));
+        StringBuilder zArgument = new StringBuilder("Only saying this section");
         String xArgument = "This is all I've got to say Section ";
-        doXYZ(xArgument, 16);
+        doXYZ(xArgument, 16, zArgument);
+        System.out.println("After Method, xArgument: " + xArgument);
+        System.out.println("After Method, zArgument: " + zArgument);
 
 
     }
 
-    private static void doXYZ(String x, int y) {
+    private static void doXYZ(String x, int y, final StringBuilder z) {
 
         final String c = x + y;
         System.out.println("c = " + c);
-
+        x = c;
+        z.append(y);
+   //     z = new StringBuilder("This is a new reference");
     }
 }
