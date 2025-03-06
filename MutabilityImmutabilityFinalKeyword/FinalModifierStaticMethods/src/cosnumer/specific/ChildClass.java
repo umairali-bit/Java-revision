60a332b45430b71b8a8bf95802f0e41aed005170
Umair Ali <umairmamoor@gmail.com>
2025-03-06 06:37:31 -0500

    hiding a static method on ChildClass

---
+++ MutabilityImmutabilityFinalKeyword/FinalModifierStaticMethods/src/cosnumer/specific/ChildClass.java
@@ -0,0 +1,26 @@
+package cosnumer.specific;
+
+import dev.lpa.generic.BaseClass;
+
+public class ChildClass extends BaseClass {
+
+    @Override
+    protected void optionalMethod() {
+
+        System.out.println("[Child:optionalMethod] EXTRA Stuff here");
+        super.optionalMethod();
+
+    }
+
+//    @Override
+//    public void recommendedMethod() {
+//
+//        System.out.println("[Child:optionalMethod]: I will do things my way");
+//        optionalMethod();
+//    }
+
+    private void mandatoryMethod() {
+        System.out.println("[Child:optionalMethod]: My own important stuff");
+    }
+}
+
---
+++ MutabilityImmutabilityFinalKeyword/FinalModifierStaticMethods/src/dev/lpa/Main.java
@@ -0,0 +1,31 @@
+package dev.lpa;
+
+import cosnumer.specific.ChildClass;
+import dev.lpa.generic.BaseClass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BaseClass parent = new BaseClass();
+        ChildClass child = new ChildClass();
+
+        BaseClass childReferredToAsBase = new ChildClass();
+
+        parent.recommendedMethod();
+        System.out.println("-".repeat(50));
+        childReferredToAsBase.recommendedMethod();
+        System.out.println("-".repeat(50));
+        child.recommendedMethod();
+
+        System.out.println("-".repeat(50));
+        parent.recommendedStatic();
+        System.out.println("-".repeat(50));
+        childReferredToAsBase.recommendedStatic();
+        System.out.println("-".repeat(50));
+        child.recommendedStatic();
+
+
+
+    }
+}
