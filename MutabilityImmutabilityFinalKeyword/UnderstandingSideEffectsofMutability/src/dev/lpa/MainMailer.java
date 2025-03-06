57dd12e182ed33bee93cf215a99f376363aea4a8
Umair Ali <umairmamoor@gmail.com>
2025-03-06 14:50:36 -0500

    MainMailer class, will contain a list containing duplicate names, then use a map to keep track of the counts for each distinct name

---
+++ MutabilityImmutabilityFinalKeyword/UnderstandingSideEffectsofMutability/src/dev/lpa/MainMailer.java
@@ -0,0 +1,15 @@
+package dev.lpa;
+
+/*
+This class will contain a list containing duplicate names, then use a map to keep track of the counts
+for each distinct name
+ */
+
+public class MainMailer {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
