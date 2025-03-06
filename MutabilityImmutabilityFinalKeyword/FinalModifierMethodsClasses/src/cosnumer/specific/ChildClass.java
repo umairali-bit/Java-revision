df58b1a64f3c8aa5902f6f97760f93ac4a1c7fc4
Umair Ali <umairmamoor@gmail.com>
2025-03-06 05:57:17 -0500

    overriding optionalMethod() in childclass

--- MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/cosnumer/specific/ChildClass.java
+++ MutabilityImmutabilityFinalKeyword/FinalModifierMethodsClasses/src/cosnumer/specific/ChildClass.java
@@ -4,5 +4,13 @@ import dev.lpa.generic.BaseClass;
 
 public class ChildClass extends BaseClass {
 
+    @Override
+    protected void optionalMethod() {
+
+        System.out.println("[Child:optionalMethod] EXTRA Stuff here");
+        super.optionalMethod();
+
+    }
+
 }
 
