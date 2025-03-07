15a1ee78509d5ea77023f675535033aec23ab5c9
Umair Ali <umairmamoor@gmail.com>
2025-03-07 07:34:03 -0500

    toString() settting kidString = n/a and using setAll on Arrays class to populate names array. The parameter for the Lambda is an integer,             an index. using that integer (i) getting that child from kids array. That could be null, so use of a ternary operator to check for that, and set the name to an empty string, otherwise set it to the kid's name. Then the join method on String has been used joining all the kids names by comma

--- MutabilityImmutabilityFinalKeyword/DesigningImmutableClasses/src/dev/lpa/Person.java
+++ MutabilityImmutabilityFinalKeyword/DesigningImmutableClasses/src/dev/lpa/Person.java
@@ -4,6 +4,8 @@ package dev.lpa;
 person class with fields using encapsulation techniques
  */
 
+import java.util.Arrays;
+
 public class Person {
 
     private String name;
@@ -33,4 +35,26 @@ public class Person {
     public void setKids(Person[] kids) {
         this.kids = kids;
     }
+
+    @Override
+    public String toString() {
+
+        String kidString = "n/a";
+
+        if (kids != null) {
+            String[] names = new String[kids.length];
+            /*
+            using setAll on Arrays class to populate names array. The parameter for the Lambda is an integer,
+            an index. using that integer (i) getting that child from kids array. That could be null, so use of
+            a ternary operator to check for that, and set the name to an empty string, otherwise set it to the kid's
+            name. Then the join method on String has been used joining all the kids names by comma
+             */
+            Arrays.setAll(names, i -> names[i] = kids[i] == null ? "" : kids[i].name);
+            kidString = String.join(",", names);
+
+        }
+        return name + ", dob = " + dob + ", kids = " + kidString;
+    }
+
+
 }
