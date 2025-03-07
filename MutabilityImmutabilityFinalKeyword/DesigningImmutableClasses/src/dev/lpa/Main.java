d868474a6c7b62e9dae4ef8d88b2a51bc867018b
Umair Ali <umairmamoor@gmail.com>
2025-03-07 07:05:24 -0500

    creating a genealogy program, tracking people, names, birthdates and kids.

--- MutabilityImmutabilityFinalKeyword/DesigningImmutableClasses/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/DesigningImmutableClasses/src/dev/lpa/Main.java
@@ -1,6 +1,14 @@
 package dev.lpa;
 
+/*
+creating a genealogy program, tracking people, names, birthdates and kids.
+ */
+
 public class Main {
 
-    
+    public static void main(String[] args) {
+
+    }
+
+
 }
