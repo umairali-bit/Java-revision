e9b4d4d51a655203addac29c099aa20639f67d45
Umair Ali <umairmamoor@gmail.com>
2025-03-08 06:41:35 -0500

    creating new person with new constrcutor, creating an array of kids for johns kids

--- MutabilityImmutabilityFinalKeyword/DesigningImmutableClasses/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/DesigningImmutableClasses/src/dev/lpa/Main.java
@@ -8,22 +8,34 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person jane = new Person();
-        jane.setName("Jane");
-        Person jim = new Person();
-        jim.setName("Jim");
-        Person joe = new Person();
-        joe.setName("Joe");
-        Person john = new Person();
-        john.setName("John");
-        john.setDob("05/05/1900");
-        john.setKids(new Person[]{jane, jim, joe});
+//        Person jane = new Person();
+//        jane.setName("Jane");
+//        Person jim = new Person();
+//        jim.setName("Jim");
+//        Person joe = new Person();
+//        joe.setName("Joe");
+//        Person john = new Person();
+//        john.setName("John");
+//        john.setDob("05/05/1900");
+//        john.setKids(new Person[]{jane, jim, joe});
+//        System.out.println(john);
+//
+//        //john is not immutable
+//        john.setName("Jacob");
+//        john.setKids(new Person[]{new Person(), new Person()});
+//        System.out.println(john);
+
+
+        Person jane = new Person("Jane", "01/01/1930");
+        Person jim = new Person("Jim", "02/02/1940");
+        Person joe = new Person("Joe", "03/03/1934");
+
+        // to set up john kids
+        Person[] johnsKids = {jane, jim, joe};
+        Person john = new Person("John", "o4/04/1910", johnsKids);
         System.out.println(john);
 
-        //john is not immutable
-        john.setName("Jacob");
-        john.setKids(new Person[]{new Person(), new Person()});
-        System.out.println(john);
+
 
     }
 
