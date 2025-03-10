507d63aeae2bc1faedade784b71a5bd93ff431e0
Umair Ali <umairmamoor@gmail.com>
2025-03-10 13:17:07 -0400

    by making kids final we have to get rid of the overrided kids method

--- MutabilityImmutabilityFinalKeyword/CreatingTrulyImmutableClasses/src/dev/hacker/PersonOfInterest.java
+++ MutabilityImmutabilityFinalKeyword/CreatingTrulyImmutableClasses/src/dev/hacker/PersonOfInterest.java
@@ -1,2 +1,11 @@
-package dev.hacker;public class PersonOfInterest {
+package dev.hacker;
+
+import dev.lpa.PersonImmutable;
+
+public class PersonOfInterest extends PersonImmutable {
+
+    public PersonOfInterest(PersonImmutable person) {
+        super(person);
+    }
+    
 }
