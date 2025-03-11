ad3f0492e8d5b3c5e7b981c3597174fe224dc901
Umair Ali <umairmamoor@gmail.com>
2025-03-11 11:42:14 -0400

    creating 5 persons and making an array of those 5 people

--- MutabilityImmutabilityFinalKeyword/ShallowVSDeepCopies/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/ShallowVSDeepCopies/src/dev/lpa/Main.java
@@ -8,5 +8,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //creating 5 persons
+
+        Person joe = new Person("Joe", "01/01/1961");
+        Person jim = new Person("Jim", "02/02/1962");
+        Person jack = new Person("Jack", "03/03/1963");
+        Person jane = new Person("Jane", "04/04/1964");
+        Person jill = new Person("Jill", "05/05/1965");
+
+        //creatring an array of person
+        Person[] persons = {joe, jim, jack, jane, jill};
+
+
     }
 }
