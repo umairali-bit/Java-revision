9613de889dd98819598905a33ee0716738425467
Umair Ali <umairmamoor@gmail.com>
2025-03-11 12:08:33 -0400

    added another field to the record

--- MutabilityImmutabilityFinalKeyword/ShallowVSDeepCopies/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/ShallowVSDeepCopies/src/dev/lpa/Main.java
@@ -3,7 +3,16 @@ package dev.lpa;
 
 import java.util.Arrays;
 
-record Person (String name, String dob) {}
+record Person (String name, String dob, Person[] kids) {
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", kids=" + Arrays.toString(kids) +
+                '}';
+    }
+}
 
 public class Main {
 
@@ -12,11 +21,11 @@ public class Main {
 
         //creating 5 persons
 
-        Person joe = new Person("Joe", "01/01/1961");
-        Person jim = new Person("Jim", "02/02/1962");
-        Person jack = new Person("Jack", "03/03/1963");
-        Person jane = new Person("Jane", "04/04/1964");
-        Person jill = new Person("Jill", "05/05/1965");
+        Person joe = new Person("Joe", "01/01/1961", null);
+        Person jim = new Person("Jim", "02/02/1962", null);
+        Person jack = new Person("Jack", "03/03/1963", null);
+        Person jane = new Person("Jane", "04/04/1964", null);
+        Person jill = new Person("Jill", "05/05/1965", null);
 
         //creating an array of person
         Person[] persons = {joe, jim, jack, jane, jill};
