d5621c40ad4662f9bb2fedd8efb4ab768094cdcb
Umair Ali <umairmamoor@gmail.com>
2025-03-12 11:04:10 -0400

    creating two stringBuilder objects and creating two students with those objects. Creating a list of students and printing them.

--- MutabilityImmutabilityFinalKeyword/ImmutableCollectionsUnmodifiableViews/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/ImmutableCollectionsUnmodifiableViews/src/dev/lpa/Main.java
@@ -1,4 +1,25 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        StringBuilder bobsNotes = new StringBuilder();
+        StringBuilder billsNotes = new StringBuilder("Bill struggles with generics");
+
+        Student bob = new Student("Bob", bobsNotes);
+        Student bill = new Student("Bill", billsNotes);
+
+        List<Student> students = new ArrayList<>(List.of(bob,bill));
+
+        students.forEach(System.out::println);
+        System.out.println("-".repeat(30));
+
+
+
+    }
 }
