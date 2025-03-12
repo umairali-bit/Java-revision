c027cf43f56299dd971ce89c665316d5ca4ac244
Umair Ali <umairmamoor@gmail.com>
2025-03-12 11:32:09 -0400

    making the studentNotes immutable

--- MutabilityImmutabilityFinalKeyword/ImmutableCollectionsUnmodifiableViews/src/dev/lpa/Student.java
+++ MutabilityImmutabilityFinalKeyword/ImmutableCollectionsUnmodifiableViews/src/dev/lpa/Student.java
@@ -6,4 +6,26 @@ public class Student {
     private final StringBuilder studentNotes;
 
 
+    public Student(String name, StringBuilder studentNotes) {
+        this.name = name;
+        this.studentNotes = new StringBuilder(studentNotes);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public StringBuilder getStudentNotes() {
+        return studentNotes;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", studentNotes=" + studentNotes +
+                '}';
+    }
 }
