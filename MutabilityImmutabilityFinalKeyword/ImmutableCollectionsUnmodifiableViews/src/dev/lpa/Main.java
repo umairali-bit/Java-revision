5a17f1efbd27f6ff02b6e106a77fee22091e3a9c
Umair Ali <umairmamoor@gmail.com>
2025-03-12 12:11:43 -0400

    creating a copy of students list by passing the previous list to it, adding a string builder object and appending a message to it.

--- MutabilityImmutabilityFinalKeyword/ImmutableCollectionsUnmodifiableViews/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/ImmutableCollectionsUnmodifiableViews/src/dev/lpa/Main.java
@@ -11,15 +11,27 @@ public class Main {
         StringBuilder bobsNotes = new StringBuilder();
         StringBuilder billsNotes = new StringBuilder("Bill struggles with generics");
 
+
         Student bob = new Student("Bob", bobsNotes);
         Student bill = new Student("Bill", billsNotes);
+       // Student babe = new Student("Babe", babesNotes);
 
         List<Student> students = new ArrayList<>(List.of(bob,bill));
+
+        //creating a copy of students list by passing the previous list to it
+        List<Student> studentsFirstCopy = new ArrayList<>(students);
+        studentsFirstCopy.add(new Student( " Bonnie", new StringBuilder()));
+        StringBuilder bonnieNotes = studentsFirstCopy.get(2).getStudentNotes();
+        bonnieNotes.append("Bonnie is taking 3 of my courses");
 
         bobsNotes.append("Bob was one of my students. ");
 
         students.forEach(System.out::println);
         System.out.println("-".repeat(30));
+        studentsFirstCopy.forEach(System.out::println);
+        System.out.println("-".repeat(30));
+
+
 
 
 
