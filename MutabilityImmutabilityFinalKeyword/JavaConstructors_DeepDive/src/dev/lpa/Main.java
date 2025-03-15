04471ce14657300f59e5fc735f2b21d598b4a32b
Umair Ali <umairmamoor@gmail.com>
2025-03-15 07:39:26 -0400

    two private String fields in the Parent class, with getters and setters and toString

--- MutabilityImmutabilityFinalKeyword/JavaConstructors_DeepDive/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/JavaConstructors_DeepDive/src/dev/lpa/Main.java
@@ -1,10 +1,13 @@
 package dev.lpa;
 
+import external.Child;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Parent parent = new Parent();
+        Child child = new Child();
 
     }
 }
--- MutabilityImmutabilityFinalKeyword/JavaConstructors_DeepDive/src/dev/lpa/Parent.java
+++ MutabilityImmutabilityFinalKeyword/JavaConstructors_DeepDive/src/dev/lpa/Parent.java
@@ -1,4 +1,28 @@
 package dev.lpa;
 
 public class Parent {
+
+    private String name;
+    private String dob;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    @Override
+    public String toString() {
+        return "name= ' " + name + '\'' +", dob='" + dob+ '\'';
+    }
 }
