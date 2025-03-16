1d5f2794aaca7cc2b641475ddbc059108f255157
Umair Ali <umairmamoor@gmail.com>
2025-03-16 17:36:20 -0400

    added two instance fields in Generations, added dates for geenrations, modified the println statement

--- MutabilityImmutabilityFinalKeyword/MasteringEnumConstruuctors/src/dev/lpa/Generations.java
+++ MutabilityImmutabilityFinalKeyword/MasteringEnumConstruuctors/src/dev/lpa/Generations.java
@@ -2,15 +2,20 @@ package dev.lpa;
 
 public enum Generations {
 
-    GEN_Z,
-    MILLENNIAL,
-    GEN_X,
-    BABY_BOOMER,
-    SILENT_GENERATION,
-    GREATEST_GENERATION;
+    GEN_Z (2001, 2025),
+    MILLENNIAL(1981,2000),
+    GEN_X(1965,1980),
+    BABY_BOOMER(1946, 1964),
+    SILENT_GENERATION(1927,1945),
+    GREATEST_GENERATION(1901,1926);
+
+    private int startYear;
+    private int endYear;
 
 
-    Generations() {
-        System.out.println(this);
+  Generations(int startYear, int endYear) {
+        this.startYear = startYear;
+        this.endYear = endYear;
+        System.out.println(this + " " + startYear + " - " + endYear);
     }
 }
