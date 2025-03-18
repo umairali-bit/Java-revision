4ec9c388725b18933ff36952d5b2314c6dcbd254
Umair Ali <umairmamoor@gmail.com>
2025-03-18 07:18:15 -0400

    testing getWeaponByChar() in main

--- MutabilityImmutabilityFinalKeyword/PriateAdventurewithConstructorsAndInitializers/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/PriateAdventurewithConstructorsAndInitializers/src/dev/lpa/Main.java
@@ -2,16 +2,20 @@ package dev.lpa;
 
 import dev.lpa.game.GameConsole;
 import dev.lpa.game.ShooterGame;
+import dev.lpa.pirate.Weapon;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        var console = new GameConsole<>(new ShooterGame("The shooter Game"));
+//        var console = new GameConsole<>(new ShooterGame("The shooter Game"));
+//
+//        int playerIndex = console.addPlayer();
+//        console.playGame(playerIndex);
 
-        int playerIndex = console.addPlayer();
-        console.playGame(playerIndex);
+        Weapon weapon = Weapon.getWeaponByChar('P');
+        System.out.println(weapon);
 
 
 
