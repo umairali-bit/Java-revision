d4eef12362d8de2f84151cda1e8aafd833367bdd
Umair Ali <umairmamoor@gmail.com>
2025-03-18 07:26:00 -0400

    testing the getWeaponsByLevel()

--- MutabilityImmutabilityFinalKeyword/PriateAdventurewithConstructorsAndInitializers/src/dev/lpa/Main.java
+++ MutabilityImmutabilityFinalKeyword/PriateAdventurewithConstructorsAndInitializers/src/dev/lpa/Main.java
@@ -18,7 +18,8 @@ public class Main {
         System.out.println("Weapon = " + weapon + ", hitPoints = " + weapon.getHitPoints() + ", minLevel = "
                 + weapon.getMinLevel());
 
-
+        var list = Weapon.getWeaponsByLevel('1');
+        list.forEach(w -> System.out.println(w)); // could be used as System.out::println
 
 
 
