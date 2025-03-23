f92194d64e87874a0ea769cf6dcace886c63e1dd
Umair Ali <umairmamoor@gmail.com>
2025-03-23 11:51:57 -0400

    Building a complete Pirate Game: Loot, Combat, and Town Features in Java

---
+++ MutabilityImmutabilityFinalKeyword/BuildingACompletePirateGame/src/dev/lpa/Main.java
@@ -0,0 +1,38 @@
+package dev.lpa;
+
+import dev.lpa.game.GameConsole;
+import dev.lpa.pirate.PirateGame;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+////        var console = new GameConsole<>(new ShooterGame("The shooter Game"));
+////
+////        int playerIndex = console.addPlayer();
+////        console.playGame(playerIndex);
+//
+//        Weapon weapon = Weapon.getWeaponByChar('P');
+//        System.out.println("Weapon = " + weapon + ", hitPoints = " + weapon.getHitPoints() + ", minLevel = "
+//                + weapon.getMinLevel());
+//
+//       // var list = Weapon.getWeaponsByLevel('1');
+//       // list.forEach(w -> System.out.println(w)); // could be used as System.out::println
+//
+//        Pirate umair = new Pirate("Umair");
+//        System.out.println(umair);
+//
+//        PirateGame.getTowns(0).forEach(System.out::println);
+//        System.out.println("-".repeat(50));
+//        PirateGame.getTowns(1).forEach(System.out::println);
+
+
+        var gameConsole = new GameConsole<>(new PirateGame("The Pirate Game"));
+       int playerIndex =  gameConsole.addPlayer();
+        gameConsole.playGame(playerIndex);
+
+
+
+    }
+}
