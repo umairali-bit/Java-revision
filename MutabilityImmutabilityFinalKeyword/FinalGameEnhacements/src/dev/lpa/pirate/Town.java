39a6efa2465bea2403ab75c8913082129c3e6634
Umair Ali <umairmamoor@gmail.com>
2025-03-24 07:19:47 -0400

    compact contructor initializing fields

--- MutabilityImmutabilityFinalKeyword/FinalGameEnhacements/src/dev/lpa/pirate/Town.java
+++ MutabilityImmutabilityFinalKeyword/FinalGameEnhacements/src/dev/lpa/pirate/Town.java
@@ -1,12 +1,27 @@
 package dev.lpa.pirate;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.EnumSet;
 import java.util.List;
 
 public record Town(String name, String island, int level,
                    List<Loot> loot,
-                   List<Feature> feature,
+                   List<Feature> features,
                    List<Combatant> opponents) {
+
+    public Town {
+        loot = randomReduced(new ArrayList<>(EnumSet.allOf(Loot.class)), level + 2);//1st level will have 2 treasures
+        features = randomReduced(new ArrayList<>(EnumSet.allOf(Feature.class)), level + 3);
+        opponents = new ArrayList<>();
+
+        if (level == 0) {
+            opponents.add(new Islander("Joe", Weapon.KNIFE));
+        } else {
+            opponents.add(new Islander("Joe", Weapon.MACHETE));
+            opponents.add(new Soldier("John", Weapon.PISTOL));
+        }
+    }
 
     //A generic method that returns a List<T> randomizes the elements in the list and returns a sublist of those elements
     private <T> List<T> randomReduced(List<T> list, int size) {
