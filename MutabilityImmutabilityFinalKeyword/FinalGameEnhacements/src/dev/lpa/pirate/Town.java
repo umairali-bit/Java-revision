a628d810d7db627fdb60852e1683e78a29b5759a
Umair Ali <umairmamoor@gmail.com>
2025-03-24 07:07:24 -0400

    A generic method that returns a List<T> randomizes the elements in the list and retuurns a sublist of those elements

--- MutabilityImmutabilityFinalKeyword/FinalGameEnhacements/src/dev/lpa/pirate/Town.java
+++ MutabilityImmutabilityFinalKeyword/FinalGameEnhacements/src/dev/lpa/pirate/Town.java
@@ -1,6 +1,20 @@
 package dev.lpa.pirate;
 
-public record Town(String name, String island, int level) {
+import java.util.Collections;
+import java.util.List;
+
+public record Town(String name, String island, int level,
+                   List<Loot> loot,
+                   List<Feature> feature,
+                   List<Combatant> opponents) {
+
+    //A generic method that returns a List<T> randomizes the elements in the list and returns a sublist of those elements
+    private <T> List<T> randomReduced(List<T> list, int size) {
+
+        Collections.shuffle(list);
+        return list.subList(0,size);
+
+    }
 
 
 }
