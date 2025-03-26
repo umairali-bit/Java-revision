fdf6f8d53328fd3c895ef7ebfae6c82bc2396862
Umair Ali <umairmamoor@gmail.com>
2025-03-26 11:17:30 -0400

    assigning the source of the stream to var, and using it in the terminal.After that tried to execute an operation to the same stream, it failed because we cannot resuse the stream

--- ComprehensiveJavaStreams/DeepDiveJavaStreamPipelines/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/DeepDiveJavaStreamPipelines/src/dev/lpa/Main.java
@@ -64,19 +64,19 @@ public class Main {
         System.out.println("\n------------------------");
 
         //chain of listing 5 operations on stream mentioned below is called Pipeline
-        bingoPool.stream()
+        var tempStream = bingoPool.stream()
                 .limit(15)
                 .filter(s -> s.indexOf('G') == 0 || s.indexOf('O') == 0)
                 .map(s-> s.charAt(0) + "-" + s.substring(1))
-                .sorted()
-                .forEach(s -> System.out.print(s + " "));
+                .sorted();
+//                .forEach(s -> System.out.print(s + " "));
+
+        tempStream.forEach(s-> System.out.println(s + ""));
 
         System.out.println("\n------------------------");
 
+        tempStream.forEach(s-> System.out.println(s.toLowerCase() + ""));
 
-        for (int i = 0; i < 15; i++) {
-            System.out.println(bingoPool.get(i));
-        }
 
 
 
