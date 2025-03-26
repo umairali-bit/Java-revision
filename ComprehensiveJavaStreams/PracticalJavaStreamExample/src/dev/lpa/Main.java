483c1f1738a5dd8350be84a0c1a858fa9bed2429
Umair Ali <umairmamoor@gmail.com>
2025-03-26 07:29:37 -0400

    shuffled the bingoPool list, printing first 15 in the list via forLoop

--- ComprehensiveJavaStreams/PracticalJavaStreamExample/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/PracticalJavaStreamExample/src/dev/lpa/Main.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
@@ -17,14 +18,21 @@ public class Main {
         for (char c : "BINGO".toCharArray()){
             for (int i = start; i<(start + 15); i++) {
                 bingoPool.add("" + c + i);
-                System.out.println("" + c + i);
+ //               System.out.println("" + c + i);
             }
 
 
             start += 15;
         }
 
+        Collections.shuffle(bingoPool);
 
+        for (int i = 0; i < 15; i++) {
+            System.out.println(bingoPool.get(i));
+
+        }
+
+        System.out.println("-".repeat(50));
     }
 
 
