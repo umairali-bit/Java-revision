984155b73c1ce2775629ee32c333a40f9020da05
Umair Ali <umairmamoor@gmail.com>
2025-03-28 06:15:08 -0400

    initalizing the streamB in a variable using finite stream and then mapping the integer to B followed by the generated number

--- ComprehensiveJavaStreams/ChallengeMasteringStreamSourcesAndDataConcatenation/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/ChallengeMasteringStreamSourcesAndDataConcatenation/src/dev/lpa/Main.java
@@ -1,8 +1,19 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int seed = 1;
+        var streamB = Stream.iterate(seed, n -> n <= 15, n -> n + 1 ) //finite stream
+                .map(i -> "B" + i);//mapping the integer to B, then is followed by the generated number
+        streamB.forEach(System.out::println);
+        }
+
+
     }
-}
+
