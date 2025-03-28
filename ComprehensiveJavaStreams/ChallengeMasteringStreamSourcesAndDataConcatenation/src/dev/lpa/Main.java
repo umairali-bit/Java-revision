ede48bff873dbc254370473357d585f06cdeb4bb
Umair Ali <umairmamoor@gmail.com>
2025-03-28 06:27:07 -0400

    infinite stream with predicate

--- ComprehensiveJavaStreams/ChallengeMasteringStreamSourcesAndDataConcatenation/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/ChallengeMasteringStreamSourcesAndDataConcatenation/src/dev/lpa/Main.java
@@ -9,9 +9,15 @@ public class Main {
     public static void main(String[] args) {
 
         int seed = 1;
-        var streamB = Stream.iterate(seed, n -> n <= 15, n -> n + 1 ) //finite stream
+        var streamB = Stream.iterate(seed, n -> n <= 15, n -> n + 1 ) //finite stream on static iterate(), using unary operator
                 .map(i -> "B" + i);//mapping the integer to B, then is followed by the generated number
-        streamB.forEach(System.out::println);
+
+        seed += 15;
+        var streamI = Stream.iterate(seed, i -> i + 1)//infinite stream with predicate
+                                    .limit(15)
+                                        .map(n -> "I" + n);
+
+        streamI.forEach(System.out::println);
         }
 
 
