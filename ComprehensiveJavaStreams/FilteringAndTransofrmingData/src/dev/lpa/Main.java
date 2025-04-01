6eca5ee39e12264ba9c745a8a2748511a1cbaac5
Umair Ali <umairmamoor@gmail.com>
2025-04-01 07:39:09 -0400

    for range of elements we can use takeWhile()

--- ComprehensiveJavaStreams/FilteringAndTransofrmingData/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/FilteringAndTransofrmingData/src/dev/lpa/Main.java
@@ -7,12 +7,24 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //IntStream.iterate with three-parameter version of iterate, 1st casting a char, predicate and Unary operator
-        // version of stream pipeline without any intermediate operations
+        /*
+
+         IntStream.iterate with three-parameter version of iterate, 1st casting a char, predicate and Unary operator
+         version of stream pipeline without any intermediate operations
+         can be replaced by method reference Character::isAlphabetic
+         dropWhile() takes a predicate and evaluate until it first becomes false
+         .skip(5)
+         .filter(i -> Character.toUpperCase(i) > 'E')
+         takeWhile(i -> i < 'a') for range of alphabets
+
+
+         */
+
         IntStream.iterate ((int) 'A', i -> i <= (int) 'z', i -> i +1)
-                .filter(i -> Character.isAlphabetic(i)) //can be replaced by method reference Character::isAlphabetic
-                .dropWhile(i-> i <= 'E') // dropWhile() takes a predicate and evaluate until it first becomes false
-               // .skip(5)
+                .filter(i -> Character.isAlphabetic(i))
+                .dropWhile(i-> Character.toUpperCase(i) <= 'E')
+                .takeWhile(i -> i <'a')
+                // .skip(5)
                 //.filter(i -> Character.toUpperCase(i) > 'E')
                 .forEach(d -> System.out.printf("%c ", d));
 
