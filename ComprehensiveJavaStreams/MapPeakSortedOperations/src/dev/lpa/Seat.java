8a3497964275ce150b9dc61634e1d15794d05fa6
Umair Ali <umairmamoor@gmail.com>
2025-04-02 06:28:03 -0400

    custom constructor

--- ComprehensiveJavaStreams/MapPeakSortedOperations/src/dev/lpa/Seat.java
+++ ComprehensiveJavaStreams/MapPeakSortedOperations/src/dev/lpa/Seat.java
@@ -2,5 +2,8 @@ package dev.lpa;
 
 public record Seat(char rowMarker, int seatNumber, double price) {
 
-
+    public Seat(char rowMarker, int seatNumber) {
+        this(rowMarker, seatNumber,
+                rowMarker > 'C' && (seatNumber <= 2 || seatNumber >= 9) ? 50 : 75 );
+    }
 }
