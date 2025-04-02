bad290ff0b728360e8d915b22131d3bffa0b0503
Umair Ali <umairmamoor@gmail.com>
2025-04-02 06:31:02 -0400

    toString()

--- ComprehensiveJavaStreams/MapPeakSortedOperations/src/dev/lpa/Seat.java
+++ ComprehensiveJavaStreams/MapPeakSortedOperations/src/dev/lpa/Seat.java
@@ -6,4 +6,10 @@ public record Seat(char rowMarker, int seatNumber, double price) {
         this(rowMarker, seatNumber,
                 rowMarker > 'C' && (seatNumber <= 2 || seatNumber >= 9) ? 50 : 75 );
     }
+
+
+    @Override
+    public String toString() {
+        return "%c%03d %.0f".formatted(rowMarker, seatNumber, price);
+    }
 }
