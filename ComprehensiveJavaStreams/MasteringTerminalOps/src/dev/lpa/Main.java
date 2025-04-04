250416cebdbbaada73a3ccb9c37d0c0ce81f2cee
Umair Ali <umairmamoor@gmail.com>
2025-04-04 06:31:48 -0400

    summaryStatistic() priting out summary of the data with min,max, average, count

--- ComprehensiveJavaStreams/MasteringTerminalOps/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/MasteringTerminalOps/src/dev/lpa/Main.java
@@ -1,4 +1,16 @@
 package dev.lpa;
 
+import java.util.stream.IntStream;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+        var result = IntStream
+                .iterate(0, i -> i <= 1000, i -> i + 10)
+                .summaryStatistics();
+
+        System.out.println("Result = " + result );
+
+    }
 }
