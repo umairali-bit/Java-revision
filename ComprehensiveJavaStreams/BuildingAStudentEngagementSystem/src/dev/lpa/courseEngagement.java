7e916972929eeda1a6f3111abd73cfcb03ecadaf
Umair Ali <umairmamoor@gmail.com>
2025-04-06 06:33:22 -0400

    CourseEnagagement class with encapsulation and immutabilibity both

---
+++ ComprehensiveJavaStreams/BuildingAStudentEngagementSystem/src/dev/lpa/courseEngagement.java
@@ -0,0 +1,25 @@
+package dev.lpa;
+
+/*
+Each student will have a course engagement instance, for every course they're enrolled in.
+It should have the fields for the course, the enrollment date, the engagement type, the last lecture, and the last activity date.
+It should have the usual getters, plus getters for calculated fields as shown here.
+The getMonthsSinceActive method should return the months elapsed, since the last course activity.
+
+ */
+
+import java.time.LocalDate;
+
+public class courseEngagement {
+
+    private final Course course;
+    private final LocalDate enrollmentDate;
+    private final String engagenmentType;
+    private final int lastLecture;
+    private final LocalDate lastActivityDate;
+
+
+
+
+
+}
