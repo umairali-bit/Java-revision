ed32383921a5bc5723481ccd69817a5136df79d6
Umair Ali <umairmamoor@gmail.com>
2025-04-06 13:41:55 -0400

    testing in main

--- ComprehensiveJavaStreams/BuildingAStudentEngagementSystem/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/BuildingAStudentEngagementSystem/src/dev/lpa/Main.java
@@ -1,4 +1,15 @@
 package dev.lpa;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        Course pymc = new Course("PYMC", "Python MasterClass");
+        Course jmc = new Course("JMC", "Java MasterClass");
+        Student joe = new Student("US",2019,30,"M",true,
+                pymc, jmc);
+        System.out.println(joe);
+
+    }
 }
