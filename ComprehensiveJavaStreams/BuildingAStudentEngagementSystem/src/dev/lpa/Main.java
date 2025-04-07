ff2ae2ed017d4e99ba172501bac70b3143bee98e
Umair Ali <umairmamoor@gmail.com>
2025-04-07 06:46:08 -0400

    testing getRandomStudent() in main by using Stream.generate() that takes a supplier

--- ComprehensiveJavaStreams/BuildingAStudentEngagementSystem/src/dev/lpa/Main.java
+++ ComprehensiveJavaStreams/BuildingAStudentEngagementSystem/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.stream.Stream;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,14 +9,19 @@ public class Main {
 
         Course pymc = new Course("PYMC", "Python MasterClass");
         Course jmc = new Course("JMC", "Java MasterClass");
-        Student joe = new Student("US",2019,30,"M",true,
-                pymc, jmc);
-        System.out.println(joe);
+//        Student joe = new Student("US",2019,30,"M",true,
+//                pymc, jmc);
+//        System.out.println(joe);
+//
+//        joe.watchLecture("JMC", 10,5,2019);
+//        joe.watchLecture("PYMC",7,7,2020);
+//
+//        System.out.println(joe);
 
-        joe.watchLecture("JMC", 10,5,2019);
-        joe.watchLecture("PYMC",7,7,2020);
-
-        System.out.println(joe);
+        //using stream.generate with supplier()
+        Stream.generate(() -> Student.getRandomStudent(jmc,pymc))
+                .limit(10)
+                .forEach(i -> System.out.println(i));
 
     }
 }
