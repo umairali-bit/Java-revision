459e9dd976cf406ae719143c30cff2a00e4844bc
Umair Ali <umairmamoor@gmail.com>
2025-04-11 10:55:59 -0400

    list of randomly generated courses for students, limit students 1000, tolist(), getting a set of american stuudents

--- ComprehensiveJavaStreams/AdvancedStreamProcessing/src/dev/lpa/MainCollect.java
+++ ComprehensiveJavaStreams/AdvancedStreamProcessing/src/dev/lpa/MainCollect.java
@@ -1,10 +1,33 @@
 package dev.lpa;
 
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class MainCollect {
 
     public static void main(String[] args) {
 
         Course pymc = new Course("PYMC", "Python MasterClass");
         Course jmc = new Course("JMC", "Java MasterClass");
+
+//        List of Students randomly generated courses by using Stream.generate and Supplier
+//        limit student to 1000
+//        toList() to give a list of students in the order that they were created
+//        get a set of American students
+
+        List<Student> students = Stream.generate(() -> Student.getRandomStudent(pymc,jmc))
+                .limit(1000)
+                .toList();
+
+        Set<Student> americanStudents = students.stream()
+                .filter((s) -> s.getCountryCode().equals("US"))
+                .collect(Collectors.toSet());
+        System.out.println("# of American Students = " + americanStudents.size());
+
+
+
+
     }
 }
