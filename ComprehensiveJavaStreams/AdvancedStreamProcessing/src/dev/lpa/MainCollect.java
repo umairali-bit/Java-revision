df1f92bb80b0e5e6cbb0033d00ddc479998921b5
Umair Ali <umairmamoor@gmail.com>
2025-04-11 10:44:34 -0400

    two courses in the main method of MainCollect

--- ComprehensiveJavaStreams/AdvancedStreamProcessing/src/dev/lpa/MainCollect.java
+++ ComprehensiveJavaStreams/AdvancedStreamProcessing/src/dev/lpa/MainCollect.java
@@ -1,4 +1,10 @@
 package dev.lpa;
 
 public class MainCollect {
+
+    public static void main(String[] args) {
+
+        Course pymc = new Course("PYMC", "Python MasterClass");
+        Course jmc = new Course("JMC", "Java MasterClass");
+    }
 }
