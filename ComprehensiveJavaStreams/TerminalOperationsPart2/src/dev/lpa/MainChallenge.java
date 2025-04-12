110486b87804c79b6670cbb4cca22c90b3715696
Umair Ali <umairmamoor@gmail.com>
2025-04-12 07:03:13 -0400

    adding third course Creating Games in Java without passing a lecture count

--- ComprehensiveJavaStreams/TerminalOperationsPart2/src/dev/lpa/MainChallenge.java
+++ ComprehensiveJavaStreams/TerminalOperationsPart2/src/dev/lpa/MainChallenge.java
@@ -2,6 +2,24 @@ package dev.lpa;
 
 public class MainChallenge {
 
-    Course pymc = new Course("PYMC", "Python MasterClass", 50);
-    Course jmc = new Course("JMC", "Java MasterClass", 100);
+    /*
+    Copy the two courses, jmc and pymc, from the MainCollect's main method, passing both an additional argument
+    for the lecture count. 50 for pymc, and 100 for jmc.
+
+
+*/
+    public static void main(String[] args) {
+
+        Course pymc = new Course("PYMC", "Python MasterClass", 50);
+        Course jmc = new Course("JMC", "Java MasterClass", 100);
+        Course gamesJava = new Course ("GAJA", "Creating Games in Java");
+
+
+
+
+
+
+
+    }
+
 }
