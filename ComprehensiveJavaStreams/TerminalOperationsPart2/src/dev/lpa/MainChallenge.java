1a73242d197cfb595f332dd7a536a70b89fbfe9e
Umair Ali <umairmamoor@gmail.com>
2025-04-12 08:23:04 -0400

    Using Stream.iterate to generate 5000 random students and create a list of these.

--- ComprehensiveJavaStreams/TerminalOperationsPart2/src/dev/lpa/MainChallenge.java
+++ ComprehensiveJavaStreams/TerminalOperationsPart2/src/dev/lpa/MainChallenge.java
@@ -1,37 +1,89 @@
 package dev.lpa;
 
+import java.sql.SQLOutput;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class MainChallenge {
 
     /*
-    Copy the two courses, jmc and pymc, from the MainCollect's main method, passing both an additional argument
-    for the lecture count. 50 for pymc, and 100 for jmc.
-
+    Create a new class called MainChallenge, with a main method that does the following:
+    Copy the two courses, jmc and pymc, from the MainCollect's main method, passing both an additional
+    argument for the lecture count. 50 for pymc, and 100 for jmc.
+    Add a third course, titled "Creating Games in Java".   You don't have to pass a lecture count for this one.
+    Use Stream.generate or Stream.iterate to generate 5000 random students and create a list of these.
+    Use the getPercentComplete method, to calculate the average percentage completed for all students for just the
+    Java Masterclass, using the reduce terminal operation.
+    Use this result, multiplying it by 1.25, to collect a group of students (either as a list, or a set).
+    These would be the students who've completed more than three quarters of that average percentage.
+    Sort by the longest enrolled students who are still active, because you're going to offer your new course to 10 of
+    these students, for a trial run.
+    Add the new course to these ten students.
+    Make one change to the Student's getRandomStudent method, using a minimum lecture of 30.
 
 */
     public static void main(String[] args) {
 
         Course pymc = new Course("PYMC", "Python MasterClass", 50);
         Course jmc = new Course("JMC", "Java MasterClass", 100);
-        Course gamesJava = new Course ("GAJA", "Creating Games in Java");
+        Course jgames = new Course ("JGame", "Creating Games in Java");
 
 
-        List<Student> students = Stream.generate(() -> Student.getRandomStudent(pymc,jmc,gamesJava))
-                .limit(5000)
+//        List<Student> students = Stream.generate(() -> Student.getRandomStudent(pymc,jmc,gamesJava))
+//                .limit(5000)
+//                .toList();
+//
+//        var result = students.stream()
+//                .map(i -> i.getPercentComplete("JMC"))
+//                .reduce(
+//                        new double[] {0.0, 0.0}, //[sum, count]
+//                        (acc, percent) -> new double[]{acc[0] + percent, acc[1] + 1}, //accumulator
+//                        (a,b) -> new double[]{a[0] + b[0], a[1],b[1]}
+//                );
+//        double average = result[1] == 0 ? 0 :result[0] /result[1];
+//
+//        double threshold = 0.75 * average * 1.25;
+//
+//        Set<Student> topStudents = students.stream()
+//                .filter(i -> i.getPercentComplete("JMC") > threshold)
+//                .collect(Collectors.toSet());
+//        System.out.println("average" + average);
+//
+//        System.out.println("Top Student count = " + topStudents.size());
+//
+//        Comparator<Student> longTerm = Comparator.comparing(i -> i.getAgeEnrolled());
+//
+//
+//        students.stream()
+//                .filter(i -> i.getPercentComplete("JMC") >= threshold)
+//                .sorted(longTerm)
+//                .limit(10)
+//                .toList()
+//                .forEach(s -> {
+//                    s.addCourse(gamesJava);
+//                    System.out.print(s.getStudentId() + " ");
+//                });
+
+
+
+        List<Student> students = Stream.iterate(1, s -> s <= 500, s-> s + 1)
+                .map(s -> Student.getRandomStudent(jmc,pymc))
                 .toList();
 
-        var result = students.stream()
-                .map(i -> i.getPercentComplete("JMC"))
-                .reduce(
-                        new double[] {0.0, 0.0}, //[sum, count]
-                        (acc, percent) -> new double[]{acc[0] + percent, acc[1] + 1}, //accumulator
-                        (a,b) -> new double[]{a[0] + b[0], a[1],b[1]}
-                );
-        double average = result[1] == 0 ? 0 :result[0] /result[1];
 
-        double threshold = 0.75 * average * 1.25;
+
+
+
+
+
+
+
+
+
+
 
 
 
