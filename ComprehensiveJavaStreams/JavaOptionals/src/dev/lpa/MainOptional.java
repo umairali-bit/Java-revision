b918eb00a5c8fce0e2958b6198fa1413afaffb33
Umair Ali <umairmamoor@gmail.com>
2025-04-15 04:44:29 -0400

    creating two courses and a list of 1000 students that randomly takes these courses

--- ComprehensiveJavaStreams/JavaOptionals/src/dev/lpa/MainOptional.java
+++ ComprehensiveJavaStreams/JavaOptionals/src/dev/lpa/MainOptional.java
@@ -1,9 +1,19 @@
 package dev.lpa;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class MainOptional {
 
 
     public static void main(String[] args) {
+
+        Course pymc = new Course("PYMC", "Python MasterClass");
+        Course jmc = new Course("JMC", "Java MasterClass");
+
+        List<Student> students = Stream.generate(() -> Student.getRandomStudent(pymc,jmc))
+                .limit(1000)
+                .toList();
 
     }
 }
