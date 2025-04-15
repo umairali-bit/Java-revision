8f28c78a8db82b9820b5992cf52a5b53294f6f4a
Umair Ali <umairmamoor@gmail.com>
2025-04-15 12:29:09 -0400

    creating two courses and list of students

--- ComprehensiveJavaStreams/MasteringStreamTerminal/src/dev/lpa/MainTerminalOptional.java
+++ ComprehensiveJavaStreams/MasteringStreamTerminal/src/dev/lpa/MainTerminalOptional.java
@@ -1,4 +1,21 @@
 package dev.lpa;
 
+import java.util.List;
+import java.util.stream.Stream;
+
 public class MainTerminalOptional {
+
+    public static void main(String[] args) {
+
+
+        Course pymc = new Course("PYMC", "Python MasterClass");
+        Course jmc = new Course("JMC", "Java Masterclass");
+
+        List<Student> students =
+                Stream.generate(() -> Student.getRandomStudent(jmc,pymc))
+                        .limit(1000)
+                        .toList();
+
+
+    }
 }
