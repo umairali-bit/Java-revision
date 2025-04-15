f16205e804b40b7fe2caac639f432e0a46bb904f
Umair Ali <umairmamoor@gmail.com>
2025-04-15 04:56:19 -0400

    getStudent() that uses Optioanl methods

--- ComprehensiveJavaStreams/JavaOptionals/src/dev/lpa/MainOptional.java
+++ ComprehensiveJavaStreams/JavaOptionals/src/dev/lpa/MainOptional.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -18,4 +20,23 @@ public class MainOptional {
                 .collect(Collectors.toList()); //because we want to modify the list
 
     }
+
+// private static method that returns Optional with a type argument of Student. It will take a List of Students,
+// as well as String for the type of retrieval, which can be first last or any.
+    private static Optional<Student> getStudent(List<Student> list, String type) {
+
+        if (list == null || list.size() == 0) {
+ //           return null;
+            return Optional.empty();
+        } else if (type.equals("first")) {
+            return Optional.of(list.get(0));
+        } else if (type.equals("last")) {
+            return Optional.of(list.get(list.size() - 1));
+        }
+
+        return Optional.of(list.get(new Random().nextInt(list.size())));
+
+    }
+
+
 }
