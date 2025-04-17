b56134caf08054a76d175d553fc631048b893e16
Umair Ali <umairmamoor@gmail.com>
2025-04-17 06:27:10 -0400

    creating three courses and randomly generating 5000 students taking 2 of them

--- ComprehensiveJavaStreams/ChallengeAdvancedStream/src/dev/lpa/MainMapping.java
+++ ComprehensiveJavaStreams/ChallengeAdvancedStream/src/dev/lpa/MainMapping.java
@@ -1,4 +1,20 @@
 package dev.lpa;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class MainMapping {
+
+    public static void main(String[] args) {
+
+
+        Course pymc = new Course("PYMC", "Python MasterClass", 50);
+        Course jmc = new Course("JMC", "Java MasterClass", 100);
+        Course jgames = new Course ("JGame", "Creating Games in Java");
+
+        List<Student> students = IntStream
+                .rangeClosed(1, 5000)
+                .mapToObj(s -> Student.getRandomStudent(jmc,pymc))
+                .toList();
+    }
 }
