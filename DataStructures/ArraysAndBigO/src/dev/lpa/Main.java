803597ad54c04544730c92aa6b424f264eb04fec
Umair Ali <umairmamoor@gmail.com>
2025-04-24 18:52:35 -0400

    initializing of classic array with 7 slots, adding values to the 7 indices

--- DataStructures/ArraysAndBigO/src/dev/lpa/Main.java
+++ DataStructures/ArraysAndBigO/src/dev/lpa/Main.java
@@ -4,5 +4,23 @@ public class Main {
 
     public static void main(String[] args) {
 
+//        review of Array
+
+        int[] intArray = new int[7];
+
+        intArray[0] = 20;
+        intArray[1] = 35;
+        intArray[2] = -15;
+        intArray[3] = 7;
+        intArray[4] = 55;
+        intArray[5] = 1;
+        intArray[6] = -22;
+
+
+
+
+
+
+
     }
 }
