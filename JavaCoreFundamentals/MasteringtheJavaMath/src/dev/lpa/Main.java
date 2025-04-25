8440a38c373ae33b1c4d47f8ae766810ee062c26
Umair Ali <umairmamoor@gmail.com>
2025-04-25 05:15:57 -0400

    maxValue less than 5, a for loop to print out ids

--- JavaCoreFundamentals/MasteringtheJavaMath/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/MasteringtheJavaMath/src/dev/lpa/Main.java
@@ -3,5 +3,11 @@ package dev.lpa;
 public class Main {
     public static void main(String[] args) {
 
+        int maxValue = Integer.MAX_VALUE - 5;
+
+        for (int j = 0, id = maxValue; j < 10; j++, id++) {
+            System.out.printf("Assigning id %,d%n", id);
+        }
+
     }
 }
