51afdf2b21574e79e050d9a5e9bc33e53cd25f23
Umair Ali <umairmamoor@gmail.com>
2025-04-25 17:09:42 -0400

    deleting an element from the array

--- DataStructures/ArraysAndBigO/src/dev/lpa/Main.java
+++ DataStructures/ArraysAndBigO/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -32,6 +34,18 @@ public class Main {
         System.out.println("index = " + index);
 
 //    time complexity is O(n) here when we are retrieving an index
+
+//      deleting an element from an array
+        int[] nums = {10, 20, 30, 40, 50};
+        int indexToRemove = 2; // remove the element 30
+        System.out.println(Arrays.toString(nums));
+
+
+        for (int i = indexToRemove; i < nums.length - 1; i++) {
+            nums[i] = nums[i + 1];
+        }
+
+        System.out.println(Arrays.toString(nums));
 
 
 
