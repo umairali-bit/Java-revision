617e5c83f2e623ec9450c514ce67941baf8ea7c9
Umair Ali <umairmamoor@gmail.com>
2025-04-26 08:01:08 -0400

    random;y generating numbers between 1 to 10 and casting it to int

--- JavaCoreFundamentals/ComphrehensiveGuidToRandom/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/ComphrehensiveGuidToRandom/src/dev/lpa/Main.java
@@ -1,4 +1,71 @@
 package dev.lpa;
 
+
 public class Main {
+    public static void main(String[] args) {
+
+        int maxValue = Integer.MAX_VALUE - 5;
+
+//      Math.incrementExact(id)
+        for (int j = 0, id = maxValue; j < 10; j++, id ++ ) //id = Math.incrementExact(id)) {
+        {
+            System.out.printf("Assigning id %,d%n", id);
+        }
+
+
+//      abs, returns the magnitude of a number ignoring its sign.
+        System.out.println(Math.abs(-50));
+
+//        getting the Integer MinValue
+        System.out.println(Math.abs(Integer.MIN_VALUE)); // -2147483648
+        System.out.println(Math.abs((long) Integer.MIN_VALUE)); //2147483648
+
+
+        System.out.println("Max = " + Math.max(10, -10));
+        System.out.println("Min = " + Math.min(10.0000002, 10.001));
+        System.out.println("Min = " + Math.min(10.0000002f, 10.001f));
+
+
+        System.out.println("Round Down = " + Math.round(10.2));
+        System.out.println("Round Up = " + Math.round(10.8));
+        System.out.println("Round ? = " + Math.round(10.5));
+
+//      Floor will always round up, Ceil will always round down
+        System.out.println("Floor = " + Math.floor(10.8));
+        System.out.println("Ceil = " + Math.ceil(10.2));
+
+//      sqrt, and pow methods
+        System.out.println("Square root of 100 = " + Math.sqrt(100));
+        System.out.println("2 to the third power (2x2x2) = " + Math.pow(2,3));
+        System.out.println("10 to the fifth power (10x10x10x10x10) = " + Math.pow(10,5));
+
+
+//      Math.random()
+        for (int i = 0; i < 10; i ++) {
+            System.out.println(Math.random());
+
+        }
+
+        /*
+        0.9524069173210151
+        0.9560177482366994
+        0.6761617916731141
+        0.7878079734145225
+        0.9389765549066796
+        0.606371867885008
+        0.1971184466989454
+        0.8465325352321285
+         */
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.print((int) (Math.random() * 10) + " "); //0 2 3 5 9 4 5 8 3 0
+        }
+
+
+
+
+
+
+    }
 }
