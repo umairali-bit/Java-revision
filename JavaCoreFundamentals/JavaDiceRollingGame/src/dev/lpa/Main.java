e9126e4a30f75e490906a05ab675e091cc03d2bf
Umair Ali <umairmamoor@gmail.com>
2025-04-28 08:13:42 -0400

    rollDice() creating a newDice var that generates random integers for dice between 1 to 7, testing the method in main

--- JavaCoreFundamentals/JavaDiceRollingGame/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/JavaDiceRollingGame/src/dev/lpa/Main.java
@@ -16,11 +16,33 @@ public class Main {
 //   for dice using a list
         List<Integer> currentDice = new ArrayList<>();
 
+        int rolls = 0;
+        do {
+            rollDice(currentDice);
+            currentDice.clear();
+        } while (++rolls < 5);
+
+
+
     }
 
 
 //    for rolling and re-rolling dice
     private static void rollDice(List<Integer> currentDice) {
+
+        int randomCount = 5 - currentDice.size();// you need to roll 5 dice
+
+        var newDice = random
+                .ints(randomCount, 1, 7)
+                .boxed()
+                .toList();
+
+        currentDice.addAll(newDice);
+
+        System.out.println("Random dice number: " + currentDice);
+
+
+
 
     }
 }
