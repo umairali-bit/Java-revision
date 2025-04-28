02e846dacac9372b1a9d2d939fc5edd35bd7d152
Umair Ali <umairmamoor@gmail.com>
2025-04-28 07:48:58 -0400

    currentDice list and a method to roll and re-roll the dice

--- JavaCoreFundamentals/JavaDiceRollingGame/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/JavaDiceRollingGame/src/dev/lpa/Main.java
@@ -1,15 +1,26 @@
 package dev.lpa;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-//    two static variables
+//  two static variables
     private static final Random random = new Random();
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+
+//   for dice using a list
+        List<Integer> currentDice = new ArrayList<>();
+
+    }
+
+
+//    for rolling and re-rolling dice
+    private static void rollDice(List<Integer> currentDice) {
 
     }
 }
