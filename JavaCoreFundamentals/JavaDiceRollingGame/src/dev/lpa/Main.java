9ac3f9d6807bfde5a08de84d4cf2c215b305720c
Umair Ali <umairmamoor@gmail.com>
2025-04-28 07:42:35 -0400

    two private final static vairables random and scanner

--- JavaCoreFundamentals/JavaDiceRollingGame/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/JavaDiceRollingGame/src/dev/lpa/Main.java
@@ -1,6 +1,13 @@
 package dev.lpa;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+
+//    two static variables
+    private static final Random random = new Random();
+    private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
