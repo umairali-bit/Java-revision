5c46a649cdb507e1fd0c3608b185d58b15b9f8c5
Umair Ali <umairmamoor@gmail.com>
2025-04-30 07:03:56 -0400

    writing code in main for the ouuter, inner loops

--- DataStructures/SortAlgorithms/src/dev/lpa/BubbleSort/BubbleMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/BubbleSort/BubbleMain.java
@@ -4,15 +4,26 @@ public class BubbleMain {
 
     public static void main(String[] args) {
 
+       int[] intArray = {20, 35, -15, 7, 55, 1 , -22};
+
+//       lastUnsortedIndex = 6;
+       for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+           for (int i = 0; i < lastUnsortedIndex; i ++) {
+
+               if (intArray[i] > intArray[i + 1]) {
+                   swap(intArray, i, i + 1);
+               }
+           }
+       }
+
+       for (int i = 0; i< intArray.length; i++) {
+           System.out.println(intArray[i]);
+       }
+
     }
 
-
-
-
-
-
-
-// implementation of the bubbleSort requires a swap()
+//     implementation of the bubbleSort requires a swap()
 
 public static void swap(int[] array, int i, int j) {
 
