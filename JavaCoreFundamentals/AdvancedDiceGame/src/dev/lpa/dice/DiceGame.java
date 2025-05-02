4b76298dc52d3c07c00096084576251a8da791e3
Umair Ali <umairmamoor@gmail.com>
2025-05-02 07:31:41 -0400

    implementing required methods from Game

--- JavaCoreFundamentals/AdvancedDiceGame/src/dev/lpa/dice/DiceGame.java
+++ JavaCoreFundamentals/AdvancedDiceGame/src/dev/lpa/dice/DiceGame.java
@@ -1,6 +1,23 @@
 package dev.lpa.dice;
 
 import dev.lpa.game.Game;
+import dev.lpa.game.GameAction;
+
+import java.util.Map;
 
 public class DiceGame extends Game<DicePlayer> {
+
+    public DiceGame(String gameName) {
+        super(gameName);
+    }
+
+    @Override
+    public DicePlayer createNewPlayer(String name) {
+        return new DicePlayer();
+    }
+
+    @Override
+    public Map<Character, GameAction> getGameActions(int playerIndex) {
+        return null;
+    }
 }
