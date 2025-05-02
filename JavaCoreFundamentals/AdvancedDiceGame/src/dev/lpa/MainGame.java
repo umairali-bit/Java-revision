0a7f77852f59906b53350d319e2b79552313fd11
Umair Ali <umairmamoor@gmail.com>
2025-05-02 07:16:01 -0400

    testing the enum

--- JavaCoreFundamentals/AdvancedDiceGame/src/dev/lpa/MainGame.java
+++ JavaCoreFundamentals/AdvancedDiceGame/src/dev/lpa/MainGame.java
@@ -1,8 +1,20 @@
 package dev.lpa;
 
+import dev.lpa.dice.ScoredItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainGame {
 
     public static void main(String[] args) {
+
+        List<Integer> currentDice = new ArrayList<>(
+                List.of(2,2,4,4,4));
+
+        for (ScoredItem s : ScoredItem.values()) {
+            System.out.printf("Score for %s is %d %n", s, s.score(currentDice));
+        }
 
     }
 }
