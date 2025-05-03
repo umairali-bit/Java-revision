d7ec9a190d0d03922ed487366b60a469b5a023ec
Umair Ali <umairmamoor@gmail.com>
2025-05-03 16:29:40 -0400

    testing in main

--- JavaCoreFundamentals/AdvancedDiceGame/src/dev/lpa/MainGame.java
+++ JavaCoreFundamentals/AdvancedDiceGame/src/dev/lpa/MainGame.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
+import dev.lpa.dice.DiceGame;
 import dev.lpa.dice.ScoredItem;
+import dev.lpa.game.GameConsole;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,12 +11,17 @@ public class MainGame {
 
     public static void main(String[] args) {
 
-        List<Integer> currentDice = new ArrayList<>(
-                List.of(2,2,4,4,4));
+//        List<Integer> currentDice = new ArrayList<>(
+//                List.of(2,2,4,4,4));
+//
+//        for (ScoredItem s : ScoredItem.values()) {
+//            System.out.printf("Score for %s is %d %n", s, s.score(currentDice));
+//        }
 
-        for (ScoredItem s : ScoredItem.values()) {
-            System.out.printf("Score for %s is %d %n", s, s.score(currentDice));
-        }
+
+        var console = new GameConsole<> (new DiceGame("Dice Rolling Game"));
+        console.playGame(console.addPlayer());
+
 
     }
 }
