e4515b1375d73a5c6b0e676089d7b247071e5f76
Umair Ali <umairmamoor@gmail.com>
2025-05-04 06:10:36 -0400

    implementation of SelectSort

--- DataStructures/SortAlgorithms/src/dev/lpa/SelectionSort/SelectionSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/SelectionSort/SelectionSortMain.java
@@ -6,19 +6,21 @@ public class SelectionSortMain {
 
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
-
         for (int lastUnsortedIndex = intArray.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
 
             int largestIndex = 0;
 
             for (int i = 1; i <= lastUnsortedIndex; i ++) {
-
-                if (intArray[i] > largestIndex) {
+                if (intArray[i] > intArray[largestIndex]) {
                     largestIndex = i;
                 }
             }
 
+            swap(intArray, largestIndex,lastUnsortedIndex);
+        }
 
+        for (int i = 0; i <intArray.length; i ++) {
+            System.out.print(intArray[i] + " ");
         }
 
     }
