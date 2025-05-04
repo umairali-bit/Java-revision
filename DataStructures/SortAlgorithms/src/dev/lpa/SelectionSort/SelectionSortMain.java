aae03cbad8413a67a7148ec5b5b033a0a0c49738
Umair Ali <umairmamoor@gmail.com>
2025-05-04 05:56:06 -0400

    swap() for selection sort

--- DataStructures/SortAlgorithms/src/dev/lpa/SelectionSort/SelectionSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/SelectionSort/SelectionSortMain.java
@@ -2,7 +2,38 @@ package dev.lpa.SelectionSort;
 
 public class SelectionSortMain {
 
-    int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+    public static void main(String[] args) {
+
+
+        int[] intArray = { 20, 35, -15, 7, 55, 1, -22 };
+
+        for (int lastUnsortedIndex = intArray.length -1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+
+            int largestIndex = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i ++) {
+
+                if (intArray[i] > largestIndex) {
+                    largestIndex = i;
+                }
+            }
+
+
+        }
+
+    }
+
+    public static void swap (int[] arr, int i, int j) {
+
+        if (i == j) {
+            return;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+
+    }
 
 
 
