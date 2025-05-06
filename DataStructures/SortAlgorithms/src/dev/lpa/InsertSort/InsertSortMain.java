2ad822311752fdb1a4688b5fba7726ca32655ac9
Umair Ali <umairmamoor@gmail.com>
2025-05-05 21:11:15 -0400

    priting out the array

--- DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertSortMain.java
@@ -6,5 +6,12 @@ public class InsertSortMain {
 
         int[] intArray = { 20, 25, -15, 7,55, 1, -22};
 
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+
+
+
     }
 }
