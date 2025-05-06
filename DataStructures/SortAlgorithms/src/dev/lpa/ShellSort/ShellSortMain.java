93e115c9c18a5ba5d301f5e60c329f79d7803bd1
Umair Ali <umairmamoor@gmail.com>
2025-05-06 07:53:12 -0400

    outer loop, initializing gap valuue and reducing it to each iteration

--- DataStructures/SortAlgorithms/src/dev/lpa/ShellSort/ShellSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/ShellSort/ShellSortMain.java
@@ -8,6 +8,16 @@ public class ShellSortMain {
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
 
 
+//        initialize gap value and reduce it to each iteration
+        for (int gap = intArray.length / 2; gap > 0; gap /=2) {
+
+        }
+
+
+
+
+
+
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i] + " ");
         }
