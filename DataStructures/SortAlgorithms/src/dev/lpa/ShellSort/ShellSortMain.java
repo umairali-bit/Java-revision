7e59cba71b4a70c1a4941bc05584b01f7b3e406f
Umair Ali <umairmamoor@gmail.com>
2025-05-06 07:41:23 -0400

    for loop to print out intArray

--- DataStructures/SortAlgorithms/src/dev/lpa/ShellSort/ShellSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/ShellSort/ShellSortMain.java
@@ -6,5 +6,10 @@ public class ShellSortMain {
 
 
         int[] intArray = { 20, 35, -15, 7, 55, 1, -22};
+
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i] + " ");
+        }
     }
 }
