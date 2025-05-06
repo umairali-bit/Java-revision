28c784ad4d22e8adf5afd67f08fabb3749d716ff
Umair Ali <umairmamoor@gmail.com>
2025-05-05 21:16:31 -0400

    outerloop

--- DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertSortMain.java
@@ -6,6 +6,12 @@ public class InsertSortMain {
 
         int[] intArray = { 20, 25, -15, 7,55, 1, -22};
 
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; i++) {
+
+        }
+
+
+
 
         for (int i = 0; i < intArray.length; i++) {
             System.out.println(intArray[i]);
