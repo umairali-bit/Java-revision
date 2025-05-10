dbafb4c7400868234b681245de89b99c35927689
Umair Ali <umairmamoor@gmail.com>
2025-05-10 06:52:33 -0400

    method to calculate factorial without recursion - first condition, num has to be greater than 1

--- DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortMain.java
@@ -4,5 +4,19 @@ public class MergeSortMain {
 
     public static void main(String[] args) {
 
+
+
+
+
     }
+
+//  method to calculate factorial in an iterative fashion - not using recursion
+
+    public static int iterativeFactorial (int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+    }
+
 }
