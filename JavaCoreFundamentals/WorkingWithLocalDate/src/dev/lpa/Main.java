c5814286cd09d7a7f8e56cf10969cb476df14d2e
Umair Ali <umairmamoor@gmail.com>
2025-05-10 07:34:09 -0400

    printing out todays date

--- JavaCoreFundamentals/WorkingWithLocalDate/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/WorkingWithLocalDate/src/dev/lpa/Main.java
@@ -1,8 +1,16 @@
 package dev.lpa;
 
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+
+//        getting todays date
+
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+
 
     }
 }
