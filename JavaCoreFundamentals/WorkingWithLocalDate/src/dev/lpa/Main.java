adc99ffbf022ff2bac4a97e15def852f4589a1a2
Umair Ali <umairmamoor@gmail.com>
2025-05-10 07:37:02 -0400

    format 2025-05-05

--- JavaCoreFundamentals/WorkingWithLocalDate/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/WorkingWithLocalDate/src/dev/lpa/Main.java
@@ -7,9 +7,14 @@ public class Main {
     public static void main(String[] args) {
 
 //        getting todays date
-
         LocalDate today = LocalDate.now();
         System.out.println(today);
+
+//        Dates in other formats
+//        2025-05-05
+        LocalDate five5 = LocalDate.of(2025, 5,5);
+        System.out.println(five5);
+
 
 
     }
