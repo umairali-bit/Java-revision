6158e52e35e7f3b9d25e13321137f2a7d09852c7
Umair Ali <umairmamoor@gmail.com>
2025-05-10 07:17:23 -0400

    recursiveFactorial method

--- DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortMain.java
@@ -10,6 +10,20 @@ public class MergeSortMain {
 
     }
 
+
+//    n! = n * (n - 1)! - recursive method to calculate the factorial ()
+
+    public static int recursiveFactorial (int num) {
+
+        if (num == 0) {
+            return 1;
+        }
+
+        return num * recursiveFactorial(num - 1);
+
+    }
+
+
 //  method to calculate factorial in an iterative fashion - not using recursion
 
     public static int iterativeFactorial (int num) {
