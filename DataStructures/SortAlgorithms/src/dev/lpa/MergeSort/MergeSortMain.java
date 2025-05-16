6e280e0f3c82af00b8ba47381ed68fcfb67a4438
Umair Ali <umairmamoor@gmail.com>
2025-05-16 07:10:28 -0400

    printArray[]

--- DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortMain.java
@@ -63,5 +63,14 @@ public class MergeSortMain {
 
     }
 
+//  printing results
+    private static void printArray (int[] numbers) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+    }
+
 
 }
