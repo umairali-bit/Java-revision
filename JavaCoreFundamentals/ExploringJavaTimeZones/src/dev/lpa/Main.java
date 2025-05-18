75afd97d4b948b19a689583e26f9b15cddb97fb0
Umair Ali <umairmamoor@gmail.com>
2025-05-18 07:11:17 -0400

    Zone ID class gives the time zone you are in

--- JavaCoreFundamentals/ExploringJavaTimeZones/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/ExploringJavaTimeZones/src/dev/lpa/Main.java
@@ -1,4 +1,13 @@
 package dev.lpa;
 
+import java.time.ZoneId;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+//  Zone ID class gives the time zone you are in
+        System.out.println(ZoneId.systemDefault());
+
+    }
 }
