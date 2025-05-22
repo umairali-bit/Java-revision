03e750912dc4eb985012a157792ab082369923ec
Umair Ali <umairmamoor@gmail.com>
2025-05-22 06:16:34 -0400

    quick sort implementation

---
+++ DataStructures/SortAlgorithms/src/dev/lpa/QuickSort/QuickSortMain.java
@@ -0,0 +1,16 @@
+package dev.lpa.QuickSort;
+
+public class QuickSortMain {
+
+    public static void main(String[] args) {
+        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+    }
+
+    public static void printArray(int[] numbers) {
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+
+    }
+}
