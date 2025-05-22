6d01a10220ccad14e6f6a3d85e83d949c428e0ef
Umair Ali <umairmamoor@gmail.com>
2025-05-22 06:56:18 -0400

    generatingNumbers() and PrintArrayMethod()

--- DataStructures/SortAlgorithms/src/dev/lpa/QuickSort/QuickSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/QuickSort/QuickSortMain.java
@@ -1,16 +1,42 @@
 package dev.lpa.QuickSort;
 
+import java.util.Random;
+
 public class QuickSortMain {
 
     public static void main(String[] args) {
-        int[] intArray = {20, 35, -15, 7, 55, 1, -22};
+
+//        int[] randomArray = generatingNumbers();
+//        printArray(randomArray);
+
+        printArray();
+
+
     }
 
-    public static void printArray(int[] numbers) {
+    public static int[]  generatingNumbers () {
+
+        Random rand = new Random();
+        int[] numbers = new int[10];
 
         for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
+            numbers[i] = rand.nextInt(100);
+
         }
+        return numbers;
+
+
+    }
+
+    public static void printArray() {
+
+        int[] array = generatingNumbers();
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+
 
     }
 }
