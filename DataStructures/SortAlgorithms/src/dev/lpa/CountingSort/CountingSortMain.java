56f5d1e405c9b19239b1b359ed283781cef467f6
Umair Ali <umairmamoor@gmail.com>
2025-05-23 11:37:58 -0400

    generatingNumbers() and Countingsort()

--- DataStructures/SortAlgorithms/src/dev/lpa/CountingSort/CountingSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/CountingSort/CountingSortMain.java
@@ -1,15 +1,62 @@
 package dev.lpa.CountingSort;
 
+import java.util.Random;
+
 public class CountingSortMain {
 
 
     public static void main(String[] args) {
 
+        int[] intArray = generatingNumbers();
+
+        System.out.println("Before");
+        printArray(intArray);
+
+        countingSort(intArray, 0, intArray.length-1);
+
+        System.out.println("\nAfter");
+        printArray(intArray);
+
+    }
+
+    private static void countingSort(int[] input, int min, int max) {
+
+//  count array with the size of the actual array
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < input.length; i++) {
+//  to count each value
+           countArray[input[i] - min]++;
+        }
+
+//  writing values back to the array
+    int j = 0;
+        for (int i = min; i <=max; i++) {
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+
+            }
+        }
+
+    }
+
+
+    public static int[] generatingNumbers () {
+        Random random = new Random();
+        int[] numbers = new int[10];
+
+        for (int i = 0; i < numbers.length; i ++) {
+            numbers[i] = random.nextInt(10);
+
+        }
+        return numbers;
+
     }
 
 
 
-    public static void printArray(){
+    public static void printArray (int[] intArray){
 
         for (int i = 0; i < intArray.length; i ++) {
             System.out.print(intArray[i] + " ");
