37dabdfdbb49caeb385cfb116f6358011ba1ffdb
Umair Ali <umairmamoor@gmail.com>
2025-05-23 11:06:29 -0400

    Counting Sort implementation

---
+++ DataStructures/SortAlgorithms/src/dev/lpa/CountingSort/CountingSortMain.java
@@ -0,0 +1,24 @@
+package dev.lpa.CountingSort;
+
+public class CountingSortMain {
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static void printArray(){
+
+        for (int i = 0; i < intArray.length; i ++) {
+            System.out.print(intArray[i] + " ");
+        }
+
+    }
+
+
+}
+
+
+
