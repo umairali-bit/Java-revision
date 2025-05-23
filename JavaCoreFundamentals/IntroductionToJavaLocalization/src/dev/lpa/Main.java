2e64733782f2f95b3c12ac2e71c5aed2a15e72e7
Umair Ali <umairmamoor@gmail.com>
2025-05-23 17:01:14 -0400

    creating locales

--- JavaCoreFundamentals/IntroductionToJavaLocalization/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/IntroductionToJavaLocalization/src/dev/lpa/Main.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 import java.util.Locale;
 
 public class Main {
@@ -10,6 +12,17 @@ public class Main {
 //  Changing default by using set default
         Locale.setDefault(Locale.UK);
         System.out.println("Default Locale = " + Locale.getDefault());
+
+//  Creating Locales
+        Locale en = new Locale("en");
+        Locale enAU = new Locale("en", "AU");
+        Locale enCA = new Locale("en", "CA");
+
+        Locale enIN = new Locale.Builder().setLanguage("en").setRegion("IN").build();
+        Locale enNZ = new Locale.Builder().setLanguage("en").setRegion("NZ").build();
+
+
+
 
 
 
