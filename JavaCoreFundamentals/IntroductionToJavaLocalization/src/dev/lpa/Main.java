c43945ef1e7c74323e7ed2d6def5ab0ab718a5d1
Umair Ali <umairmamoor@gmail.com>
2025-05-23 16:50:03 -0400

    Default Locale

---
+++ JavaCoreFundamentals/IntroductionToJavaLocalization/src/dev/lpa/Main.java
@@ -0,0 +1,14 @@
+package dev.lpa;
+
+import java.util.Locale;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//  Default Locale
+
+        System.out.println("Default Locale " + Locale.getDefault());
+
+    }
+}
