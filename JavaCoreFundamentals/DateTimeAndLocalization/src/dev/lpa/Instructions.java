b86cecd328e840cb5b3b00c04ccb7c7109db0d82
Umair Ali <umairmamoor@gmail.com>
2025-05-25 19:05:50 -0400

    private record with custom constructors and a method

---
+++ JavaCoreFundamentals/DateTimeAndLocalization/src/dev/lpa/Instructions.java
@@ -0,0 +1,24 @@
+package dev.lpa;
+
+public class Instructions {
+
+    /*
+    Scheduling a meeting for people in different areas of the world
+
+Imagine you work for a company that has employees from around the world, and you need to find an hour of time to schedule
+two employees to meet.The meeting should be on a work day, Monday through Friday, and between the hours of 7 am and 9 pm
+(meaning the meeting should not be scheduled any later than 8 pm) for either employee.One of the employees is on the East
+Coast of the United States, the other is in Sydney, Australia.Print all the possible hours in the upcoming 10 day range,
+ ignoring the current day, when these two employees could potentially meet together.
+
+Print the date and time of each possible hour, for both zones, using the employee's locale.
+The example output on this slide, demonstrates what one possible hour would look like, and the dates formatted to the
+employee's locale.
+Include the week day in the output, and the hour using am or pm, rather than a 24 hour format.
+Jane [America/New_York] : Wednesday, June 14, 2023, 5:00 PM <---> Joe [Australia/Sydney] : Thursday, 15 June 2023, 7:00 am
+
+
+
+
+     */
+}
--- JavaCoreFundamentals/DateTimeAndLocalization/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/DateTimeAndLocalization/src/dev/lpa/Main.java
@@ -1,4 +1,30 @@
 package dev.lpa;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
 public class Main {
+
+    private record Employee(String name, Locale locale, ZoneId zone) {
+
+        public Employee(String name, String locale, String zone) {
+           this(name, Locale.forLanguageTag(locale), ZoneId.of(zone));
+        }
+
+        public Employee(String name, Locale locale, String zone) {
+            this(name, locale, ZoneId.of(zone));
+        }
+
+        String getDateInfo(ZonedDateTime zdt, DateTimeFormatter dtf) {
+            return "%s [%s] : %s".formatted(name, zone, zdt.format(dtf.localizedBy(locale)));
+        }
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
 }
