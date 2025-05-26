c6be63e7ae0e83412af14e1c39ec135f66fb6feb
Umair Ali <umairmamoor@gmail.com>
2025-05-26 07:11:29 -0400

    generateArray()

--- DataStructures/SortAlgorithms/src/dev/lpa/RadixSort/RadixSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/RadixSort/RadixSortMain.java
@@ -1,9 +1,30 @@
 package dev.lpa.RadixSort;
 
+import java.util.Random;
+
 public class RadixSortMain {
 
 
     public static void main(String[] args) {
+
+        int[] newArray = generateArray();
+
+        System.out.println("Before");
+        printArray(newArray);
+
+    }
+
+
+    public static int[] generateArray() {
+
+        Random random = new Random(10);
+        int[] array = new int [10];
+
+        for (int i = 0; i <array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+
+        return array;
 
     }
 
