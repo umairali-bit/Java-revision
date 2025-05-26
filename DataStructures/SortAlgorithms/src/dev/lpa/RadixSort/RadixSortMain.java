eb8e1e57cf94726b39e58a20d8675e432518a56d
Umair Ali <umairmamoor@gmail.com>
2025-05-26 07:05:10 -0400

    printArray method

--- DataStructures/SortAlgorithms/src/dev/lpa/RadixSort/RadixSortMain.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/RadixSort/RadixSortMain.java
@@ -1,4 +1,17 @@
 package dev.lpa.RadixSort;
 
 public class RadixSortMain {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void printArray(int[] array) {
+
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+
+    }
 }
