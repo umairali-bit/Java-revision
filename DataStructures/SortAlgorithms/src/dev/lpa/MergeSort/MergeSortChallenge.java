7a0b1074039320dfe10a0407fb3cdf58c4283f1c
Umair Ali <umairmamoor@gmail.com>
2025-05-27 15:51:35 -0400

    merge sort challenge

---
+++ DataStructures/SortAlgorithms/src/dev/lpa/MergeSort/MergeSortChallenge.java
@@ -0,0 +1,14 @@
+/*
+Modify the merge sort algorithm so that it sorts integers in DESCENDING order
+Use the usual example array
+
+ */
+
+package dev.lpa.MergeSort;
+
+public class MergeSortChallenge {
+
+    public static void main(String[] args) {
+
+    }
+}
