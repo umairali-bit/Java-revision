dce491d31c9614c46bb54a269267b77f2ffc0800
Umair Ali <umairmamoor@gmail.com>
2025-05-27 03:46:26 -0400

    creating a ResouceBundle variable

--- JavaCoreFundamentals/JavaInternationalization/src/dev/lpa/Main.java
+++ JavaCoreFundamentals/JavaInternationalization/src/dev/lpa/Main.java
@@ -1,4 +1,14 @@
 package dev.lpa;
 
+import java.util.ResourceBundle;
+
 public class Main {
+
+    public static void main(String[] args) {
+
+// creating a ResourceBundle variable
+        ResourceBundle rb = ResourceBundle.getBundle("BasicText");
+
+
+    }
 }
