1204a1890830fd6f68d6ed3a41d5bbd620a9a340
Umair Ali <umairmamoor@gmail.com>
2025-05-29 09:10:35 -0400

    generatingArray()

--- DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertionSortChallenge.java
+++ DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertionSortChallenge.java
@@ -9,14 +9,25 @@ public class InsertionSortChallenge {
 
     public static void main(String[] args) {
 
+        int[] numbers = generateArray();
+
+        printArray(numbers);
+
     }
 
+
+    public static int[] generateArray(){
+
+        int[] numbers = { 20, 25, -15, 7,55, 1, -22};
+
+        return numbers;
+    }
 
 
     public static void printArray(int[] numbers) {
 
         for (int i = 0; i< numbers.length; i ++) {
-            System.out.println(numbers[i] + " ");
+            System.out.print(numbers[i] + " ");
         }
     }
 }
