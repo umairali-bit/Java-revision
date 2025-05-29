1f055fafc76692951da6d8b9b460c4be6336341d
Umair Ali <umairmamoor@gmail.com>
2025-05-29 09:06:13 -0400

    printing the array for the insertion sort challenge

---
+++ DataStructures/SortAlgorithms/src/dev/lpa/InsertSort/InsertionSortChallenge.java
@@ -0,0 +1,22 @@
+package dev.lpa.InsertSort;
+
+/*
+Change insertion sort so that it uses recursion
+Sort the usual example array
+ */
+
+public class InsertionSortChallenge {
+
+    public static void main(String[] args) {
+
+    }
+
+
+
+    public static void printArray(int[] numbers) {
+
+        for (int i = 0; i< numbers.length; i ++) {
+            System.out.println(numbers[i] + " ");
+        }
+    }
+}
