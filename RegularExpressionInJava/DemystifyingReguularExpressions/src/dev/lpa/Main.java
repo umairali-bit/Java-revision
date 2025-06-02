c3cd256cf3382498d4fce56cfb13db27ed0e7a60
Umair Ali <umairmamoor@gmail.com>
2025-06-02 04:29:49 -0400

    printing out hello world with string's formatted method and uusing String.format

--- RegularExpressionInJava/DemystifyingReguularExpressions/src/dev/lpa/Main.java
+++ RegularExpressionInJava/DemystifyingReguularExpressions/src/dev/lpa/Main.java
@@ -1,4 +1,12 @@
 package dev.lpa;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        String helloWorld = "%s %s".formatted("Hello", "World");
+        String helloWorld2 = String.format("%s %s", "Hello", "World");
+        System.out.println("Using string's formatted method: " + helloWorld);
+        System.out.println("Using String.format: " + helloWorld2);
+    }
 }
