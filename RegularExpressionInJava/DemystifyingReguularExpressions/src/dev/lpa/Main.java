ab2a93c03bd279e667dd242425ef6dc4a617fa25
Umair Ali <umairmamoor@gmail.com>
2025-06-02 04:39:45 -0400

    format() that replaces %s with the next arg

--- RegularExpressionInJava/DemystifyingReguularExpressions/src/dev/lpa/Main.java
+++ RegularExpressionInJava/DemystifyingReguularExpressions/src/dev/lpa/Main.java
@@ -8,5 +8,18 @@ public class Main {
         String helloWorld2 = String.format("%s %s", "Hello", "World");
         System.out.println("Using string's formatted method: " + helloWorld);
         System.out.println("Using String.format: " + helloWorld2);
+
+        String helloWorld3 = Main.format("%s %s", "Hello", "World");
+        System.out.println(helloWorld3);
+    }
+
+    private static String format(String regexp, String... args) {
+
+        int index = 0;
+        while(regexp.contains("%s")) {
+            regexp = regexp.replaceFirst("%s", args[index++]);
+        }
+
+        return regexp;
     }
 }
