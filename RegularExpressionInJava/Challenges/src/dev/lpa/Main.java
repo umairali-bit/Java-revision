6c1c103ea32a735a3b981873daf753c065e748d8
Umair Ali <umairmamoor@gmail.com>
2025-06-05 06:07:12 -0400

    Write a regular expression that matches the exact sentence "Hello, World!". Use the matches method on String to check if the input sentence matches this pattern.

--- RegularExpressionInJava/Challenges/src/dev/lpa/Main.java
+++ RegularExpressionInJava/Challenges/src/dev/lpa/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        String helloWorld = "%s %s".formatted("Hello,","World!");
+        System.out.println(helloWorld);
+
+        boolean isMatch = helloWorld.matches("Hello, World!");
+        System.out.println(isMatch);
+
+
+
     }
 }
