7d005ca6238b33223922d4f6dcabff05c17ae508
Umair Ali <umairmamoor@gmail.com>
2025-06-07 17:54:14 -0400

    matcher class

--- RegularExpressionInJava/AdvancedRegularExpressions/src/dev/lpa/Main.java
+++ RegularExpressionInJava/AdvancedRegularExpressions/src/dev/lpa/Main.java
@@ -5,9 +5,15 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
+        //pattern class
         String sentence = "I like motorcycles.";
         boolean matched = Pattern.matches("[A-Z].*[.]", sentence);
-        System.out.println(matched);
+        System.out.println(matched + ": " + sentence);
+
+        //matcher class
+        Pattern firstPattern = Pattern.compile("[A-Z].*[.]");
+        var matcher = firstPattern.matcher(sentence);
+        System.out.println(matcher.matches() + ": " + sentence);
 
 
     }
