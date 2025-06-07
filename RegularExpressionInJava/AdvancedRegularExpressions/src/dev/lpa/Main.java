0df31e2f497bf69e7cfba7f8aaa5f079fba43782
Umair Ali <umairmamoor@gmail.com>
2025-06-07 17:50:09 -0400

    matching the pattern

--- RegularExpressionInJava/AdvancedRegularExpressions/src/dev/lpa/Main.java
+++ RegularExpressionInJava/AdvancedRegularExpressions/src/dev/lpa/Main.java
@@ -1,7 +1,13 @@
 package dev.lpa;
 
+import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) {
+
+        String sentence = "I like motorcycles.";
+        boolean matched = Pattern.matches("[A-Z].*[.]", sentence);
+        System.out.println(matched);
 
 
     }
