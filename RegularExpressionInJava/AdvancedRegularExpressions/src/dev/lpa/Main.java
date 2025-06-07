6e2eec527b6359835739e74fa35b8b5edf417b9a
Umair Ali <umairmamoor@gmail.com>
2025-06-07 18:18:06 -0400

    matcher.end()

--- RegularExpressionInJava/AdvancedRegularExpressions/src/dev/lpa/Main.java
+++ RegularExpressionInJava/AdvancedRegularExpressions/src/dev/lpa/Main.java
@@ -14,6 +14,9 @@ public class Main {
         Pattern firstPattern = Pattern.compile("[A-Z].*[.]");
         var matcher = firstPattern.matcher(sentence);
         System.out.println(matcher.matches() + ": " + sentence);
+        System.out.println("sentence.length: " + sentence.length());
+        System.out.println("Matched Ending Index: " + matcher.end());
+
 
 
     }
