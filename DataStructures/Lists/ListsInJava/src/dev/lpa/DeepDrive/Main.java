9ac572ebee03dc3c4907c22b80ca878586a39cb7
Umair Ali <umairmamoor@gmail.com>
2025-06-08 16:52:45 -0400

    employee list in main

---
+++ DataStructures/Lists/ListsInJava/src/dev/lpa/DeepDrive/Main.java
@@ -0,0 +1,14 @@
+package dev.lpa.DeepDrive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        List<Employee> employeeList = new ArrayList<>();
+
+
+    }
+}
