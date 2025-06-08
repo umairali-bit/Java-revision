14a7772a4f1275bcaef8da8d9d7f14a3e2abed13
Umair Ali <umairmamoor@gmail.com>
2025-06-08 16:50:21 -0400

    sets, gets, toString()

--- DataStructures/Lists/ListsInJava/src/dev/lpa/DeepDrive/Employee.java
+++ DataStructures/Lists/ListsInJava/src/dev/lpa/DeepDrive/Employee.java
@@ -6,5 +6,42 @@ public class Employee {
     private String lastName;
     private int id;
 
+    public Employee(String firstName, String lastName, int id) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.id = id;
+    }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", id=" + id +
+                '}';
+    }
 }
