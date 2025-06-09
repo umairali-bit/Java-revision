85bfae27c69b23622ea910b8b2bc95b5490824bb
Umair Ali <umairmamoor@gmail.com>
2025-06-09 06:44:54 -0400

    matcher.group()

--- RegularExpressionInJava/ExtractingDatawithMatcher/src/dev/lpa/Main.java
+++ RegularExpressionInJava/ExtractingDatawithMatcher/src/dev/lpa/Main.java
@@ -26,6 +26,9 @@ public class Main {
         System.out.println("Matched Ending Index: " + matcher.end());
         System.out.println("Matched on : " + sentence.substring(matcher.start(), matcher.end()));
 
+        System.out.println("Matched on : " + matcher.group());
+
+
 
 
     }
