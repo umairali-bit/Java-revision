83b7d2145eacdfaa96d9b2b874c29c2772f39945
Umair Ali <umairmamoor@gmail.com>
2025-06-13 06:50:16 -0400

    phonePattern

--- RegularExpressionInJava/InDepthReview/src/dev/lpa/Review.java
+++ RegularExpressionInJava/InDepthReview/src/dev/lpa/Review.java
@@ -1,8 +1,27 @@
 package dev.lpa;
 
+import java.util.regex.Pattern;
+
 public class Review {
 
     public static void main(String[] args) {
+
+
+        String phoneList = """
+                (800) 123-4567
+                (800)123-4567
+                (800) 123 4567
+                800-123-4567
+                800 123-4567
+                8001234567
+                
+                """;
+
+        Pattern phonePattern =
+                Pattern.compile("\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}");
+
+
+
 
     }
 }
