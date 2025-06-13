233b6b9fb62c662f4bf21af8eeaf25421a83caab
Umair Ali <umairmamoor@gmail.com>
2025-06-13 06:52:39 -0400

    PhoneMatcher and forEach with group()

--- RegularExpressionInJava/InDepthReview/src/dev/lpa/Review.java
+++ RegularExpressionInJava/InDepthReview/src/dev/lpa/Review.java
@@ -1,5 +1,6 @@
 package dev.lpa;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Review {
@@ -20,6 +21,9 @@ public class Review {
         Pattern phonePattern =
                 Pattern.compile("\\([0-9]{3}\\) [0-9]{3}-[0-9]{4}");
 
+
+        Matcher phoneMatcher = phonePattern.matcher(phoneList);
+        phoneMatcher.results().forEach(mr -> System.out.println(mr.group()));
 
 
 
