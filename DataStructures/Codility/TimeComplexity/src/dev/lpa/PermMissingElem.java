2d466dbf4463e204e4cb75b1108f589cc29ddbf0
Umair Ali <umairmamoor@gmail.com>
2025-07-16 09:12:11 -0400

    exploring the problem

--- DataStructures/Codility/TimeComplexity/src/dev/lpa/PermMissingElem.java
+++ DataStructures/Codility/TimeComplexity/src/dev/lpa/PermMissingElem.java
@@ -1,8 +1,35 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class PermMissingElem {
 
     public static void main(String[] args) {
+
+        int[] A = {2,3,1,5};
+
+    }
+
+    public int solution(int[] A) {
+
+        Arrays.sort(A);
+
+        for (int x = 0; x < A.length; x++ ) {
+            if(x == A[x]) {
+                x ++;
+            }
+            else if(x != A[x]){
+                return x;
+
+            }
+
+            }
+
+        }
+
+
+
+
 
     }
 
