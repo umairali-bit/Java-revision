5eb74a414f7e3344594edb201e955d34c84fdad9
Umair Ali <umairmamoor@gmail.com>
2025-07-18 02:40:17 -0400

    skeleton of the problem

--- DataStructures/Codility/TimeComplexity/src/dev/lpa/Equilibrium.java
+++ DataStructures/Codility/TimeComplexity/src/dev/lpa/Equilibrium.java
@@ -4,6 +4,31 @@ public class Equilibrium {
 
     public static void main(String[] args) {
 
+        Equilibrium solver = new Equilibrium();
+
+        int[] A = {3,1,2,4,3};
+
+        System.out.println("Array");
+        printArray(A);
+
+        System.out.println("Equilibrium");
+        int a = solver.solution(A);
+        System.out.println(a);
+
+
+    }
+
+    public int solution(int[] A) {
+
+    }
+
+
+    public static void printArray(int[] array) {
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+
+        }
     }
 
 
