128778511c54becb4916d57d24954e657dc1954b
Umair Ali <umairmamoor@gmail.com>
2025-07-18 03:37:04 -0400

    FrogJump skeleton

---
+++ DataStructures/Codility/TimeComplexity/src/dev/lpa/FrogJump.java
@@ -0,0 +1,23 @@
+package dev.lpa;
+
+public class FrogJump {
+
+    public static void main(String[] args) {
+
+        FrogJump solver = new FrogJump();
+
+        int X = 30;
+        int Y = 85;
+        int D = 30;
+
+        int result = solver.solution (X, Y, D);
+        System.out.println("Minimal number of jumps " + result);
+
+
+    }
+
+    public int solution(int X, int Y, int D) {
+
+        //finding the distance
+    }
+}
