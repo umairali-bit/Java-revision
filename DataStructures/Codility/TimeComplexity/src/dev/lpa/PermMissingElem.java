10910b8d81da41013bf8a2b7bcd3f9c5db442b79
Umair Ali <umairmamoor@gmail.com>
2025-07-18 02:04:14 -0400

    PermMissingElem solution

--- DataStructures/Codility/TimeComplexity/src/dev/lpa/PermMissingElem.java
+++ DataStructures/Codility/TimeComplexity/src/dev/lpa/PermMissingElem.java
@@ -8,6 +8,17 @@ public class PermMissingElem {
 
         int[] A = {2,3,1,5};
 
+        PermMissingElem solver = new PermMissingElem();
+
+        System.out.println("Array");
+        printArray(A);
+
+        System.out.println("\nMissing Element in Array");
+        int a = solver.solution(A);
+        System.out.println(a);
+
+
+
     }
 
     public int solution(int[] A) {
@@ -15,16 +26,22 @@ public class PermMissingElem {
         Arrays.sort(A);
 
         for (int x = 0; x < A.length; x++ ) {
-            if(x == A[x]) {
-                x ++;
-            }
-            else if(x != A[x]){
-                return x;
+           if(A[x] != x + 1){
+                return x + 1;
 
             }
 
             }
+        return A.length + 1;
 
+        }
+
+
+        public static void printArray (int[] array) {
+
+            for (int i = 0; i < array.length; i ++) {
+                System.out.print(array[i] + " ");
+            }
         }
 
 
@@ -34,9 +51,6 @@ public class PermMissingElem {
     }
 
 
-
-
-}
 
 
 
