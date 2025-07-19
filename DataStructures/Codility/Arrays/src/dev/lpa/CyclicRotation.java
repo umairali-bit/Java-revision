be414e587a347d016aacee45207cbd799bc1265c
Umair Ali <umairmamoor@gmail.com>
2025-07-19 02:35:40 -0400

    CyclicRotation problem

--- DataStructures/Codility/Arrays/src/dev/lpa/CyclicRotation.java
+++ DataStructures/Codility/Arrays/src/dev/lpa/CyclicRotation.java
@@ -4,5 +4,17 @@ public class CyclicRotation {
 
     public static void main(String[] args) {
 
+        int[] A = {3,8,9,7,6};
+        int K = 3;
+
+        CyclicRotation solver = new CyclicRotation();
+        int s = solver.solution(A,K);
+        System.out.println(s);
+
+    }
+
+    public int[] solution(int[] A, int K) {
+
+
     }
 }
