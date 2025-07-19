3df5851dfb450be26f1cc33d75eeb2a060962953
Umair Ali <umairmamoor@gmail.com>
2025-07-19 13:11:54 -0400

    main method

--- DataStructures/Codility/CountingElements/src/dev/lpa/PermCheck.java
+++ DataStructures/Codility/CountingElements/src/dev/lpa/PermCheck.java
@@ -1,4 +1,19 @@
 package dev.lpa;
 
 public class PermCheck {
+
+    public static void main(String[] args) {
+
+        PermCheck solver = new PermCheck();
+        int[] A = {4,1,3,2};
+
+        System.out.println("Array");
+        printArray(A);
+
+        System.out.println("\nIs it permutation");
+        int a = solver.solution(A);
+        System.out.println(a);
+
+
+    }
 }
