2c59e87daf3c38c0dbd55462552a84fe626d194d
Umair Ali <umairmamoor@gmail.com>
2025-07-19 04:32:59 -0400

    Main method

--- DataStructures/Codility/CountingElements/src/dev/lpa/FrogRiverOne.java
+++ DataStructures/Codility/CountingElements/src/dev/lpa/FrogRiverOne.java
@@ -4,5 +4,16 @@ public class FrogRiverOne {
 
     public static void main(String[] args) {
 
+        FrogRiverOne solver = new FrogRiverOne();
+
+        int[] A = {1,3,1,4,2,3,5,4};
+
+        System.out.println("Array");
+        printArray(A);
+
+        System.out.println("\nAll times are covered");
+        int a = solver.solution(A);
+
+
     }
 }
