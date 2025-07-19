fbe7ef3e2c8800925eadea2307257e3b4e54ab20
Umair Ali <umairmamoor@gmail.com>
2025-07-19 04:07:16 -0400

    main method

--- DataStructures/Codility/Arrays/src/dev/lpa/OddOccurrenceInArray.java
+++ DataStructures/Codility/Arrays/src/dev/lpa/OddOccurrenceInArray.java
@@ -1,8 +1,17 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class OddOccurrenceInArray {
 
     public static void main(String[] args) {
+
+        int[] A = {9,3,9,3,9,7,9};
+
+        OddOccurrenceInArray solver = new OddOccurrenceInArray();
+
+        int[] a = solver.solution(A);
+        System.out.println(Arrays.toString(a));
 
     }
 
