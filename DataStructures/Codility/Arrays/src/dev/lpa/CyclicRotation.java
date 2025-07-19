ffb3f6f99a610d8f2b8b75e19abb0b325b64f7fc
Umair Ali <umairmamoor@gmail.com>
2025-07-19 03:05:42 -0400

    notes

--- DataStructures/Codility/Arrays/src/dev/lpa/CyclicRotation.java
+++ DataStructures/Codility/Arrays/src/dev/lpa/CyclicRotation.java
@@ -1,5 +1,7 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class CyclicRotation {
 
     public static void main(String[] args) {
@@ -8,12 +10,36 @@ public class CyclicRotation {
         int K = 3;
 
         CyclicRotation solver = new CyclicRotation();
-        int s = solver.solution(A,K);
-        System.out.println(s);
+        int[] s = solver.solution(A,K);
+        System.out.println(Arrays.toString(s));
 
     }
 
     public int[] solution(int[] A, int K) {
+
+        int N = A.length;
+
+        if (N == 0) return A;
+
+        //if K is larger than N
+        K = K % N;
+
+        if (K == 0) return A;
+
+        int[] rotated = new int[N];
+
+        // copy last K elements to the beginning of rotated array
+        for (int i = 0; i < K; i++) {
+            rotated[i] = A[N - K + i];
+        }
+
+        //copy the rest of elements after K to the rotated array
+        for (int i = K; i < N; i++) {
+            rotated[i] = A[i -K];
+        }
+
+        return rotated;
+
 
 
     }
