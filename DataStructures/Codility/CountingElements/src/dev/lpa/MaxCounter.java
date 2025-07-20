d7373e2cf188c7150eeb6ce8b8eca1d3dd86a6cf
Umair Ali <umairmamoor@gmail.com>
2025-07-20 09:29:18 -0400

    solution

--- DataStructures/Codility/CountingElements/src/dev/lpa/MaxCounter.java
+++ DataStructures/Codility/CountingElements/src/dev/lpa/MaxCounter.java
@@ -1,18 +1,55 @@
 package dev.lpa;
 
+import java.util.Arrays;
+
 public class MaxCounter {
 
     public static void main(String[] args) {
 
-        MaxCounter solver = new MaxCounter();
+
 
         int[] A = {3,4,4,6,1,4,4};
+
+        int N = 5;
 
         System.out.println("Initial Array");
         printArray(A);
 
-//        int[] a = solver.solution(N, A);
-//        System.out.println("Max Counter[] = " + a);
+        System.out.println("\nmax counters:");
+
+        System.out.println(Arrays.toString(new MaxCounter().solution(N, A)));
+
+    }
+
+    public int[] solution (int N, int[] A) {
+
+        int[] counters = new int[5];
+        Arrays.fill(counters, 0);
+        int start = 0;
+        int current_max = 0;
+
+        for (int i : A) {
+
+            int x = i - 1;
+            if (i > N){
+                start = current_max;
+            } else if (counters[x] < start) {
+                counters[x] = start + 1;
+            } else {
+                counters[x] += 1;
+            } if (i <= N && counters[x] > current_max) {
+                current_max = counters[x];
+            }
+
+            for (int j = 0; j < counters.length; j++) {
+                if (counters[j] < start) {
+                    counters[j] = start;
+                }
+            }
+
+
+        }
+        return counters;
 
     }
 
