c92ad19ec973c01a6ef0deecfab759d0ce495587
Umair Ali <umairmamoor@gmail.com>
2025-07-22 15:06:46 -0400

    Matcher with the groups

--- RegularExpressionInJava/InDepthReview/src/dev/lpa/Review.java
+++ RegularExpressionInJava/InDepthReview/src/dev/lpa/Review.java
@@ -19,12 +19,32 @@ public class Review {
                 """;
 
         Pattern phonePattern =
-                Pattern.compile("\\(*[0-9]{3}[)\\s-]*[0-9]{3}[)\\s-]*[0-9]{4}");
+                Pattern.compile("\\(*[0-9]{3}[)\\s-]*\\d{3}[)\\s-]*\\p{Digit}{4}");
 
 
         Matcher phoneMatcher = phonePattern.matcher(phoneList);
         phoneMatcher.results().forEach(mr -> System.out.println(mr.group()));
 
+
+        String htmlSnippets = """
+                
+                <H1>My Heading</H1>
+                <h2>Sub-Heading<h2>
+                <p>This is a paragraph about something<p>
+                <h3 id="third">Summary</h3>
+                <br/>
+                <p>Testing</p>
+                
+                """;
+
+        Pattern htmlPattern =
+                Pattern.compile("<(\\w+)[^>]*>([^\\v</>]*)(</\\1>)*");
+
+        Matcher m = htmlPattern.matcher(htmlSnippets);
+        m.results()
+                .forEach(mr -> System.out.println("Full Tag: " + mr.group(0)
+                + "\n\tType: " + mr.group(1)
+                + "\n\tText: " + mr.group(2)));
 
 
     }
