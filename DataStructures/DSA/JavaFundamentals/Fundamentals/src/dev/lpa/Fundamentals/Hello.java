7594a4b2495e5ca66ba720c74ef1e84d47af8484
Umair Ali <umairmamoor@gmail.com>
2025-07-23 03:27:08 -0400

    notes

--- DataStructures/DSA/JavaFundamentals/Fundamentals/src/dev/lpa/Fundamentals/Hello.java
+++ DataStructures/DSA/JavaFundamentals/Fundamentals/src/dev/lpa/Fundamentals/Hello.java
@@ -4,6 +4,28 @@ public class Hello {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println(4 + 3);
+        System.out.println(3 * 6);
+
+        int age = 26;
+        int aGe = 12; // not same as age
+        int marks = 98;
+
+    //    int 2ge = 56 //invalid
+
+        int secondMarksOfStudent = 23; // camel cases
+
+    //    int second Marks Of Student //invalid
+
+        int second_allowed$allowed = 22; //valid
+
+        int $marks = 12; //valid
+
+    //invalid    int static = 12;
+
+
+
+
     }
 
 
