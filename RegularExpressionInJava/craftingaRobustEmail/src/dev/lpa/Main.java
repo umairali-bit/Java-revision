4c6228b79347b67b2519ebbb2a80cc7b34933e75
Umair Ali <umairmamoor@gmail.com>
2025-07-23 05:32:49 -0400

    Crafting a robust email

---
+++ RegularExpressionInJava/craftingaRobustEmail/src/dev/lpa/Main.java
@@ -0,0 +1,20 @@
+package dev.lpa;
+/*
+In this challenge, your goal is to write a program that uses regular expressions to validate email addresses, based on
+the predefined set of rules below.
+An email address should be formatted as username@domain.
+The username part of the email address can contain alphanumeric characters, so lowercase a to z, upper case A to Z,
+digits 0 to 9, dots or periods, underscores, and hyphens or dashes (-).
+The domain part of the email address can contain alphanumeric characters and digits, as well as periods or dashes,
+such as xyz.com, or abc-xyz.org, but could also be xyz.edu.uk for example.
+
+Your program should:
+Use regular expressions and the Pattern class to compile the pattern.
+Use the Matcher class to match the entered email address against the compiled pattern.
+For valid email addresses, display the username and the domain name, using one of the group methods.
+Use the examples shown here, or ones of your own invention, to test your code.
+
+ */
+
+public class Main {
+}
