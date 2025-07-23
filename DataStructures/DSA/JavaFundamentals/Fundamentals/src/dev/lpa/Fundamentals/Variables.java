e1551b393dc4372c746a46a0221b2ba60a40053b
Umair Ali <umairmamoor@gmail.com>
2025-07-23 03:44:23 -0400

    fundamentals

--- DataStructures/DSA/JavaFundamentals/Fundamentals/src/dev/lpa/Fundamentals/Variables.java
+++ DataStructures/DSA/JavaFundamentals/Fundamentals/src/dev/lpa/Fundamentals/Variables.java
@@ -6,5 +6,20 @@ public class Variables {
 
 
         boolean isPass = true;
+
+        int count = 12;
+        byte marks = 13;
+        short studentCount = 1235;
+        float pi = 3.14f;
+        double secondPi = 3.14;
+
+
+
+        System.out.println(pi);
+        System.out.println(secondPi);
+
+        char myLetter = 'A';
+        System.out.println(myLetter);
+
     }
 }
