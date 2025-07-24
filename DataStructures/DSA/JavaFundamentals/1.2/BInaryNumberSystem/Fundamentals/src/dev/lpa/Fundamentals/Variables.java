fc45d9649e7bfcc584fc86527d0c52a61ef9af5d
Umair Ali <umairmamoor@gmail.com>
2025-07-24 13:39:25 -0400

    1.2

---
+++ DataStructures/DSA/JavaFundamentals/1.2/BInaryNumberSystem/Fundamentals/src/dev/lpa/Fundamentals/Variables.java
@@ -0,0 +1,25 @@
+package dev.lpa.Fundamentals;
+
+public class Variables {
+
+    public static void main(String[] args) {
+
+
+        boolean isPass = true;
+
+        int count = 12;
+        byte marks = 13;
+        short studentCount = 1235;
+        float pi = 3.14f;
+        double secondPi = 3.14;
+
+
+
+        System.out.println(pi);
+        System.out.println(secondPi);
+
+        char myLetter = 'A';
+        System.out.println(myLetter);
+
+    }
+}
