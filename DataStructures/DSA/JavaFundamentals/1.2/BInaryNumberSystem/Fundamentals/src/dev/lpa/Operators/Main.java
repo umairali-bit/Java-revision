1d17fbe325274840a8faba16d474cd665c7f0f5c
Umair Ali <umairmamoor@gmail.com>
2025-07-24 14:28:54 -0400

    operators

--- DataStructures/DSA/JavaFundamentals/1.2/BInaryNumberSystem/Fundamentals/src/dev/lpa/Operators/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.2/BInaryNumberSystem/Fundamentals/src/dev/lpa/Operators/Main.java
@@ -10,6 +10,24 @@ public class Main {
     //    int result = a + b;
         System.out.println(a + b);
 
+        //subtraction operator
+        int c = 12;
+        int d = 7;
+        //    int result = c - d;
+        System.out.println(c - d);
+
+        //multiplication operator
+        int e = 12;
+        int f = 7;
+        //    int result = e * f;
+        System.out.println(e * f);
+
+        //division operator
+        int g = 12;
+        int h = 7;
+        //    int result = g / h;
+        System.out.println((double) g / h);
+
 
 
     }
