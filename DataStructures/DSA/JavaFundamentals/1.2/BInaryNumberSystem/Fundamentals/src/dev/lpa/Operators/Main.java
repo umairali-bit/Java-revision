ba95355452bfaccd13e0d874e811bfd8cf697099
Umair Ali <umairmamoor@gmail.com>
2025-07-24 14:25:34 -0400

    addition

--- DataStructures/DSA/JavaFundamentals/1.2/BInaryNumberSystem/Fundamentals/src/dev/lpa/Operators/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.2/BInaryNumberSystem/Fundamentals/src/dev/lpa/Operators/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //addition operator
+        int a = 12;
+        int b = 7;
+    //    int result = a + b;
+        System.out.println(a + b);
+
+
 
     }
 }
