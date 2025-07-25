b663510f33608a6c6242e8d899f139ec337919ba
Umair Ali <umairmamoor@gmail.com>
2025-07-25 04:41:35 -0400

    ifElse statements

--- DataStructures/DSA/JavaFundamentals/1.3/JavaConditionalStatements/Fundamentals/src/dev/lpa/Fundamentals/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.3/JavaConditionalStatements/Fundamentals/src/dev/lpa/Fundamentals/Main.java
@@ -4,5 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
+
+        int age = 4;
+
+        if (age >= 18) {
+
+            System.out.println("You can vote");
+            System.out.println("Good morning");
+
+        } else {
+            System.out.println("You cannot vote!");
+        }
+
+        int day = 3;
+
+        if (day == 1) {
+            System.out.println("Go Home!");
+        } else if(day == 2) {
+            System.out.println("Go to firends!");
+        } else if (day == 3) {
+            System.out.println("Go to Party");
+        }
+        else {
+            System.out.println("Office!");
+        }
+        System.out.println("Hello World");
+
+
+
     }
 }
