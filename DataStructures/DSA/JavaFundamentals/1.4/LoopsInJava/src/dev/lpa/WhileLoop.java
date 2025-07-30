e2e003eddf67670c2c36645489c03439eb40ae06
Umair Ali <umairmamoor@gmail.com>
2025-07-30 05:49:36 -0400

    boolean while loop

--- DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/WhileLoop.java
+++ DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/WhileLoop.java
@@ -1,13 +1,26 @@
 package dev.lpa;
 
+import java.util.Scanner;
+
 public class WhileLoop {
     public static void main(String[] args) {
 
-        int i = 0;
+//        int i = 0;
+//
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
 
-        while (i < 5) {
-            System.out.println(i);
-            i++;
+        Scanner sc = new Scanner(System.in);
+
+        boolean hasLearnt = false;
+
+        while(!hasLearnt) {
+
+            System.out.println("Lets learn!!");
+            System.out.println("Learnt?");
+            hasLearnt = sc.nextBoolean();
         }
 
     }
