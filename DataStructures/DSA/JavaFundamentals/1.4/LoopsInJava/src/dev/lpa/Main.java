2a30b61cfdb30d8e00ddaa34bcba71e3aa8d62c5
Umair Ali <umairmamoor@gmail.com>
2025-07-30 04:21:27 -0400

    A table of 10

--- DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/Main.java
@@ -1,11 +1,23 @@
 package dev.lpa;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         for(int i = 1; i<5; i++) {
             System.out.println(i + ": Hello World ");
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number");
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i<=10; i++) {
+            System.out.println(n + " * " + i +" = " + i*n);
         }
 
 
