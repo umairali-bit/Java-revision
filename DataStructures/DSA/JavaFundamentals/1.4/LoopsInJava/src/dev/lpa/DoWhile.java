1640eae8a596e0f2fd8b160d33ae3e52f05c5288
Umair Ali <umairmamoor@gmail.com>
2025-07-31 05:19:37 -0400

    doWhile loop

---
+++ DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/DoWhile.java
@@ -0,0 +1,17 @@
+package dev.lpa;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+
+        int i = 8;
+
+        do {
+            System.out.println(i);
+            i++;
+        } while (i <= 5);
+
+        System.out.println("Out of loop");
+
+    }
+}
