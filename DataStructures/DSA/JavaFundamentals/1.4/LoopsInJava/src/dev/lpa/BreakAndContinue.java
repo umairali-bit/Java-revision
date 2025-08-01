8fb46ce6877cbf2ef0fb50a8d744095f73f47dfe
Umair Ali <umairmamoor@gmail.com>
2025-08-01 06:37:10 -0400

    break

---
+++ DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/BreakAndContinue.java
@@ -0,0 +1,13 @@
+package dev.lpa;
+
+public class BreakAndContinue {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i <= 20; i += 2) {
+            System.out.println(i);
+            if(i >= 10) break;
+
+        }
+    }
+}
