2a77d1c14bb5878bfac5e2c002653a53576f8186
Umair Ali <umairmamoor@gmail.com>
2025-08-06 05:49:33 -0400

    nested loops

--- DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/BreakAndContinue.java
+++ DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/BreakAndContinue.java
@@ -5,8 +5,6 @@ public class BreakAndContinue {
 
 
         for (int i = 0; i <= 20; i += 2) {
-            System.out.println(i);
-            if(i >= 10) break;
 
         }
 
---
+++ DataStructures/DSA/JavaFundamentals/1.4/LoopsInJava/src/dev/lpa/NestedLoops.java
@@ -0,0 +1,15 @@
+package dev.lpa;
+
+public class NestedLoops {
+
+    public static void main(String[] args) {
+
+
+        // running a loop 10 times
+        for (int count = 0; count <= 10; count++){
+            for (int i = 1; i < 6; i++) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
