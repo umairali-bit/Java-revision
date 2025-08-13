a022dca83d0d3ca3318ca3e9441880911b0ccc3b
Umair Ali <umairmamoor@gmail.com>
2025-08-13 05:17:56 -0400

    arrays

--- DataStructures/DSA/JavaFundamentals/1.5/Arrays/src/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.5/Arrays/src/Main.java
@@ -2,6 +2,22 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int age[] = new int[5];
+
+        age[0] = 5;
+        age[1] = 2;
+
+        System.out.println(age[0]);
+        System.out.println(age[1]);
+        System.out.println(age[2]);
+
+        System.out.println(age.length);
+
+
+        int marks[] = {98, 12,45,12,65};
+
+        System.out.println(marks[0]); //98
+
 
 
 
