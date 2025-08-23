022fe49cfa6b63c710421bacb3b44b9c9aa7ef40
Umair Ali <umairmamoor@gmail.com>
2025-08-23 03:42:33 -0400

    Stack and Heap

--- DataStructures/DSA/JavaFundamentals/1.7/StringInJava/src/Strings/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.7/StringInJava/src/Strings/Main.java
@@ -1,4 +1,25 @@
 package Strings;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        //declaring a String
+        String name = "Walter White"; //String literal
+
+        String nameA = new String("Walter White");
+
+        /*
+        Memory - Stack and Heap
+        Stack - references
+        Heap - String Constant Pool - Object
+
+        Stack - name (reference)
+        name (reference) -> heap -> String Constant Pool -> Walter White
+
+        Stack - nameA (reference)
+        nameA -> pointing to the same heap -> String Constant Pool -> Walter White
+         */
+
+    }
 }
