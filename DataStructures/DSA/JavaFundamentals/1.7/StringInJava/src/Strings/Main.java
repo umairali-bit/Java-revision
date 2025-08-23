b844b0032e85c10286552250088705605db88eab
Umair Ali <umairmamoor@gmail.com>
2025-08-23 03:58:11 -0400

    to check references are equal or not

--- DataStructures/DSA/JavaFundamentals/1.7/StringInJava/src/Strings/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.7/StringInJava/src/Strings/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
         //declaring a String
         String name = "Walter White"; //String literal
+        String sameName = "Walter White";
 
         String nameA = new String("Walter White");
+        String nameB = new String("Walter White");
 
         /*
         Memory - Stack and Heap
@@ -19,7 +21,38 @@ public class Main {
 
         Stack - nameA (reference)
         nameA -> heap -> (without String Constant Pool) -> Walter White
+
+        Strings are immutable
+
+        String are immutable but its objects are mutable
+
+       String name = "Walter White"
+       name = "Skyler White"
+
+       now name is pointing to another object
+       Garbage collector will later erase "walter white"
          */
+
+        //to check if references are equal
+        if (name == sameName) {
+            System.out.println("References are equal");
+
+            if (nameB == nameA) {
+                System.out.println("References are equal");
+
+            }
+            else {
+                System.out.println("References are not equal");
+
+            }
+
+        } else {
+            System.out.println("References are not equal");
+
+        }
+
+
+
 
     }
 }
