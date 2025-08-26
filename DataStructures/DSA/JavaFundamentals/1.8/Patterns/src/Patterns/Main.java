31a1ac54472a8402842139a171a09575c98ef5bc
Umair Ali <umairmamoor@gmail.com>
2025-08-26 08:06:01 -0400

    incrementByI

--- DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SevenBySeven.sevenBySeven();
+        Patterns.sevenBySeven();
+        Patterns.incrementI();
 
     }
 }
--- DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Patterns.java
+++ DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Patterns.java
@@ -1,12 +1,24 @@
 package Patterns;
 
-public class SevenBySeven {
+public class Patterns {
 
     public static void sevenBySeven () {
         int n = 7;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j<= n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void incrementI () {
+
+        int n = 7;
+
+        for (int i = 1; i<=n; i++) {
+            for (int j = 1; j<=i; j++) {
                 System.out.print("*");
             }
             System.out.println();
