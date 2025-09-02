a49c494846851104d55ecfbcd2b19c7a6517e3ce
Umair Ali <umairmamoor@gmail.com>
2025-09-02 03:41:21 -0400

    InvertedRightAngledDigits()

--- DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Main.java
+++ DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Main.java
@@ -14,6 +14,7 @@ public class Main {
         Patterns.diamondPattern();
         Patterns.invertedRightAngled();
         Patterns.invertedRightAngledRight();
+        Patterns.invertedRightAngledDigits();
 
     }
 }
--- DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Patterns.java
+++ DataStructures/DSA/JavaFundamentals/1.8/Patterns/src/Patterns/Patterns.java
@@ -169,6 +169,16 @@ public class Patterns {
         }
     }
 
+    public static void invertedRightAngledDigits() {
+        int n = 7;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+    }
+
 
 
 
